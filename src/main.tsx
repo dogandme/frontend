f0b2e9b84f11cb2ae69b1624e6a819c8695f3f3e
aken-you <kinsk2839@naver.com>
2024-08-20 21:26:47 +0900

    refactor[#24] : 회원가입 router 설정
    Co-authored-by: aken-you <kinsk2839@naver.com>
    Co-authored-by: yonghyeun <ttddcc119@naver.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './global.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./global.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
