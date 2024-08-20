2b5c4d15bcbcef5a07bfd03e440ed8a3b1269454
yonghyeun <ttddcc119@naver.com>
2024-08-20 14:08:40 +0900

    fix[#18] : TextInput placeholder props 추가

---
+++ src/shared/ui/input/TextInput.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import TextInput from "./TextInput";
+
+const meta: Meta<typeof TextInput> = {
+  component: TextInput,
+  title: "shared/Input/PlainTextInput",
+  argTypes: {
+    additionalOutterStyle: {
+      control: {
+        type: "object",
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TextInput>;
+
+export const Default: Story = {
+  args: {
+    title: "Title",
+    id: "sample-id",
+    designType: "textInput",
+    condition: "default",
+    supportingText: "Supporting text",
+    placeholder: "Placeholder",
+    additionalOutterStyle: {
+      width: "328px",
+    },
+  },
+};
+
+export const PlainTextInputWithError: Story = {
+  args: {
+    ...Default.args,
+    condition: "error",
+  },
+};
