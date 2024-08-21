e71d78dccd6673206f6d0a9c9eee0e0fefb72a62
yonghyeun <ttddcc119@naver.com>
2024-08-21 16:34:12 +0900

    feat[#18] : Input 컴포넌트 disabled props 추가
    - div 태그와 같이 active 하지 않은 태그는 disabled 가상 선택자 스타일 속성이 적용되지 않음
    - 그렇기 때문에 disabled 와 같은 이벤트를 흉내내는 클래스 속성 추가 및 핸들링

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -12,6 +12,7 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   statusText: null | string;
   trailingNode?: React.ReactNode;
   leadingNode?: React.ReactNode;
+  disabled: boolean;
 }
 
 const Input = ({
@@ -22,6 +23,7 @@ const Input = ({
   statusText = null,
   trailingNode,
   leadingNode,
+  disabled = false,
   ...props
 }: InputProps) => {
   // 휴먼에러를 방지하기 위해 leadingNode,trailign 유효성 확인
@@ -35,7 +37,14 @@ const Input = ({
   // input 컴포넌트의 상태에 따라 적용되는 스타일
   const status: StatusType = isError ? "error" : "default";
   const statusStyles = inputStyles[componentType][status];
-  const { statusTextClass, ...wrapperClasses } = statusStyles;
+  const { statusTextClass, ...wrapperClassesObject } = statusStyles;
+
+  // wrapper 에 해당하는 div에선 hover,focus에 대한 가상 선택자 스타일이 적용되지만
+  // disabled에 대한 가상 선택자 스타일은 적용되지 않습니다.
+  // 그렇기 때문에 disabled에 대한 스타일을 따로 분리하여 적용합니다.
+  const { disabled: disableClass, ...restStylesObject } = wrapperClassesObject;
+  const restClasess = Object.values(restStylesObject).join(" ");
+  const wrapperClasses = [restClasess, disabled ? disableClass : ""].join(" ");
 
   return (
     <div className="flex flex-col items-start">
@@ -44,13 +53,14 @@ const Input = ({
           {label}
         </label>
       </div>
-      <div className={`${Object.values(wrapperClasses).join(" ")}`}>
+      <div className={wrapperClasses}>
         {leadingNode}
         <input
           id={id}
           name={id}
           className={baseStyles.input}
           autoComplete="off"
+          disabled={disabled}
           {...props}
         />
         {trailingNode}
