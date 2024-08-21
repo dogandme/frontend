3aba37f4720df3bd33a07cda63884216bd8f62b0
kinsk2839 <96980857+aken-you@users.noreply.github.com>
2024-08-21 18:15:51 +0900

    [Feat] Button 컴포넌트 생성 (#20)
    * feat[#17] : Button 컴포넌트 공통 스타일 적용
    
    * feat[#17] : Button 컴포넌트 색상 스타일 적용
    
    * feat[#17] : Button 컴포넌트 사이즈 스타일 설정
    
    - size에 따라 padding, text size, height가 달라짐
    - icon, text의 유무에 따라 padding이 달라짐
    
    * refactor[#17] : Button 컴포넌트 색상 스타일 수정
    
    - type이 text이고 state가 enabled이면, background-color는 없음
    
    * refactor[#17] : Button 컴포넌트의 스타일을 정의하는 파일 추가
    
    * refactor[#17] : Button 컴포넌트 outline: none으로 설정
    
    * style[#17] : children의 첫 번째 자식 요소를 저장하는 변수와 마지막 자식 요소를 저장하는 변수 네이밍 변경
    
    * refactor[#17] : 첫 번째 자식 요소의 타입이 string인지 저장하는 변수 생성
    
    * refactor[#17] : 마지막 자식 요소의 타입이 string인지 저장하는 변수 생성
    
    * refactor[#17] : Button 컴포넌트가 disabled 상태일 때, 포인터 이벤트(클릭, 호버, 포커스 등)를 비활성화
    
    * refactor[#17] : :focus 가상 클래스를 :focus-visible로 변경
    
    * feat[#17] : Button 컴포넌트 스토리북 작성
    
    * chore[#17] : tailwind.config.js에서 오타 수정
    
    * feat[#17] : Button 컴포넌트 export
    
    * feat[#17] : Button 컴포넌트의 children 타입 설정
    
    - ButtonHTMLAttributes<HTMLButtonElement>에서 children이 필수가 아니다.
    - children을 필수값으로 입력하기 위해, children 타입 설정
    
    * refactor[#17] : children에 여러 요소가 있을 경우를 대비해 조건 수정
    
    - 이전에는 children에 올 수 있는 하위 요소를 2개만 허락했었음
    - children에 여러 요소가 있더라도 스타일이 적용될 수 있도록 조건 수정
    
    * style[#17] : Button 컴포넌트 props 네이밍 변경
    
    * feat[#17] : Button 컴포넌트의 children에 여러 개의 텍스트 요소를 포함할 경우 error throw
    
    * refactor[#17] : Button 컴포넌트의 여러 개의 자식이 있는 경우에 관한 조건 수정
    
    - 2개를 초과할 경우 에러 처리
    - hasIconFirst와 hasTextFirst 조건 수정
    
    * refactor[#17] : Button 컴포넌트에서 children 타입이 동일할 경우 에러 처리
    
    * refactor[#17] : isSameType 위치 변경
    
    * feat[#17] : Button 컴포넌트에 forwardRef 적용
    
    * refactor[#17] : index 확장자 tsx -> ts로 변경

---
+++ src/shared/ui/button/Button.tsx
@@ -0,0 +1,92 @@
+import {
+  ButtonHTMLAttributes,
+  Children,
+  forwardRef,
+  isValidElement,
+  ReactNode,
+} from "react";
+import { colors, getSizeStyles } from "./button.styles";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  colorType: "primary" | "secondary" | "tertiary";
+  variant: "filled" | "outlined" | "text";
+  size: "xSmall" | "small" | "medium" | "large";
+  children: ReactNode;
+};
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ colorType, variant, size, children, ...props }, ref) => {
+    const baseStyles =
+      "inline-flex flex-shrink-0 items-center justify-center gap-[.625rem] rounded-[1rem] outline-none";
+    const colorStyles = colors[colorType][variant];
+
+    const childrenArray = Children.toArray(children);
+    const isText = (child: ReactNode) =>
+      typeof child === "string" ||
+      (isValidElement(child) && child.type === "div") ||
+      (isValidElement(child) && child.type === "span") ||
+      (isValidElement(child) && child.type === "p");
+
+    // children 하위의 하나의 자식만을 가지고 있는지 확인
+    const hasOneChild = childrenArray.length === 1;
+
+    // 첫 번째 child가 텍스트인지 확인
+    const firstChild = childrenArray[0];
+    const isFirstChildText = isText(firstChild);
+
+    // 아이콘만 있는 경우
+    const hasOnlyIcon = hasOneChild && !isFirstChildText;
+    // 텍스트만 있는 경우
+    const hasOnlyText = hasOneChild && isFirstChildText;
+
+    // children에 있는 요소가 여러개인지 확인
+    const haveTwoChildren = childrenArray.length === 2;
+
+    if (childrenArray.length > 2) {
+      throw new Error(
+        "Button 컴포넌트는 최대 2개의 자식만 포함할 수 있습니다. 하나의 텍스트 또는 아이콘만 포함하거나, 아이콘과 텍스트를 함께 사용해주세요",
+      );
+    }
+
+    // 마지막 child가 텍스트인지 확인
+    const lastChild = childrenArray[childrenArray.length - 1];
+    const isLastChildText = isText(lastChild);
+
+    const isSameType =
+      (isFirstChildText && isLastChildText) ||
+      (!isFirstChildText && !isLastChildText);
+
+    if (haveTwoChildren && isSameType) {
+      throw new Error(
+        "Button 컴포넌트에는 같은 타입의 요소를 여러개 포함할 수 없습니다.",
+      );
+    }
+
+    const hasIconFirst =
+      haveTwoChildren && !isFirstChildText && isLastChildText;
+    const hasTextFirst =
+      haveTwoChildren && isFirstChildText && !isLastChildText;
+
+    const sizeStyles = getSizeStyles({
+      size,
+      hasOnlyIcon,
+      hasOnlyText,
+      hasIconFirst,
+      hasTextFirst,
+    });
+
+    return (
+      <button
+        ref={ref}
+        className={`${baseStyles} ${colorStyles.enabled} ${colorStyles.focus} ${colorStyles.hover} ${colorStyles.active} ${colorStyles.disabled} ${sizeStyles}`}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  },
+);
+
+Button.displayName = "Button";
+
+export default Button;
