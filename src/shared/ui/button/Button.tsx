430cc9db1c57d5a3725a3fb896587ae3293fddac
aken-you <kinsk2839@naver.com>
2024-08-21 12:48:49 +0900

    refactor[#17] : Button 컴포넌트의 여러 개의 자식이 있는 경우에 관한 조건 수정
    - 2개를 초과할 경우 에러 처리
    - hasIconFirst와 hasTextFirst 조건 수정

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -44,22 +44,20 @@ const Button = ({
   const hasOnlyText = hasOneChild && isFirstChildText;
 
   // children에 있는 요소가 여러개인지 확인
-  const hasMultipleChildren = childrenArray.length >= 2;
+  const haveTwoChildren = childrenArray.length === 2;
+
+  if (childrenArray.length > 2) {
+    throw new Error(
+      "Button 컴포넌트는 최대 2개의 자식만 포함할 수 있습니다. 하나의 텍스트 또는 아이콘만 포함하거나, 아이콘과 텍스트를 함께 사용해주세요",
+    );
+  }
 
   // 마지막 child가 텍스트인지 확인
   const lastChild = childrenArray[childrenArray.length - 1];
   const isLastChildText = isText(lastChild);
 
-  const areAllChildrenText = childrenArray.every(isText);
-
-  if (hasMultipleChildren && areAllChildrenText) {
-    throw new Error(
-      "Button 컴포넌트에는 여러 개의 텍스트 요소를 포함할 수 없습니다. 하나의 텍스트만 포함하거나 아이콘과 함께 사용해주세요.",
-    );
-  }
-
-  const hasIconFirst = hasMultipleChildren && isLastChildText;
-  const hasTextFirst = hasMultipleChildren && isFirstChildText;
+  const hasIconFirst = haveTwoChildren && !isFirstChildText && isLastChildText;
+  const hasTextFirst = haveTwoChildren && isFirstChildText && !isLastChildText;
 
   const sizeStyles = getSizeStyles({
     size,
