9b7c2ff59f81715700b1dca39d15c1ae62a8ad05
aken-you <kinsk2839@naver.com>
2024-08-22 14:03:13 +0900

    chore[#26] : svg 파일을 컴포넌트로 사용하기 위한 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -12,6 +14,16 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [
+        svgr({
+          // svgr options: https://react-svgr.com/docs/options/
+          include: "**/*.svg", // svg 파일을 react 컴포넌트로 변환
+        }),
+      ],
+    });
   },
 };
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      include: "**/*.svg", // svg 파일을 react 컴포넌트로 변환
+    }),
+  ],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
