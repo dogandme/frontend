c45d1f4836dbf16bb4596eac27b9c9dd200777fe
yonghyeun <ttddcc119@naver.com>
2024-08-23 00:50:16 +0900

    feat[#52] : BackwardNavigationBar 위젯 생성

--- src/widgets/navigationbar/ui/BackwardNavigationBar.tsx
+++ src/widgets/navigationbar/ui/BackwardNavigationBar.tsx
@@ -3,18 +3,27 @@ import { WardLeftIcon } from "@/shared/ui/icon";
 import { Button } from "@/shared/ui/button";
 import { NavigationBar } from "@/shared/ui/navigationbar";
 
+<<<<<<< HEAD
 const BackWardButton = (props: React.HTMLAttributes<HTMLButtonElement>) => {
   const navigate = useNavigate();
   const { onClick, ...rest } = props;
 
+=======
+const BackWardButton = () => {
+  const navigate = useNavigate();
+>>>>>>> d5c9c2c (feat[#52] : BackwardNavigationBar 위젯 생성)
   return (
     <Button
       size="large"
       variant="text"
       colorType="tertiary"
+<<<<<<< HEAD
       onClick={onClick || (() => navigate(-1))}
       fullWidth={false}
       {...rest}
+=======
+      onClick={() => navigate(-1)}
+>>>>>>> d5c9c2c (feat[#52] : BackwardNavigationBar 위젯 생성)
     >
       <WardLeftIcon fill="#616161" />
     </Button>
