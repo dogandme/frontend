27b90f339df4f38ef81efce730d501f55bcb07c9
yonghyeun <ttddcc119@naver.com>
2024-08-22 23:52:57 +0900

    feat[#32] : login 페이지에 사용할 Icon 추가

--- src/shared/ui/icon/index.ts
+++ src/shared/ui/icon/index.ts
@@ -2,6 +2,8 @@ import withIcon from "./Icon";
 
 // svg 파일 Import
 import EmailSvg from "@/shared/assets/email.svg";
+import WardLeftSvg from "@/shared/assets/ward-left.svg";
 
 // Icon 컴포넌트 생성
 export const EmailIcon = withIcon(EmailSvg);
+export const WardLeftIcon = withIcon(WardLeftSvg);
