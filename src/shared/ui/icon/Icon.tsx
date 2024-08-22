0fd0c48036897be9e7f7ca9c4cce3f99f17c76b0
aken-you <kinsk2839@naver.com>
2024-08-22 17:26:51 +0900

    refactor[#26] : withIcon의 fill이 색상 토큰 값일 경우 colors를 통해 fill 처리

--- src/shared/ui/icon/Icon.tsx
+++ src/shared/ui/icon/Icon.tsx
@@ -1,4 +1,9 @@
 import { ElementType } from "react";
+import { colors } from "@/shared/constants";
+
+type Colors = typeof colors;
+type ColorKey = keyof Colors;
+type ShadeKey = keyof Colors[ColorKey];
 
 interface IconProps {
   width?: number;
@@ -7,8 +12,22 @@ interface IconProps {
 }
 
 const withIcon = (Component: ElementType) => {
-  return ({ width = 24, height = 24, fill = "#9E9E9E" }: IconProps) => {
-    return <Component width={width} height={height} fill={fill} />;
+  return ({ width = 24, height = 24, fill = colors.grey[500] }: IconProps) => {
+    const isHex = fill.match(/^#[0-9A-F]{6}$/i);
+
+    if (isHex) {
+      return <Component width={width} height={height} fill={fill} />;
+    }
+
+    const [colorKey, shade] = fill.split("-") as [ColorKey, ShadeKey];
+
+    if (!colors[colorKey] || !colors[colorKey][shade]) {
+      throw new Error("아이콘 색상을 hex 또는 theme 색상으로 지정해주세요.");
+    }
+
+    const iconColor = colors[colorKey][shade];
+
+    return <Component width={width} height={height} fill={iconColor} />;
   };
 };
 
