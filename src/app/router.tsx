cc91675369717c1e13a7d948091478cae37aa8b8
yonghyeun <ttddcc119@naver.com>
2024-08-23 11:27:03 +0900

    feat[#52] : /login 경로 페이지 생성

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -18,7 +18,7 @@ export const router = createBrowserRouter([
         children: [
           {
             index: true,
-            element: <div>login</div>,
+            element: <LoginPage />,
           },
           {
             path: "email",
