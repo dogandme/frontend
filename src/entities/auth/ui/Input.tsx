a08679fc9aaad230cbb50f43ba9a3ecdeb148c5c
yonghyeun <ttddcc119@naver.com>
2024-08-24 20:12:17 +0900

    feat[#32] : Id,passwordInput entities 추가

---
+++ src/entities/auth/ui/Input.tsx
@@ -0,0 +1,41 @@
+import { VisibilityOffIcon, VisibilityOnIcon } from "@/shared/ui/icon";
+import { Input, InputProps } from "@/shared/ui/input/Input";
+import { useState } from "react";
+
+type FixedInputProps = "type" | "componentType";
+
+export const IdInput = (props: Omit<InputProps, FixedInputProps>) => (
+  <Input
+    type="text"
+    placeholder="이메일을 입력해주세요"
+    componentType="outlinedText"
+    {...props}
+  />
+);
+
+export const PasswordInput = (props: Omit<InputProps, FixedInputProps>) => {
+  const [isVisibilityOn, setIsVisibilityOn] = useState<boolean>(false);
+
+  const handleVisibility = () => {
+    setIsVisibilityOn((prev) => !prev);
+  };
+
+  return (
+    <Input
+      componentType="outlinedText"
+      type={isVisibilityOn ? "text" : "password"}
+      trailingNode={
+        <button
+          onClick={handleVisibility}
+          tabIndex={-1}
+          aria-label="visibility-control-button"
+          type="button"
+        >
+          {isVisibilityOn ? <VisibilityOffIcon /> : <VisibilityOnIcon />}
+        </button>
+      }
+      placeholder="비밀번호를 입력해 주세요"
+      {...props}
+    />
+  );
+};
