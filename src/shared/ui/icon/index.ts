9048f8948bcc57ccb5dd4a798fb890b514804693
yonghyeun <ttddcc119@naver.com>
2024-08-24 20:06:27 +0900

    feat[#32] : visibility icon 추가

--- src/shared/ui/icon/index.ts
+++ src/shared/ui/icon/index.ts
@@ -3,7 +3,11 @@ import withIcon from "./Icon";
 // svg 파일 Import
 import EmailSvg from "@/shared/assets/email.svg";
 import WardLeftSvg from "@/shared/assets/ward-left.svg";
+import VisibilityOnSvg from "@/shared/assets/visibility-on.svg";
+import VisibilityOffSvg from "@/shared/assets/visibility-off.svg";
 
 // Icon 컴포넌트 생성
 export const EmailIcon = withIcon(EmailSvg);
 export const WardLeftIcon = withIcon(WardLeftSvg);
+export const VisibilityOnIcon = withIcon(VisibilityOnSvg);
+export const VisibilityOffIcon = withIcon(VisibilityOffSvg);
