796358ae96d5fb726e7e4c5743ad8641a9f7cdc4
yonghyeun <ttddcc119@naver.com>
2024-08-25 01:41:01 +0900

    feat[#52] : BackwardNavigationBar onClick props 설정

--- src/widgets/navigationbar/ui/BackwardNavigationBar.tsx
+++ src/widgets/navigationbar/ui/BackwardNavigationBar.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/shared/ui/button";
 import { NavigationBar } from "@/shared/ui/navigationbar";
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const BackWardButton = (props: React.HTMLAttributes<HTMLButtonElement>) => {
   const navigate = useNavigate();
   const { onClick, ...rest } = props;
@@ -12,11 +13,18 @@ const BackWardButton = (props: React.HTMLAttributes<HTMLButtonElement>) => {
 const BackWardButton = () => {
   const navigate = useNavigate();
 >>>>>>> d5c9c2c (feat[#52] : BackwardNavigationBar 위젯 생성)
+=======
+const BackWardButton = (props: React.HTMLAttributes<HTMLButtonElement>) => {
+  const navigate = useNavigate();
+  const { onClick, ...rest } = props;
+
+>>>>>>> c8a9411 (feat[#52] : BackwardNavigationBar onClick props 설정)
   return (
     <Button
       size="large"
       variant="text"
       colorType="tertiary"
+<<<<<<< HEAD
 <<<<<<< HEAD
       onClick={onClick || (() => navigate(-1))}
       fullWidth={false}
@@ -24,6 +32,11 @@ const BackWardButton = () => {
 =======
       onClick={() => navigate(-1)}
 >>>>>>> d5c9c2c (feat[#52] : BackwardNavigationBar 위젯 생성)
+=======
+      onClick={onClick || (() => navigate(-1))}
+      fullWidth={false}
+      {...rest}
+>>>>>>> c8a9411 (feat[#52] : BackwardNavigationBar onClick props 설정)
     >
       <WardLeftIcon fill="#616161" />
     </Button>
