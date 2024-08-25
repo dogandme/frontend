1c11bc81d89cd548fc7159a1c2d44168aface470
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-08-25 15:00:24 +0900

    chore[#56] : React Query 도입 (#59)

---
+++ src/app/QueryClient.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default ReactQueryProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./global.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
+import ReactQueryProvider from "./app/QueryClient";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ReactQueryProvider>
+      <RouterProvider router={router} />
+    </ReactQueryProvider>
   </StrictMode>,
 );
