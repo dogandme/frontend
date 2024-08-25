c00ce15798e0814706a2eca1f1e4878cf607a6b8
yonghyeun <ttddcc119@naver.com>
2024-08-25 14:01:13 +0900

    fix[#55] : useDebounce 주석 수정

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 /**
  * useDebounce 훅은 입력된 콜백 함수를 지연시간만큼 지연시킨 후 실행합니다.
  * 첫 번째 인수인 콜백 함수는 디바운스 될 값을 반환해야 합니다.
- * 두 번째 인수인 defaultValue는 디바운스 초기값을 설정합니다.
+ * 두 번째 인수인 initialValue는 디바운스 되기 전 초기 값을 설정합니다.
  * 세 번째 인수인 delay는 디바운스 지연시간을 설정합니다.
  */
 export const useDebounce = <F extends (...args: unknown[]) => ReturnType<F>>(
