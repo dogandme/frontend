a800de80f7d8360b64b480e3bc68b2bf4bdf2c6b
yonghyeun <ttddcc119@naver.com>
2024-08-26 21:18:41 +0900

    feat[#62] : /login/email 페이지 생성

---
+++ src/pages/login/email/email.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from "@storybook/react";
+import MainLayout from "@/pages/layout";
+import LoginLayout from "../layout";
+import { Routes, Route } from "react-router-dom";
+
+import EmailLoginPage from "./page";
+
+const meta: Meta<typeof EmailLoginPage> = {
+  title: "Pages/Login/Email",
+  tags: ["pages, autodocs"],
+  component: EmailLoginPage,
+  decorators: [
+    (Story) => (
+      <div className="w-96 border border-grey-300">
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof EmailLoginPage>;
+
+export const Default: Story = {
+  render: () => (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route element={<LoginLayout />}>
+          <Route index element={<EmailLoginPage />} />
+        </Route>
+      </Route>
+    </Routes>
+  ),
+};
