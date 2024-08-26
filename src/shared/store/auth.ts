785b38b6e928af03008c159f81fec6f7a8201649
yonghyeun <ttddcc119@naver.com>
2024-08-26 12:12:43 +0900

    feat[#60] : useLoginProcessStore 추가
    - 경로가 중복으로 쌓이는 것을 막기 위해 Set 자료구조를 사용했습니다.

--- src/shared/store/auth.ts
+++ src/shared/store/auth.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-
+import { ROUTER_PATH } from "../constants";
 type AuthStore = {
   token: string;
   role: string;
@@ -17,4 +17,44 @@ export const useAuthStore = create<AuthStore>((set) => ({
   role: "",
   setToken: (token: string) => set({ token }),
   setRole: (role: string) => set({ role }),
+}));
+
+type LoginProcess = Pick<
+  typeof ROUTER_PATH,
+  | "LOGIN"
+  | "LOGIN_BY_EMAIL"
+  | "FORGET_PASSWORD"
+  | "SIGN_UP"
+  | "SIGN_UP_USER_INFO"
+  | "SIGN_UP_PET_INFO"
+>;
+
+type LoginProcessPaths = LoginProcess[keyof LoginProcess];
+
+interface LoginProcessStore {
+  process: Set<LoginProcessPaths>;
+  pushProcess: (process: LoginProcessPaths) => void;
+  popProcess: () => void;
+  clearProcess: () => void;
+}
+
+/**
+ * login , signup process를 저장하기 위한 store 입니다.
+ * 로그인 절차나 회원가입 절차가 완료된 경우 인증 과정을 유발시킨 페이지로 리다이렉션 시키기 위해 사용됩니다.
+ * 중복값을 제거하기 위해 Set 자료구조를 사용합니다.
+ */
+export const useLoginProcessStore = create<LoginProcessStore>((set) => ({
+  process: new Set(),
+  pushProcess: (newProcess: LoginProcessPaths) => {
+    set((state) => ({ process: new Set([...state.process, newProcess]) }));
+  },
+  popProcess: () => {
+    set((state) => {
+      if (state.process.size === 0) {
+        return state;
+      }
+      return { process: new Set([...state.process].slice(0, -1)) };
+    });
+  },
+  clearProcess: () => set({ process: new Set() }),
 }));
