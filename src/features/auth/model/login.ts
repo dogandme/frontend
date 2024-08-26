65261316325d179995e8659899027d3b49b65955
yonghyeun <ttddcc119@naver.com>
2024-08-26 22:57:43 +0900

    fix[#62] : useDebounce 롤백

--- src/features/auth/model/login.ts
+++ src/features/auth/model/login.ts
@@ -1,4 +1,5 @@
-import { useEffect, useState, useCallback } from "react";
+import { useState, useCallback } from "react";
+import { useDebounce } from "@/shared/lib";
 
 interface LoginForm {
   email: string;
@@ -13,7 +14,22 @@ export const useLoginForm = () => {
     persistLogin: false,
   });
 
-  const [formValidationResult, setFormValidationResult] = useState({
+  /* 이메일 유효성  검사 */
+  const checkEmailHasError = useCallback(() => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const emailHasError = !emailRegex.test(loginForm.email);
+    const emailIsEmpty = loginForm.email.length === 0;
+
+    return {
+      emailHasError,
+      emailIsEmpty,
+      emailStatusText: emailHasError
+        ? "올바른 이메일 형식으로 입력해 주세요"
+        : "",
+    };
+  }, [loginForm.email]);
+
+  const formValidationResult = useDebounce(checkEmailHasError, {
     emailHasError: false,
     emailIsEmpty: true,
     emailStatusText: "이메일 형식으로 입력해 주세요",
@@ -33,25 +49,6 @@ export const useLoginForm = () => {
       persistLogin: !loginForm.persistLogin,
     });
   };
-
-  /* 이메일 유효성  검사 */
-  const checkEmailHasError = useCallback(() => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const emailHasError = !emailRegex.test(loginForm.email);
-    const emailIsEmpty = loginForm.email.length === 0;
-
-    return {
-      emailHasError,
-      emailIsEmpty,
-      emailStatusText: emailHasError
-        ? "올바른 이메일 형식으로 입력해 주세요"
-        : "",
-    };
-  }, [loginForm.email]);
-
-  useEffect(() => {
-    setFormValidationResult(() => checkEmailHasError());
-  }, [loginForm.email, checkEmailHasError]);
 
   return {
     loginForm,
--- src/features/auth/ui/LoginForm.stories.tsx
+++ src/features/auth/ui/LoginForm.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 import LoginForm from "./LoginForm";
 import { within, userEvent, expect } from "@storybook/test";
+
 const meta: Meta<typeof LoginForm> = {
   title: "features/auth/LoginForm",
   component: LoginForm,
@@ -24,9 +25,13 @@ export const Default: StoryObj<typeof LoginForm> = {
     const $input = canvas.getByLabelText("이메일-input");
     const $p = await canvas.findByRole("status", { name: "status-text" });
 
-    const DEFAULT_STATUS_TEXT = "이메일 형식으로 입력해 주세요";
+    const DELTA = 100;
+    const DELAY = 500 + DELTA;
     const EMPTY_STATUS_TEXT = "이메일 형식으로 입력해 주세요";
     const ERROR_STATUS_TEXT = "올바른 이메일 형식으로 입력해 주세요";
+
+    const ERROR_TEXT_COLOR = "text-pink-500";
+    const DEFAULT_TEXT_COLOR = "text-grey-500";
 
     await step(
       "인풋 필드가 포커스 되어 있지 않는 경우에 p 태그는 돔에 존재해야 한다.",
@@ -47,30 +52,80 @@ export const Default: StoryObj<typeof LoginForm> = {
       "인풋 필드가 포커스 되면 p 태그의 문자는 이메일 형식으로 입력해 주세요 라는 안내 문구가 떠야 한다. ",
       () => {
         expect($p).toHaveTextContent(EMPTY_STATUS_TEXT);
+        expect($p).toHaveClass(DEFAULT_TEXT_COLOR);
       },
     );
 
-    await userEvent.type($input, "test");
     await step(
-      "이메일 유효성 검사를 통과하지 않는 문자가 나타나면 p 태그의 문자열은 올바른 이메일 형식으로 입력해 주세요 라는 안내 문구가 떠야 한다.",
-      () => {
+      "인풋 필드에서 입력이 되는 동안엔 유효성 검사가 일어나면 안된다.",
+      async () => {
+        await userEvent.type($input, "test");
+        expect($p).toHaveTextContent(EMPTY_STATUS_TEXT);
+        expect($p).toHaveClass(DEFAULT_TEXT_COLOR);
+      },
+    );
+
+    await step(
+      "인풋 필드에서 입력이 일어나고 DELAY 이후에 유효성 검사가 일어나야 한다.",
+      async () => {
+        await new Promise((resolve) => setTimeout(resolve, DELAY));
+        expect($p).toHaveTextContent(ERROR_STATUS_TEXT);
+        expect($p).toHaveClass(ERROR_TEXT_COLOR);
+      },
+    );
+
+    await step(
+      "인풋 필드에서 이메일 형식으로 입력이 되는 동안엔 유효성 검사가 일어나면 안된다.",
+      async () => {
+        await userEvent.type($input, "test123@naver.com");
+        expect($p).toHaveTextContent(ERROR_STATUS_TEXT);
+        expect($p).toHaveClass(ERROR_TEXT_COLOR);
+      },
+    );
+
+    await step(
+      "인풋 필드에서 이메일 형식으로 입력이 모두 끝나고 DELAY 이후에 유효성 검사가 일어나야 한다.",
+      async () => {
+        await new Promise((resolve) => setTimeout(resolve, DELAY));
+        expect($p).toHaveTextContent("");
+        expect($p).toHaveClass(DEFAULT_TEXT_COLOR);
+      },
+    );
+
+    await step(
+      "인풋 필드에서 유효한 이메일 형태에서 유효성을 만족하지 않는 문자열로 변경되는 동안에도 유효성 검사는 일어나면 안된다.",
+      async () => {
+        await userEvent.type(
+          $input,
+          "{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}",
+        );
+        expect($p).toHaveTextContent("");
+        expect($p).toHaveClass(DEFAULT_TEXT_COLOR);
+      },
+    );
+
+    await step(
+      "인풋 필드에서 유효한 이메일 형태에서 유효성을 만족하지 않는 문자열로 변경되고 DELAY 이후에 유효성 검사가 일어나야 한다.",
+      async () => {
+        await new Promise((resolve) => setTimeout(resolve, DELAY));
         expect($p).toHaveTextContent(ERROR_STATUS_TEXT);
       },
     );
 
-    await userEvent.type($input, "test123@naver.com");
     await step(
-      "이메일 유효성 검사를 통과하는 문자가 나타나면 p 태그의 문자열은 빈 문자열이 되어야 한다.",
-      () => {
-        expect($p).toHaveTextContent("");
+      "인풋 필드가 모두 지워지고 DELAY 만큼의 시간이 지나면 statusText는 다시 초기 상태의 값을 가져야 한다.",
+      async () => {
+        await userEvent.clear($input);
+        await new Promise((resolve) => setTimeout(resolve, DELAY));
+        expect($p).toHaveTextContent(EMPTY_STATUS_TEXT);
       },
     );
 
-    await userEvent.clear($input);
     await step(
-      "인풋 필드가 비어 있으면 p 태그의 문자열은 이메일 형식으로 입력해 주세요 라는 안내 문구가 떠야 한다.",
-      () => {
-        expect($p).toHaveTextContent(DEFAULT_STATUS_TEXT);
+      "인풋 필드가 포커스 되어 있지 않는 경우에 DELAY 여부와 상관 없이 p 태그는 문자를 가지면 안된다.",
+      async () => {
+        await userEvent.tab();
+        expect($p).toHaveTextContent("");
       },
     );
   },
