d1e9d3e267cfd580484c41f1132a59484dd0d60c
yonghyeun <ttddcc119@naver.com>
2024-08-26 20:08:49 +0900

    fix[#62] : useDebounce 정규성 검사에서 사용하지 않기

--- src/features/auth/model/login.ts
+++ src/features/auth/model/login.ts
@@ -1,12 +1,17 @@
-import { useState } from "react";
-import { useDebounce } from "@/shared/lib";
+import { useEffect, useState } from "react";
 
 export const useLogin = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [errorObject, setErrorObject] = useState<{
+    isError: boolean;
+    statusText: string;
+  }>({ isError: false, statusText: "이메일 형식으로 입력해 주세요" });
+
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
+
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
@@ -20,33 +25,27 @@ export const useLogin = () => {
         statusText: "이메일 형식으로 입력해 주세요",
       };
     }
+    // 유효성 검증 정규식
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const isError = !emailRegex.test(email);
-    // 이메일 형식에 맞지 않을 경우
-    if (isError) {
-      return {
-        isError,
-        statusText: "올바른 이메일 형식으로 입력해 주세요",
-      };
-    }
-    // 이메일 형식이 맞을 경우
+
     return {
-      isError: false,
-      statusText: "",
+      isError,
+      statusText: isError ? "올바른 이메일 형식으로 입력해 주세요" : "",
     };
   };
 
-  const { isError, statusText } = useDebounce(checkIsErrorEmail, {
-    isError: false,
-    statusText: "이메일 형식으로 입력해 주세요",
-  });
+  /* email 정규성 검 */
+  useEffect(() => {
+    setErrorObject(() => checkIsErrorEmail());
+  }, [email]);
 
   return {
     email,
     handleEmailChange,
     password,
     handlePasswordChange,
-    emailHasError: isError,
-    emailStatusText: statusText,
+    emailHasError: errorObject.isError,
+    emailStatusText: errorObject.statusText,
   };
 };
