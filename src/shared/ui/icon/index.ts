d8f491346fd0f144aae233cf033200ec84a23171
yonghyeun <ttddcc119@naver.com>
2024-08-27 11:01:54 +0900

    feat[#71] : CloseNavigationBar 생성

--- src/shared/ui/icon/index.ts
+++ src/shared/ui/icon/index.ts
@@ -5,8 +5,10 @@ import EmailSvg from "@/shared/assets/email.svg";
 import VisibilityOnSvg from "@/shared/assets/visibility-on.svg";
 import VisibilityOffSvg from "@/shared/assets/visibility-off.svg";
 import WardLeftSvg from "@/shared/assets/ward-left.svg";
+import CloseSvg from "@/shared/assets/close.svg";
 // Icon 컴포넌트 생성
 export const EmailIcon = withIcon(EmailSvg);
 export const VisibilityOnIcon = withIcon(VisibilityOnSvg);
 export const VisibilityOffIcon = withIcon(VisibilityOffSvg);
 export const WardLeftIcon = withIcon(WardLeftSvg);
+export const CloseIcon = withIcon(CloseSvg);
