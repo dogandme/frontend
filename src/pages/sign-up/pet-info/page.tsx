5cca2f08682e1282f8d83dd5401c28eca1341503
yonghyeun <ttddcc119@naver.com>
2024-08-27 15:38:49 +0900

    feat[#71] : signup/pet-info 페이지 생성

---
+++ src/pages/sign-up/pet-info/page.tsx
@@ -0,0 +1,27 @@
+import { CloseNavigationBar } from "@/widgets/navigationbar/ui";
+import * as PetInfoForm from "@/features/auth/ui";
+
+const PetInfoPage = () => {
+  return (
+    <div>
+      <header>
+        <CloseNavigationBar />
+      </header>
+      <main className="flex flex-col items-center gap-4 self-stretch px-4 pb-32 pt-8">
+        <h1 className="headline-3 overflow-ellipsis text-center text-grey-900">
+          우리 댕댕이를 소개해 주세요
+        </h1>
+        <PetInfoForm.Form>
+          <PetInfoForm.ProfileInput />
+          <PetInfoForm.NameInput />
+          <PetInfoForm.GreedInput />
+          <PetInfoForm.CharacterInput />
+          <PetInfoForm.IntroduceTextArea />
+          <PetInfoForm.Submit />
+        </PetInfoForm.Form>
+      </main>
+    </div>
+  );
+};
+
+export default PetInfoPage;
