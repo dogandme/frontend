8dc19411d13461ff6b75c28e1ab4419ac2af81dd
yonghyeun <ttddcc119@naver.com>
2024-08-27 15:37:23 +0900

    feat[#71] : edit icon 추가

--- src/shared/ui/icon/index.ts
+++ src/shared/ui/icon/index.ts
@@ -6,9 +6,11 @@ import VisibilityOnSvg from "@/shared/assets/visibility-on.svg";
 import VisibilityOffSvg from "@/shared/assets/visibility-off.svg";
 import WardLeftSvg from "@/shared/assets/ward-left.svg";
 import CloseSvg from "@/shared/assets/close.svg";
+import EditSvg from "@/shared/assets/edit.svg";
 // Icon 컴포넌트 생성
 export const EmailIcon = withIcon(EmailSvg);
 export const VisibilityOnIcon = withIcon(VisibilityOnSvg);
 export const VisibilityOffIcon = withIcon(VisibilityOffSvg);
 export const WardLeftIcon = withIcon(WardLeftSvg);
 export const CloseIcon = withIcon(CloseSvg);
+export const EditIcon = withIcon(EditSvg);
