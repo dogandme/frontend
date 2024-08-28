99cd62c725b68f37812a0abe211521d7cbe02485
yonghyeun <ttddcc119@naver.com>
2024-08-28 09:37:38 +0900

    fix[#71] : 스토리북 docs 추가

--- src/shared/ui/chip/SelectChip.stories.tsx
+++ src/shared/ui/chip/SelectChip.stories.tsx
@@ -6,18 +6,26 @@ const meta: Meta<typeof SelectChip> = {
   title: "shared/SelectChip",
   tags: ["autodocs"],
   component: SelectChip,
+
   parameters: {
-    description: {
-      component:
-        "선택 가능한 SelectChip 컴포넌트입니다. 해당 컴포넌트는 클릭 On , Off와 유사한 상태를 가집니다. ",
+    docs: {
+      description: {
+        component:
+          "선택 가능한 SelectChip 컴포넌트입니다. 해당 컴포넌트는 클릭 On , Off와 유사한 상태를 가집니다. ",
+      },
     },
   },
+
   argTypes: {
     label: {
       description: "Chip에 표시될 라벨입니다.",
       control: {
         type: "text",
       },
+    },
+    onClick: {
+      description:
+        "버튼에 달리는 온클릭 이벤트 핸들러입니다. props로 새로운 온클릭 이벤트 핸들러가 등록 될 때 등록된 이벤트 핸들러를 실행하고 내부에 존재하는 핸들러를 추가로 실행합니다.",
     },
   },
 };
