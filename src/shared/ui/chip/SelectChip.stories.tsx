1732f20e6ffc5a9e97a6876e6ee528f8fde78a54
yonghyeun <ttddcc119@naver.com>
2024-08-28 14:20:44 +0900

    refactor[#71] : SelectChip 제어,비제어 컴포넌트로 확장

--- src/shared/ui/chip/SelectChip.stories.tsx
+++ src/shared/ui/chip/SelectChip.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { SelectChip } from "./SelectChip";
 import { expect, userEvent, within } from "@storybook/test";
+import { useState } from "react";
 
 const meta: Meta<typeof SelectChip> = {
   title: "shared/SelectChip",
@@ -11,7 +12,7 @@ const meta: Meta<typeof SelectChip> = {
     docs: {
       description: {
         component:
-          "선택 가능한 SelectChip 컴포넌트입니다. 해당 컴포넌트는 클릭 On , Off와 유사한 상태를 가집니다. ",
+          "선택 가능한 SelectChip 컴포넌트입니다. 해당 컴포넌트는 클릭 On , Off와 유사한 상태를 가집니다. 비제어 컴포넌트와 제어 컴포넌트 모두로 사용 가능하며 제어 컴포넌트의 경우 isControlledIsSelected props와 상태 핸들러를 onClick에 전달해주면 됩니다.",
       },
     },
   },
@@ -23,6 +24,13 @@ const meta: Meta<typeof SelectChip> = {
         type: "text",
       },
     },
+    controlledIsSelected: {
+      description:
+        "제어 컴포넌트의 경우 선택된 상태를 결정하는 props입니다. 해당 props가 존재할 경우 내부에서 상태를 업데이트 하지 않습니다.",
+      control: {
+        type: "boolean",
+      },
+    },
     onClick: {
       description:
         "버튼에 달리는 온클릭 이벤트 핸들러입니다. props로 새로운 온클릭 이벤트 핸들러가 등록 될 때 등록된 이벤트 핸들러를 실행하고 내부에 존재하는 핸들러를 추가로 실행합니다.",
@@ -32,14 +40,31 @@ const meta: Meta<typeof SelectChip> = {
 
 export default meta;
 
-export const Default: StoryObj<typeof SelectChip> = {
-  render: () => (
-    <div className="flex w-fit gap-2 border border-grey-300 px-2 py-2">
-      <SelectChip label="Chip" aria-label="on" />
-      <SelectChip label="Chip" aria-label="off" />
-    </div>
-  ),
+const DefaultComponent = () => {
+  const [controlledIsSelected, setControlledIsSelected] =
+    useState<boolean>(false);
+  const handler = () => {
+    setControlledIsSelected((prev) => !prev);
+  };
 
+  return (
+    <div>
+      <p>부모 컴포넌트의 상태 : {String(controlledIsSelected)}</p>
+      <div className="flex w-fit gap-2 border border-grey-300 px-2 py-2">
+        <SelectChip label="UnControlled Chip" aria-label="on" />
+        <SelectChip
+          label="Controlled Chip"
+          controlledIsSelected={controlledIsSelected}
+          onClick={handler}
+          aria-label="off"
+        />
+      </div>
+    </div>
+  );
+};
+
+export const Default: StoryObj<typeof SelectChip> = {
+  render: () => <DefaultComponent />,
   play: ({ canvasElement }) => {
     const canvas = within(canvasElement);
     const $on = canvas.getByLabelText("on");
--- src/shared/ui/chip/SelectChip.tsx
+++ src/shared/ui/chip/SelectChip.tsx
@@ -3,18 +3,39 @@ import { selectChipStyles } from "./chip.styles";
 
 interface SelectChipProps extends React.HTMLAttributes<HTMLButtonElement> {
   label: string;
+  controlledIsSelected?: boolean;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const SelectChip = ({ onClick, label, ...rest }: SelectChipProps) => {
-  const [isSelected, setIsSelected] = useState<boolean>(false);
-  const chipState = isSelected ? "selected" : "unselected";
+export const SelectChip = ({
+  controlledIsSelected,
+  onClick,
+  label,
+  ...rest
+}: SelectChipProps) => {
+  // 제어 컴포넌트의 경우에는 unControlledIsSelected를 사용하지 않음을 명확히 하기 위해 상태를 null로 초기화 합니다.
+  const [unControlledIsSelected, setIsUnControlledIsSelected] = useState<
+    boolean | null
+  >(() => controlledIsSelected || null);
 
+  // 제어 컴포넌트와 비제어컴포넌트의 경우를 구분하여 내부에서 스타일에 사용할 state인 chipState를 결정합니다.
+  const isUncontrolled = typeof controlledIsSelected === "undefined";
+  const chipState = isUncontrolled
+    ? unControlledIsSelected
+      ? "selected"
+      : "unselected"
+    : controlledIsSelected
+      ? "selected"
+      : "unselected";
+
+  // 비제어 컴포넌트의 경우엔 내부의 상태를 업데이트하고 제어 컴포넌트의 경우에는 props로 받은 onClick을 호출합니다.
+  // 이를 통해 제어 컴포넌트의 경우 부모 컴포넌트나 스토어의 상태를 업데이트 할 수 있습니다.
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (onClick) {
-      onClick(e);
+    if (isUncontrolled) {
+      setIsUnControlledIsSelected((prev) => !prev);
+      return;
     }
-    setIsSelected((prev: boolean) => !prev);
+    onClick?.(e);
   };
 
   return (
