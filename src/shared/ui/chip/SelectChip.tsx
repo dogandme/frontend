774a85e6ef1610b324c9da8badb59e0b4a50bacf
yonghyeun <ttddcc119@naver.com>
2024-08-28 14:56:16 +0900

    refactor[#71] : SelectChip 삼항 연산자 수정

--- src/shared/ui/chip/SelectChip.tsx
+++ src/shared/ui/chip/SelectChip.tsx
@@ -19,14 +19,11 @@ export const SelectChip = ({
   const isUncontrolled = typeof controlledIsSelected === "undefined";
 
   // 제어 컴포넌트와 비제어컴포넌트의 경우를 구분하여 내부에서 스타일에 사용할 state인 chipState를 결정합니다.
-  const chipState = isUncontrolled
+  const isSelected = isUncontrolled
     ? unControlledIsSelected
-      ? "selected"
-      : "unselected"
-    : controlledIsSelected
-      ? "selected"
-      : "unselected";
+    : controlledIsSelected;
 
+  const chipState = isSelected ? "selected" : "unselected";
   // 비제어 컴포넌트의 경우엔 내부의 상태를 업데이트하고 제어 컴포넌트의 경우에는 props로 받은 onClick을 호출합니다.
   // 이를 통해 제어 컴포넌트의 경우 부모 컴포넌트나 스토어의 상태를 업데이트 할 수 있습니다.
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
