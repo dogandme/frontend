41855c9343e1ee7afe5ce37840a7a856ea7801dd
aken-you <kinsk2839@naver.com>
2024-08-28 17:48:45 +0900

    feat[#23] : 이메일 회원가입 form에 submit handler 추가

--- src/features/auth/ui/SignUpByEmailForm.tsx
+++ src/features/auth/ui/SignUpByEmailForm.tsx
@@ -3,10 +3,16 @@ import { EmailInput, PasswordInput } from "@/entities/auth/ui";
 import { Button } from "@/shared/ui/button";
 import { Input } from "@/shared/ui/input";
 import { useSignUpByEmailForm } from "../model";
+import { usePostSignUpByEmail } from "../api";
+import { useAuthStore } from "@/shared/store/auth";
 
 const SignUpByEmailForm = () => {
   const [isFocusedEmailInput, setIsFocusedEmailInput] =
     useState<boolean>(false);
+
+  const { mutate: postSignUpByEmail } = usePostSignUpByEmail();
+  const setToken = useAuthStore((state) => state.setToken);
+  const setRole = useAuthStore((state) => state.setRole);
 
   const {
     form,
@@ -35,8 +41,36 @@ const SignUpByEmailForm = () => {
     else passwordConfirmStatusText = "비밀번호가 서로 일치하지 않습니다";
   }
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const canSignUp = isValidEmail && isValidPassword && isPasswordMatched;
+
+    if (!canSignUp) {
+      // todo: snackbar 띄우기
+      alert("이메일과 비밀번호를 모두 입력해 주세요");
+      return;
+    }
+
+    postSignUpByEmail(
+      { email, password },
+      {
+        onSuccess: ({ content }) => {
+          const { authorization, role } = content;
+
+          setToken(authorization);
+          setRole(role);
+        },
+        onError: (error) => {
+          // todo: snackbar 띄우기
+          alert(error.message);
+        },
+      },
+    );
+  };
+
   return (
-    <form className="flex flex-col gap-8 self-stretch">
+    <form className="flex flex-col gap-8 self-stretch" onSubmit={handleSubmit}>
       <div className="flex flex-col gap-2">
         <div>
           <div className="flex items-end justify-between gap-2">
