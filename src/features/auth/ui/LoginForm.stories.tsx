4676e2b44df1256ee634ad79de43e0563347ad1e
yonghyeun <ttddcc119@naver.com>
2024-08-30 16:14:42 +0900

    feat[#86] : API 가 정상적으로 응답받은 경우 스토어에 저장되는지 확인하는 테스트 코드 작성

--- src/features/auth/ui/LoginForm.stories.tsx
+++ src/features/auth/ui/LoginForm.stories.tsx
@@ -1,13 +1,10 @@
+import { http, HttpResponse } from "msw";
 import { Meta, StoryObj } from "@storybook/react";
 import { LoginForm } from ".";
 import { within, userEvent, expect } from "@storybook/test";
+import { useAuthStore } from "@/shared/store/auth";
 const meta: Meta<typeof LoginForm> = {
   title: "features/auth/LoginForm",
-};
-
-export default meta;
-
-export const Default: StoryObj<typeof LoginForm> = {
   decorators: [
     (Story) => (
       <div className="w-96 border border-grey-300 px-2 py-2">
@@ -15,7 +12,11 @@ export const Default: StoryObj<typeof LoginForm> = {
       </div>
     ),
   ],
+};
 
+export default meta;
+
+export const Default: StoryObj<typeof LoginForm> = {
   render: () => (
     <LoginForm.Form>
       <LoginForm.Email />
@@ -95,5 +96,88 @@ export const Default: StoryObj<typeof LoginForm> = {
       expect(alertCalled).toBe(true);
     });
     window.alert = originalAlert;
+  },
+};
+
+const ApiTestComponent = () => {
+  const { token, role, nickname, userId } = useAuthStore((state) => state);
+  return (
+    <>
+      <LoginForm.Form>
+        <LoginForm.Email />
+        <LoginForm.Password />
+        <LoginForm.PersistLogin />
+        <LoginForm.SubmitButton />
+      </LoginForm.Form>
+      <p data-testid="token">{token}</p>
+      <p data-testid="role">{role}</p>
+      <p data-testid="nickname">{nickname}</p>
+      <p data-testid="userid">{userId}</p>
+    </>
+  );
+};
+
+export const APITest: StoryObj<typeof LoginForm> = {
+  decorators: (Story) => {
+    // 스토리 시작 전 스토어 초기화
+    useAuthStore.setState({
+      token: null,
+      role: null,
+      nickname: null,
+      userId: null,
+    });
+    return <Story />;
+  },
+  render: () => <ApiTestComponent />,
+  parameters: {
+    msw: {
+      handlers: [
+        http.post("http://localhost/login", () => {
+          return HttpResponse.json({
+            code: 200,
+            message: "success",
+            content: {
+              authorization: "Bearer token",
+              role: "USER_USER",
+              nickname: "뽀송이",
+              userId: 1234,
+            },
+          });
+        }),
+      ],
+    },
+  },
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+    const $input = canvas.getByLabelText("이메일-input");
+    const $password = canvas.getByLabelText("비밀번호-input");
+    const $submit = await canvas.getByRole("submit", {
+      name: "login-submit-button",
+    });
+
+    const $token = canvas.getByTestId("token");
+    const $role = canvas.getByTestId("role");
+    const $nickname = canvas.getByTestId("nickname");
+    const $userId = canvas.getByTestId("userid");
+
+    await step("API 요청이 일어나기 전까지 토큰은 존재하지 않는다.", () => {
+      expect($token).toHaveTextContent("");
+      expect($role).toHaveTextContent("");
+      expect($nickname).toHaveTextContent("");
+      expect($userId).toHaveTextContent("");
+    });
+
+    await userEvent.type($input, "abcd123@naver.com");
+    await userEvent.type($password, "password");
+    await userEvent.click($submit);
+
+    await step("API 요청이 일어난 후에는 토큰에 값이 존재한다.", async () => {
+      // 목업된 API 데이터를 받기 위한 딜레이 설정
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      expect($token).toHaveTextContent("Bearer token");
+      expect($role).toHaveTextContent("USER_USER");
+      expect($nickname).toHaveTextContent("뽀송이");
+      expect($userId).toHaveTextContent("1234");
+    });
   },
 };
