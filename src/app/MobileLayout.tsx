6e20217f3966fe17f2e0c36dbca4a4d20017aba9
yonghyeun <ttddcc119@naver.com>
2024-09-02 23:32:01 +0900

    fix[#96] : max-width 원상 복귀

--- src/app/MobileLayout.tsx
+++ src/app/MobileLayout.tsx
@@ -1,3 +1,7 @@
 export const MobileLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="mx-auto my-0 flex h-screen flex-col">{children}</div>;
+  return (
+    <div className="mx-auto my-0 flex h-screen max-w-[37.5rem] flex-col">
+      {children}
+    </div>
+  );
 };
