5cf53cea3f09917be8d9181eb89dd3149bb476e7
yonghyeun <ttddcc119@naver.com>
2024-09-02 22:09:44 +0900

    feat[#96] : Mobile , MainLayout 높이 뷰포트 설정
    - 모바일 레이아웃의 경우 뷰포트 모두를 채우고 MainLayout 의 경우 상단 헤더와 하단 네비게이션 바를 제외한 메인 바가 모두 채우도록 설정

--- src/app/MobileLayout.tsx
+++ src/app/MobileLayout.tsx
@@ -1,7 +1,3 @@
 export const MobileLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="mx-auto my-0 flex h-screen min-h-56 w-full min-w-80 max-w-[37.5rem] flex-col">
-      {children}
-    </div>
-  );
+  return <div className="mx-auto my-0 flex h-screen flex-col">{children}</div>;
 };
