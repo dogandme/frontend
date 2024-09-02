2fe14ff3748609e85c2140a483afa7b6e481b4f1
yonghyeun <ttddcc119@naver.com>
2024-09-02 22:09:44 +0900

    feat[#96] : Mobile , MainLayout 높이 뷰포트 설정
    - 모바일 레이아웃의 경우 뷰포트 모두를 채우고 MainLayout 의 경우 상단 헤더와 하단 네비게이션 바를 제외한 메인 바가 모두 채우도록 설정

--- src/app/MobileLayout.tsx
+++ src/app/MobileLayout.tsx
@@ -1,6 +1,6 @@
 export const MobileLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto my-0 min-h-[3.25rem] w-full min-w-80 max-w-[37.5rem]">
+    <div className="mx-auto my-0 flex h-screen min-h-56 w-full min-w-80 max-w-[37.5rem] flex-col">
       {children}
     </div>
   );
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,12 +1,18 @@
-import { GlobalNavigationBar } from "@/widgets/navigationbar/ui/GlobalNavigationBar";
+import { MainFooter } from "@/widgets/footer/ui";
+import { GlobalNavigationBar } from "@/widgets/navigationbar/ui";
 import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
     // TODO 레이아웃 범위 디자이너와 상의 후 픽스하기
-    <div>
+    <>
       <GlobalNavigationBar />
-      <Outlet />
-    </div>
+      <div className="flex grow flex-col">
+        <Outlet />
+      </div>
+      {/* 해당 부분은 바텀 시트가 나오게 되면 삭제 될 부분 */}
+      <div className="rounded-t-[28px] px-4 py-4"></div>
+      <MainFooter />
+    </>
   );
 };
 
