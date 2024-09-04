493f050aa0635568ea96faef3d6364b6d916563b
yonghyeun <ttddcc119@naver.com>
2024-09-05 00:55:27 +0900

    refactor[#106] : useOverlay 위치 store -> lib 으로 변경

--- src/shared/lib/overlay.ts
+++ src/shared/lib/overlay.ts
@@ -1,36 +1,5 @@
-import { create } from "zustand";
 import { useState } from "react";
-
-export interface OverlayOptions {
-  disabledInteraction?: boolean;
-}
-
-export interface OverlayInfo {
-  id: number;
-  component: JSX.Element;
-  handleClose: () => void;
-  options: OverlayOptions;
-}
-
-interface OverlayStore {
-  overlays: OverlayInfo[];
-  addOverlays: (newOverlay: OverlayInfo) => void;
-  removeOverlay: (id: number) => void;
-}
-
-export const useOverlayStore = create<OverlayStore>((set) => ({
-  overlays: [],
-
-  addOverlays: (newOverlay: OverlayInfo) =>
-    set(({ overlays }) => ({
-      overlays: [...overlays, newOverlay],
-    })),
-
-  removeOverlay: (id: OverlayInfo["id"]) =>
-    set(({ overlays }) => ({
-      overlays: overlays.filter((overlay) => overlay.id !== id),
-    })),
-}));
+import { OverlayOptions, useOverlayStore } from "../store/overlay";
 
 export const useOverlay = (
   createOverlayComponent: (handleClose: () => void) => JSX.Element,
