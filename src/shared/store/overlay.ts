8e9e2dc2744374b1ced8cc174b031d525c4cdbed
yonghyeun <ttddcc119@naver.com>
2024-09-05 00:55:27 +0900

    refactor[#106] : useOverlay 위치 store -> lib 으로 변경

--- src/shared/store/overlay.ts
+++ src/shared/store/overlay.ts
@@ -31,31 +31,3 @@ export const useOverlayStore = create<OverlayStore>((set) => ({
       overlays: overlays.filter((overlay) => overlay.id !== id),
     })),
 }));
-
-export const useOverlay = (
-  createOverlayComponent: (handleClose: () => void) => JSX.Element,
-  options: OverlayOptions = { disabledInteraction: true },
-) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const id = new Date().getTime(); // 커스텀 훅이 호출 될 때 마다 유일한 id를 생성합니다.
-
-  const addOverlays = useOverlayStore((state) => state.addOverlays);
-  const removeOverlay = useOverlayStore((state) => state.removeOverlay);
-
-  const handleClose = () => {
-    removeOverlay(id);
-    setIsOpen(false);
-  };
-  const handleOpen = () => {
-    addOverlays({
-      id,
-      component: createOverlayComponent(handleClose),
-      handleClose,
-      options,
-    });
-    setIsOpen(true);
-  };
-
-  return { handleOpen, handleClose, isOpen };
-};
