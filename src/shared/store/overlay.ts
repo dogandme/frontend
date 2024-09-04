f5c0a62f05cff4c81fd4b1bf5feafaf15e5c66dc
yonghyeun <ttddcc119@naver.com>
2024-09-04 22:51:52 +0900

    feat[#106] : useOverlay , useOverlayStore 생성

---
+++ src/shared/store/overlay.ts
@@ -0,0 +1,61 @@
+import { create } from "zustand";
+import { useState } from "react";
+
+interface OverlayOptions {
+  disabledInteraction?: boolean;
+}
+
+export interface OverlayInfo {
+  id: number;
+  component: JSX.Element;
+  handleClose: () => void;
+  options: OverlayOptions;
+}
+
+interface OverlayStore {
+  overlays: OverlayInfo[];
+  addOverlays: (newOverlay: OverlayInfo) => void;
+  removeOverlay: (id: number) => void;
+}
+
+export const useOverlayStore = create<OverlayStore>((set) => ({
+  overlays: [],
+
+  addOverlays: (overlay: OverlayInfo) =>
+    set((newOverlay) => ({
+      overlays: [...newOverlay.overlays, overlay],
+    })),
+
+  removeOverlay: (id: OverlayInfo["id"]) =>
+    set(({ overlays }) => ({
+      overlays: overlays.filter((overlay) => overlay.id !== id),
+    })),
+}));
+
+export const useOverlay = (
+  createOverlayComponent: (handleClose: () => void) => JSX.Element,
+  options: OverlayOptions = { disabledInteraction: true },
+) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const id = new Date().getTime(); // 커스텀 훅이 호출 될 때 마다 유일한 id를 생성합니다.
+
+  const addOverlays = useOverlayStore((state) => state.addOverlays);
+  const removeOverlay = useOverlayStore((state) => state.removeOverlay);
+
+  const handleClose = () => {
+    removeOverlay(id);
+    setIsOpen(false);
+  };
+  const handleOpen = () => {
+    addOverlays({
+      id,
+      component: createOverlayComponent(handleClose),
+      handleClose,
+      options,
+    });
+    setIsOpen(true);
+  };
+
+  return { handleOpen, handleClose, isOpen };
+};
