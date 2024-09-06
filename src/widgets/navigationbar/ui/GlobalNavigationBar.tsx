d6e281ff129127721ebd1f54b9d4caea6dd6bacd
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-09-07 00:52:59 +0900

    [Feat] Google 정책에 따라 기본 로고 , 저작권 기본 문구 없애고 커스텀 로고 및 문구 작성 (#112)
    * fix[#111] : 마킹하기 버튼 바텀시트와 간격 16px -> 24px 로 수정
    
    * fix[#111] : 바텀시트 위치 page -> layout 으로 변경
    
    * feat[#111] : 커스텀 구글 로고와 저작권 문구 표시
    
    * fix[#111] : widgets/footer -> map 으로 네이밍 변경
    
    * feat[#111] : GoogleMapsCopyRight 컴포넌트 생성
    
    * feat[#111] : Google Maps 기본 로고 , 저작권 제거하기
    
    * fix[#111] : copyright 특수 기호 추가
    
    - 문구 위치 items-center -> items-end 로 변경
    
    * feat[#111] : anchor tag에 noreferrer, noopener 추가
    
    * fix[#111] : UI 구조 깨지는 문제 해결
    
    - relative 태그를 적절한 곳에 배치
    - 바텀시트 위치 MainFooter 에 넣기
    
    * feat[#111] : SEO 위해 href 에 구글 맵스 값 추가
    
    * feat[#111] : center.toString -> center.toUrlValue 메소드 변경

--- src/widgets/navigationbar/ui/GlobalNavigationBar.tsx
+++ src/widgets/navigationbar/ui/GlobalNavigationBar.tsx
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 
 export const GlobalNavigationBar = () => {
   return (
-    <nav className="bg-tangerine-500">
-      <Link to="/">
-        <ul className="flex min-w-[280px] items-center justify-between self-stretch px-1 py-2">
-          <li>
-            <h1 className="title-1 px-4 text-grey-0">MUNGWITHME</h1>
-          </li>
-          {/* TODO 기능이 존재하는 알림 버튼으로 만들기 */}
-          <button className="flex items-center justify-center px-3 py-3">
-            <NotificationIcon />
-          </button>
-        </ul>
-      </Link>
-    </nav>
+    <header>
+      <nav className="bg-tangerine-500">
+        <Link to="/">
+          <ul className="flex min-w-[280px] items-center justify-between self-stretch px-1 py-2">
+            <li>
+              <h1 className="title-1 px-4 text-grey-0">MUNGWITHME</h1>
+            </li>
+            {/* TODO 기능이 존재하는 알림 버튼으로 만들기 */}
+            <button className="flex items-center justify-center px-3 py-3">
+              <NotificationIcon />
+            </button>
+          </ul>
+        </Link>
+      </nav>
+    </header>
   );
 };
