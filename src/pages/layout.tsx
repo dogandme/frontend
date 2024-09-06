d2d4cd27b35b341ff3b15a5c0e65d6d08c7215c8
yonghyeun <ttddcc119@naver.com>
2024-09-06 23:35:34 +0900

    fix[#111] : UI 구조 깨지는 문제 해결
    - relative 태그를 적절한 곳에 배치
    - 바텀시트 위치 MainFooter 에 넣기

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -6,13 +6,9 @@ const MainLayout = () => {
     // TODO 레이아웃 범위 디자이너와 상의 후 픽스하기
     <>
       <GlobalNavigationBar />
-      <section className="flex grow flex-col overflow-y-scroll">
+      <main className="flex grow flex-col overflow-y-scroll">
         <Outlet />
-      </section>
-      {/* TODO 바텀시트 나오면 바텀시트로 대체하기 
-        TODO 저작권 정책 아이콘 어디에 둘 지 생각해보기
-      */}
-      <div className="z-999 absolute bottom-[5rem] w-full rounded-t-[28px] bg-grey-0 px-4 py-4"></div>
+      </main>
       <MainFooter />
     </>
   );
--- src/widgets/map/ui/MainFooter.tsx
+++ src/widgets/map/ui/MainFooter.tsx
@@ -6,53 +6,56 @@ export const MainFooter = () => {
   const { active, inactive, base } = mainFooterStyles;
 
   return (
-    <nav>
-      <ul className="flex justify-between gap-2 bg-grey-0 px-2 pb-[34px]">
-        <li className="grow">
-          <NavLink
-            to={ROUTER_PATH.SEARCH}
-            className={({ isActive }) =>
-              `${isActive ? active : inactive} ${base}`
-            }
-          >
-            <CompassIcon />
-            발견
-          </NavLink>
-        </li>
-        <li className="grow">
-          <NavLink
-            to={ROUTER_PATH.MAP}
-            className={({ isActive }) =>
-              `${isActive ? active : inactive} ${base}`
-            }
-          >
-            <MapIcon />
-            지도
-          </NavLink>
-        </li>
-        <li className="grow">
-          <NavLink
-            to={ROUTER_PATH.COMMUNITY}
-            className={({ isActive }) =>
-              `${isActive ? active : inactive} ${base}`
-            }
-          >
-            <CommunityIcon />
-            커뮤니티
-          </NavLink>
-        </li>
-        <li className="grow">
-          <NavLink
-            to={ROUTER_PATH.MY_PAGE}
-            className={({ isActive }) =>
-              `${isActive ? active : inactive} ${base}`
-            }
-          >
-            <CommunityIcon />
-            기록
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <footer className="relative">
+      <div className="z-999 absolute -top-[2rem] w-full rounded-t-[28px] bg-grey-0 px-4 py-4"></div>
+      <nav>
+        <ul className="flex justify-between gap-2 bg-grey-0 px-2 pb-[34px]">
+          <li className="grow">
+            <NavLink
+              to={ROUTER_PATH.SEARCH}
+              className={({ isActive }) =>
+                `${isActive ? active : inactive} ${base}`
+              }
+            >
+              <CompassIcon />
+              발견
+            </NavLink>
+          </li>
+          <li className="grow">
+            <NavLink
+              to={ROUTER_PATH.MAP}
+              className={({ isActive }) =>
+                `${isActive ? active : inactive} ${base}`
+              }
+            >
+              <MapIcon />
+              지도
+            </NavLink>
+          </li>
+          <li className="grow">
+            <NavLink
+              to={ROUTER_PATH.COMMUNITY}
+              className={({ isActive }) =>
+                `${isActive ? active : inactive} ${base}`
+              }
+            >
+              <CommunityIcon />
+              커뮤니티
+            </NavLink>
+          </li>
+          <li className="grow">
+            <NavLink
+              to={ROUTER_PATH.MY_PAGE}
+              className={({ isActive }) =>
+                `${isActive ? active : inactive} ${base}`
+              }
+            >
+              <CommunityIcon />
+              기록
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </footer>
   );
 };
