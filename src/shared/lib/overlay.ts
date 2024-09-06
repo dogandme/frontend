df0e5c5062f301664105ab5d592f8a9874ae12bf
yonghyeun <ttddcc119@naver.com>
2024-09-06 20:40:32 +0900

    fix[#106] : disableInteraction 기본값 설정 위치 변경

--- src/shared/lib/overlay.ts
+++ src/shared/lib/overlay.ts
@@ -3,10 +3,10 @@ import { OverlayOptions, useOverlayStore } from "../store/overlay";
 
 export const useOverlay = (
   createOverlayComponent: (handleClose: () => Promise<void>) => JSX.Element,
-  options: OverlayOptions = { disableInteraction: true },
+  options: OverlayOptions = {},
 ) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const { disableInteraction, beforeClose, afterClose } = options;
+  const { disableInteraction = true, beforeClose, afterClose } = options;
   const id = new Date().getTime(); // 커스텀 훅이 호출 될 때 마다 유일한 id를 생성합니다.
 
   const addOverlay = useOverlayStore((state) => state.addOverlay);
