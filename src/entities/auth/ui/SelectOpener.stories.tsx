881ee09d117b88ca3f27c7c012044def312bf732
aken-you <kinsk2839@naver.com>
2024-09-07 18:06:12 +0900

    refactor[#66] : auth에서 사용할 SelectOpener 컴포넌트 스토리북 작성

---
+++ src/entities/auth/ui/SelectOpener.stories.tsx
@@ -0,0 +1,124 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { SelectOpener } from "./SelectOpener";
+import { useState } from "react";
+import { Select } from "@/shared/ui/select";
+import { expect, fireEvent, waitFor, within, fn } from "@storybook/test";
+
+const meta: Meta = {
+  title: "entities/auth/SelectOpener",
+  component: SelectOpener,
+  tags: ["autodocs"],
+  argTypes: {
+    label: {
+      description: "label을 나타냅니다.",
+      control: {
+        type: "text",
+      },
+    },
+    essential: {
+      description: "필수 여부를 나타냅니다.",
+      control: {
+        type: "boolean",
+      },
+    },
+    placeholder: {
+      description: "placeholder를 나타냅니다.",
+      control: {
+        type: "text",
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SelectOpener>;
+
+export const Default: Story = {
+  args: {
+    label: "label",
+    essential: false,
+    placeholder: "placeholder",
+    value: "",
+  },
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    const $label = canvas.getByText("label");
+    const $opener = canvas.getByRole("button", {
+      name: /placeholder/i,
+    });
+
+    // 오프너 클릭 이벤트 핸들러 모킹
+    const handleOpenerClick = fn();
+    $opener.onclick = handleOpenerClick;
+
+    await step("label을 클릭하면, button이 포커스되고 클릭된다.", async () => {
+      fireEvent.click($label);
+
+      await waitFor(() => {
+        expect($opener).toHaveFocus();
+        expect(handleOpenerClick).toHaveBeenCalled();
+      });
+    });
+  },
+};
+
+export const Example: Story = {
+  render: () => {
+    const genderOptionList = [
+      {
+        value: "MALE",
+        name: "남자",
+      },
+      {
+        value: "FEMALE",
+        name: "여자",
+      },
+    ] as const;
+
+    /* eslint-disable */
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [selectedValue, setSelectedValue] = useState<
+      "FEMALE" | "MALE" | null
+    >(null);
+
+    const selectedName = genderOptionList.find(
+      (option) => option.value === selectedValue,
+    )?.name;
+
+    return (
+      <div>
+        <SelectOpener
+          label="성별"
+          essential={true}
+          placeholder="성별을 선택해 주세요"
+          value={selectedName ?? ""}
+          disabled={false}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+
+        <Select isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          <Select.BottomSheet>
+            <Select.OptionList>
+              {genderOptionList.map(({ value, name }) => {
+                return (
+                  <Select.Option
+                    key={value}
+                    value={value}
+                    isSelected={value === selectedValue}
+                    onClick={() => {
+                      setSelectedValue(value);
+                    }}
+                  >
+                    {name}
+                  </Select.Option>
+                );
+              })}
+            </Select.OptionList>
+          </Select.BottomSheet>
+        </Select>
+      </div>
+    );
+  },
+};
