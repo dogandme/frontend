69f031e5db4666f097ad0c2487a8850ec1b9948b
aken-you <kinsk2839@naver.com>
2024-09-07 22:26:20 +0900

    refactor[#66] : 기본 정보 입력 폼의 input, select, check list 컴포넌트 생성

--- src/features/auth/ui/UserInfoRegistrationForm.tsx
+++ src/features/auth/ui/UserInfoRegistrationForm.tsx
@@ -6,37 +6,263 @@ import { Input } from "@/shared/ui/input";
 import { useState } from "react";
 import { usePutUserInfoRegistration } from "../api";
 import { useAuthStore } from "@/shared/store/auth";
+import { Select } from "@/shared/ui/select";
+import { useUserInfoRegistrationFormStore } from "../store";
 
-const UserInfoRegistrationForm = () => {
-  // todo: input 상태 리팩토링 필요
-  const [nickname, setNickname] = useState<string>("");
-  const [checkedItems, setCheckedItems] = useState<boolean[]>([
-    false,
-    false,
-    false,
-  ]);
+const validateNickname = (nickName: string) => {
+  const regExp = /^[a-zA-Z0-9ㄱ-ㅎ가-힣]{1,20}$/;
+  return regExp.test(nickName);
+};
 
-  // 전체 선택되어 있는 경우
-  const allChecked = checkedItems.every(Boolean);
-  // 전체 선택되어 있지 않고 하나 이상 선택되어 있는 경우
-  const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
-
-  // nickname 제한
-  // 20자 이내의 한글 영어 숫자만 사용 가능합니다.
-  // 특수문자 입력 x
-  // 공백 입력 x
-
-  const validateNickname = (nickName: string) => {
-    const regExp = /^[a-zA-Z0-9ㄱ-ㅎ가-힣]{1,20}$/;
-    return regExp.test(nickName);
-  };
+const NicknameInput = () => {
+  const nickname = useUserInfoRegistrationFormStore((state) => state.nickname);
+  const setNickname = useUserInfoRegistrationFormStore(
+    (state) => state.setNickname,
+  );
 
   const isValidNickname = validateNickname(nickname);
 
+  return (
+    <Input
+      type="text"
+      id="nickname"
+      name="nickname"
+      label="닉네임"
+      placeholder="닉네임을 입력해 주세요"
+      statusText="20자 이내의 한글 영어 숫자만 사용 가능합니다."
+      essential
+      componentType="outlinedText"
+      isError={!isValidNickname && nickname.length > 0}
+      value={nickname}
+      onChange={(e) => setNickname(e.target.value)}
+      maxLength={20}
+    />
+  );
+};
+
+const GenderSelect = () => {
+  const genderOptionList = [
+    {
+      value: "MALE",
+      name: "남자",
+    },
+    {
+      value: "FEMALE",
+      name: "여자",
+    },
+  ] as const;
+
+  const gender = useUserInfoRegistrationFormStore((state) => state.gender);
+  const setGender = useUserInfoRegistrationFormStore(
+    (state) => state.setGender,
+  );
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const selectedName = genderOptionList.find((e) => e.value === gender)?.name;
+
+  return (
+    <>
+      <div className="pb-5">
+        <SelectOpener
+          id="gender"
+          name="gender"
+          label="성별"
+          essential
+          placeholder="성별을 선택해 주세요"
+          value={selectedName ?? ""}
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        />
+      </div>
+
+      <Select isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <Select.BottomSheet>
+          <Select.OptionList>
+            {genderOptionList.map(({ value, name }) => {
+              return (
+                <Select.Option
+                  key={value}
+                  value={value}
+                  isSelected={value === gender}
+                  onClick={() => setGender(value)}
+                >
+                  {name}
+                </Select.Option>
+              );
+            })}
+          </Select.OptionList>
+        </Select.BottomSheet>
+      </Select>
+    </>
+  );
+};
+
+const AgeRangeSelect = () => {
+  const ageRangeOptionList = [
+    {
+      value: 10,
+      name: "10대",
+    },
+    {
+      value: 20,
+      name: "20~30대",
+    },
+    {
+      value: 30,
+      name: "30~40대",
+    },
+    {
+      value: 40,
+      name: "50~60대",
+    },
+    {
+      value: 50,
+      name: "60대 이상",
+    },
+  ] as const;
+
+  const ageRange = useUserInfoRegistrationFormStore((state) => state.ageRange);
+  const setAgeRange = useUserInfoRegistrationFormStore(
+    (state) => state.setAgeRange,
+  );
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const selectedName = ageRangeOptionList.find(
+    (e) => e.value === ageRange,
+  )?.name;
+
+  return (
+    <>
+      <div className="pb-5">
+        <SelectOpener
+          id="age"
+          name="age"
+          label="연령대"
+          essential
+          value={selectedName ?? ""}
+          onClick={() => setIsOpen(true)}
+          placeholder="연령대를 선택해 주세요"
+        />
+      </div>
+
+      <Select isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <Select.BottomSheet>
+          <Select.OptionList>
+            {ageRangeOptionList.map(({ value, name }) => {
+              return (
+                <Select.Option
+                  key={value}
+                  value={value}
+                  isSelected={value === ageRange}
+                  onClick={() => setAgeRange(value)}
+                >
+                  {name}
+                </Select.Option>
+              );
+            })}
+          </Select.OptionList>
+        </Select.BottomSheet>
+      </Select>
+    </>
+  );
+};
+
+const RegionSetting = () => {
+  return (
+    <div>
+      <div className="flex items-start gap-1 pb-2">
+        <span className="title-3 text-grey-700">동네설정</span>
+        <Badge colorType="primary" />
+      </div>
+
+      <Button
+        type="button"
+        variant="outlined"
+        colorType="tertiary"
+        size="medium"
+      >
+        <MapLocationSearchingIcon />
+        <span>동네 설정하기</span>
+      </Button>
+    </div>
+  );
+};
+
+const AgreementCheckboxList = () => {
+  const agreementList = [
+    {
+      id: "terms-of-service-agreement",
+      label: "이용약관 동의 (필수)",
+      link: "/",
+    },
+    {
+      id: "privacy-policy-agreement",
+      label: "개인정보 수집 및 이용 동의 (필수)",
+      link: "/",
+    },
+    {
+      id: "marketing-information-agreement",
+      label: "마케팅 정보 수신 동의 (선택)",
+      link: "/",
+    },
+  ];
+
+  const checkList = useUserInfoRegistrationFormStore(
+    (state) => state.checkList,
+  );
+  const setCheckList = useUserInfoRegistrationFormStore(
+    (state) => state.setCheckList,
+  );
+
+  // 전체 선택되어 있는 경우
+  const allChecked = checkList.every(Boolean);
+  // 전체 선택되어 있지 않고 하나 이상 선택되어 있는 경우
+  const isIndeterminate = checkList.some(Boolean) && !allChecked;
+
+  return (
+    <section className="flex flex-col gap-4">
+      <AgreementCheckbox
+        id="agree-to-all-terms"
+        checked={allChecked}
+        isIndeterminate={isIndeterminate}
+        label="전체 동의합니다."
+        onChange={(e) => {
+          const { checked } = e.target;
+
+          setCheckList([checked, checked, checked]);
+        }}
+      />
+
+      {agreementList.map(({ id, label, link }, idx) => {
+        return (
+          <AgreementCheckbox
+            key={id}
+            id={id}
+            checked={checkList[idx]}
+            label={label}
+            onChange={() => {
+              const newCheckList = [...checkList];
+              newCheckList[idx] = !newCheckList[idx];
+
+              setCheckList(newCheckList);
+            }}
+            agreementLink={link}
+          />
+        );
+      })}
+    </section>
+  );
+};
+
+const UserInfoRegistrationForm = () => {
+  const { nickname, checkList } = useUserInfoRegistrationFormStore.getState();
+  const isValidNickname = validateNickname(nickname);
+
   const token = useAuthStore((state) => state.token);
-  const userId = useAuthStore((state) => state.userId);
-  // todo: setNickname으로 변경 (set 설정자 함수와 이름이 중복되어 임시방편으로 설정)
-  const setUserNickname = useAuthStore((state) => state.setNickname);
+  const setNickname = useAuthStore((state) => state.setNickname);
   const setRole = useAuthStore((state) => state.setRole);
 
   const { mutate: putUserInfoRegistration } = usePutUserInfoRegistration();
@@ -44,7 +270,7 @@ const UserInfoRegistrationForm = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!token || !userId) {
+    if (!token) {
       throw new Error("로그인 정보가 없습니다");
     }
 
@@ -61,7 +287,7 @@ const UserInfoRegistrationForm = () => {
       throw new Error("올바른 닉네임을 입력해 주세요");
     }
 
-    const areRequiredAgreementsChecked = checkedItems[0] && checkedItems[1];
+    const areRequiredAgreementsChecked = checkList[0] && checkList[1];
 
     if (!areRequiredAgreementsChecked) {
       throw new Error("필수 약관에 모두 동의해 주세요");
@@ -70,7 +296,6 @@ const UserInfoRegistrationForm = () => {
     putUserInfoRegistration(
       {
         token,
-        userId,
         nickname,
         gender: "MALE",
         age: 10,
@@ -83,7 +308,7 @@ const UserInfoRegistrationForm = () => {
             content: { nickname, role },
           } = data;
 
-          setUserNickname(nickname);
+          setNickname(nickname);
           setRole(role);
 
           // todo: 회원가입 완료 페이지로 이동
@@ -97,114 +322,16 @@ const UserInfoRegistrationForm = () => {
 
   return (
     <form className="flex flex-col gap-8 self-stretch" onSubmit={handleSubmit}>
-      <section className="flex flex-col gap-8 self-stretch">
-        <Input
-          type="text"
-          id="nickname"
-          name="nickname"
-          label="닉네임"
-          placeholder="닉네임을 입력해 주세요"
-          statusText="20자 이내의 한글 영어 숫자만 사용 가능합니다."
-          essential
-          componentType="outlinedText"
-          isError={!isValidNickname && nickname.length > 0}
-          value={nickname}
-          onChange={(e) => setNickname(e.target.value)}
-        />
-        <SelectOpener
-          id="sex"
-          name="sex"
-          label="성별"
-          essential
-          value={"남자"}
-          placeholder="성별을 선택해 주세요"
-        />
-        <SelectOpener
-          id="age"
-          name="age"
-          label="연령대"
-          essential
-          value={"10대"}
-          placeholder="연령대를 선택해 주세요"
-        />
-
-        <div>
-          <div className="flex items-start gap-1 pb-2">
-            <span className="title-3 text-grey-700">동네설정</span>
-            <Badge colorType="primary" />
-          </div>
-
-          <Button
-            type="button"
-            variant="outlined"
-            colorType="tertiary"
-            size="medium"
-          >
-            <MapLocationSearchingIcon />
-            <span>동네 설정하기</span>
-          </Button>
-        </div>
+      <section className="flex flex-col gap-4 self-stretch">
+        <NicknameInput />
+        <GenderSelect />
+        <AgeRangeSelect />
+        <RegionSetting />
       </section>
 
       <hr className="text-grey-200" />
 
-      <section className="flex flex-col gap-4">
-        <AgreementCheckbox
-          id="agree-to-all-terms"
-          checked={allChecked}
-          isIndeterminate={isIndeterminate}
-          label="전체 동의합니다."
-          onChange={(e) =>
-            setCheckedItems([
-              e.target.checked,
-              e.target.checked,
-              e.target.checked,
-            ])
-          }
-        />
-
-        <AgreementCheckbox
-          id="terms-of-service-agreement"
-          checked={checkedItems[0]}
-          label="이용약관 동의 (필수)"
-          onChange={(e) =>
-            setCheckedItems([
-              e.target.checked,
-              checkedItems[1],
-              checkedItems[2],
-            ])
-          }
-          agreementLink="/"
-        />
-
-        <AgreementCheckbox
-          id="privacy-policy-agreement"
-          label="개인정보 수집 및 이용 동의 (필수)"
-          checked={checkedItems[1]}
-          onChange={(e) =>
-            setCheckedItems([
-              checkedItems[0],
-              e.target.checked,
-              checkedItems[2],
-            ])
-          }
-          agreementLink="/"
-        />
-
-        <AgreementCheckbox
-          id="marketing-information-agreement"
-          label="마케팅 정보 수신 동의 (선택)"
-          checked={checkedItems[2]}
-          onChange={(e) =>
-            setCheckedItems([
-              checkedItems[0],
-              checkedItems[1],
-              e.target.checked,
-            ])
-          }
-          agreementLink="/"
-        />
-      </section>
+      <AgreementCheckboxList />
 
       <Button type="submit" colorType="primary" variant="filled" size="large">
         회원가입
