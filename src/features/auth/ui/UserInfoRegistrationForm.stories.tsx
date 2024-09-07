7cfe026e81d356bdf9461644a615bd81656c8331
aken-you <kinsk2839@naver.com>
2024-09-08 00:30:52 +0900

    test[#66] : 기본 정보 등록 폼 storybook에서 닉네임 input 검사하는 테스트끼리 묶기

--- src/features/auth/ui/UserInfoRegistrationForm.stories.tsx
+++ src/features/auth/ui/UserInfoRegistrationForm.stories.tsx
@@ -24,62 +24,63 @@ export const Default: Story = {
 
   play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement);
+
     const $nicknameInput = canvasElement.querySelector(
       'input[name="nickname"]',
     ) as HTMLInputElement;
 
-    await step(
-      'focus된 상태에서 입력값의 길이가 0일때, "20자 이내의 한글 영어 숫자만 사용 가능합니다." 안내문구가 뜬다.',
-      async () => {
-        await userEvent.click($nicknameInput);
+    await step("nickname input 검사", async () => {
+      const STATUS_TEXT = "20자 이내의 한글 영어 숫자만 사용 가능합니다.";
+      const textColor = {
+        base: "text-grey-500",
+        error: "text-pink-500",
+      };
 
-        const $statusText = canvas.getByText(
-          "20자 이내의 한글 영어 숫자만 사용 가능합니다.",
-        );
-        expect($statusText).toBeInTheDocument();
-        expect($statusText).toHaveClass("text-grey-500");
-      },
-    );
+      await step(
+        "focus된 상태에서 입력값의 길이가 0일때, 안내 문구가 뜬다.",
+        async () => {
+          await userEvent.click($nicknameInput);
 
-    await step(
-      '닉네임 형식에 맞지 않게 입력할 경우, "20자 이내의 한글 영어 숫자만 사용 가능합니다." 색상이 pink-500으로 표시된다.',
-      async () => {
-        await userEvent.type($nicknameInput, "!!@#$@");
-        // outfocus되도 statusText를 pink-500 색상으로 표시
-        await userEvent.tab();
+          const $statusText = canvas.getByText(STATUS_TEXT);
+          expect($statusText).toBeInTheDocument();
+          expect($statusText).toHaveClass(textColor.base);
+        },
+      );
 
-        const $statusText = canvas.getByText(
-          "20자 이내의 한글 영어 숫자만 사용 가능합니다.",
-        );
-        expect($statusText).toHaveClass("text-pink-500");
-      },
-    );
+      await step(
+        "닉네임 형식에 맞지 않게 입력할 경우, 안내 문구가 핑크색으로 표시된다.",
+        async () => {
+          await userEvent.type($nicknameInput, "!!@#$@");
+          // outfocus되도 statusText를 pink-500 색상으로 표시
+          await userEvent.tab();
 
-    await step(
-      '닉네임 형식에 맞게 입력한 경우, "20자 이내의 한글 영어 숫자만 사용 가능합니다." 색상이 grey-500으로 표시된다.',
-      async () => {
-        // 이메일 입력 필드의 값을 초기화합니다.
-        await userEvent.clear($nicknameInput);
-        await userEvent.type($nicknameInput, "hihihi");
+          const $statusText = canvas.getByText(STATUS_TEXT);
+          expect($statusText).toHaveClass(textColor.error);
+        },
+      );
 
-        const $statusText = canvas.getByText(
-          "20자 이내의 한글 영어 숫자만 사용 가능합니다.",
-        );
+      await step(
+        "닉네임 형식에 맞게 입력한 경우, 안내 문구는 기본 색상으로 표시된다.",
+        async () => {
+          // 이메일 입력 필드의 값을 초기화합니다.
+          await userEvent.clear($nicknameInput);
+          await userEvent.type($nicknameInput, "hihihi");
 
-        expect($statusText).toHaveClass("text-grey-500");
-      },
-    );
+          const $statusText = canvas.getByText(STATUS_TEXT);
 
-    await step(
-      '닉네임 형식에 맞게 입력한 상태에서 outfocus되면, "20자 이내의 한글 영어 숫자만 사용 가능합니다." 문구가 사라진다.',
-      async () => {
-        await userEvent.tab();
+          expect($statusText).toHaveClass(textColor.base);
+        },
+      );
 
-        const $statusText = canvas.queryByText(
-          "20자 이내의 한글 영어 숫자만 사용 가능합니다.",
-        );
-        expect($statusText).not.toBeInTheDocument();
-      },
-    );
+      await step(
+        "닉네임 형식에 맞게 입력한 상태에서 outfocus되면, 안내 문구가 사라진다.",
+        async () => {
+          await userEvent.tab();
+
+          const $statusText = canvas.queryByText(STATUS_TEXT);
+          expect($statusText).not.toBeInTheDocument();
+        },
+      );
+    });
   },
 };
