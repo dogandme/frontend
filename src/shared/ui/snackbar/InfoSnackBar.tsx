77aac15e6afd7e1e37518b4686bd17b1e1f02b45
yonghyeun <ttddcc119@naver.com>
2024-09-07 18:12:40 +0900

    feat[#115] : positionClassName props 추가

--- src/shared/ui/snackbar/InfoSnackBar.tsx
+++ src/shared/ui/snackbar/InfoSnackBar.tsx
@@ -5,6 +5,7 @@ interface InfoSnackBarProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   onClose: () => Promise<void>;
   autoHideDuration?: number | null;
+  positionClassName?: string;
 }
 
 /**
@@ -12,11 +13,13 @@ interface InfoSnackBarProps extends React.HTMLAttributes<HTMLDivElement> {
  * @param children - 스낵바에 표시할 메시지
  * @param onClose - 스낵바를 닫는 함수
  * @param autoHideDuration - 스낵바가 자동으로 사라지는 시간 (ms), 기본적으로 InfoSnackbar는 1초 후 사라집니다. 만약 사라지기를 원치 않는 경우엔 null 을 전달해 주세요
+ * @param positionClassName - 스낵바의 위치를 지정하는 클래스명
  */
 export const InfoSnackBar = ({
   children,
   onClose,
   autoHideDuration = 1000,
+  positionClassName = "absolute top-4 left-1/2 transform -translate-x-1/2",
   ...props
 }: InfoSnackBarProps) => {
   useEffect(() => {
@@ -24,9 +27,8 @@ export const InfoSnackBar = ({
       return;
     }
 
-    const timerId = setTimeout(async () => {
-      // TODO 애니메이션 기능이 추가되면  해당 코드 블록에 애니메이션 관련 CSS 넣기
-      await onClose();
+    const timerId = setTimeout(() => {
+      onClose();
     }, autoHideDuration);
 
     return () => clearTimeout(timerId);
@@ -34,9 +36,6 @@ export const InfoSnackBar = ({
 
   const baseClassName =
     "shadow-info-snackbar inline-flex min-w-[328px] max-w-96 items-center justify-between rounded-2xl bg-grey-0 py-1 pl-4 pr-3";
-  // TODO 디자이너와 상의하여 스낵바 노출 위치 픽스하기
-  const positionClassName =
-    "absolute top-4 left-1/2 transform -translate-x-1/2";
 
   return (
     <div className={`${baseClassName} ${positionClassName}`} {...props}>
