a039692eff399237846d0f20847ebb8f6e1e211e
aken-you <kinsk2839@naver.com>
2024-09-07 18:05:40 +0900

    refactor[#66] : auth 도메인에서 사용할 SelectOpener 컴포넌트 수정
    - 기존에는 Input 컴포넌트로 만들었으나 클릭 범위가 제한되어 있었음
    - button 태그를 이용해 새로운 컴포넌트 생성

--- src/entities/auth/ui/SelectOpener.tsx
+++ src/entities/auth/ui/SelectOpener.tsx
@@ -1,19 +1,57 @@
+import { Badge } from "@/shared/ui/badge";
 import { ArrowDropDownIcon } from "@/shared/ui/icon";
-import { Input, InputProps } from "@/shared/ui/input/Input";
+import { ButtonHTMLAttributes, useRef } from "react";
 
-type FixedInputProps = "type" | "componentType" | "readOnly" | "trailingNode";
+interface SelectOpenerProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+  essential?: boolean;
+  placeholder?: string;
+}
 
 export const SelectOpener = ({
+  label,
+  essential = false,
+  placeholder = "",
+  value,
+  disabled,
   ...props
-}: Omit<InputProps, FixedInputProps>) => {
+}: SelectOpenerProps) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleLabelClick = () => {
+    buttonRef.current?.focus();
+    buttonRef.current?.click();
+  };
+
+  const isValueEmpty = value === undefined || value === "";
+
+  const baseStyles =
+    "body-2 flex h-12 w-full items-center justify-between gap-1 rounded-2xl border px-3 py-2 ";
+  const colorStyles =
+    "border-grey-300 bg-grey-0 hover:border-grey-500 hover:bg-grey-100 focus:border-tangerine-500 focus:bg-grey-0 focus-visible:outline-none disabled:border-grey-100 disabled:bg-grey-0";
+  const textStyles = `body-2 ${disabled ? "text-grey-300" : !isValueEmpty ? "text-grey-700" : "text-grey-500"}`;
+
   return (
-    <Input
-      type="text"
-      trailingNode={<ArrowDropDownIcon />}
-      componentType="outlinedText"
-      readOnly
-      style={{ cursor: "pointer" }}
-      {...props}
-    />
+    <div>
+      {/* label */}
+      <div
+        className="flex w-fit cursor-pointer gap-1 pb-2"
+        onClick={handleLabelClick}
+      >
+        <p className="title-3 text-grey-700">{label}</p>
+        {essential && <Badge colorType="primary" />}
+      </div>
+
+      {/* trigger */}
+      <button
+        ref={buttonRef}
+        type="button"
+        className={`${baseStyles} ${colorStyles} ${textStyles}`}
+        {...props}
+      >
+        <span>{isValueEmpty ? placeholder : value}</span>
+        <ArrowDropDownIcon />
+      </button>
+    </div>
   );
 };
