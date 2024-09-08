8a2e663b88fb5213ae96028d58a05d4f0ab5946d
yonghyeun <ttddcc119@naver.com>
2024-09-08 11:59:35 +0900

    refactor[#118] : tailwind의 캐싱 활용하기 위해 대부분 css 모두 tailwind 로 변경

--- src/entities/map/ui/Pin.tsx
+++ src/entities/map/ui/Pin.tsx
@@ -6,17 +6,23 @@ export interface PinProps {
   alt?: string;
 }
 
+const Pin = ({ imageUrl, alt }: PinProps) => (
+  <>
+    <div className="pin relative flex h-[2.75rem] w-8 items-center justify-center bg-tangerine-500">
+      <div className="h-[1.625rem] w-[1.625rem] translate-y-[-0.375rem] rounded-full bg-grey-0">
+        <img src={imageUrl} alt={alt} className="h-full w-full rounded-2xl" />
+      </div>
+    </div>
+    <span className="absolute translate-x-[0.25rem] translate-y-[-0.5rem]">
+      <PinShadowIcon fill="tangerine-900" />
+    </span>
+  </>
+);
+
 export const Default = ({ imageUrl, alt }: PinProps) => {
   return (
-    <div className="pin-wrapper">
-      <div className="pin">
-        <div className="pin-inner">
-          <img src={imageUrl} alt={alt} className="h-full w-full rounded-2xl" />
-        </div>
-      </div>
-      <span className="pin-shadow">
-        <PinShadowIcon fill="#BB370C" />
-      </span>
+    <div className="relative">
+      <Pin imageUrl={imageUrl} alt={alt} />
     </div>
   );
 };
@@ -27,16 +33,9 @@ export const Multiple = ({
   amount,
 }: PinProps & { amount: number }) => {
   return (
-    <div className="pin-wrapper">
-      <div className="pin">
-        <div className="pin-inner">
-          <img src={imageUrl} alt={alt} className="h-full w-full rounded-2xl" />
-        </div>
-      </div>
-      <div className="pin-shadow">
-        <PinShadowIcon fill="#BB370C" />
-      </div>
-      <div className="pin-amount">
+    <div className="relative">
+      <Pin imageUrl={imageUrl} alt={alt} />
+      <div className="absolute left-[0.75rem] top-[0.7rem]">
         <Badge colorType="secondary">{`+${Math.min(amount, 99)}`}</Badge>
       </div>
     </div>
