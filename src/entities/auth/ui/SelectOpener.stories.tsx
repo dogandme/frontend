392d4dcd89d79728815e3ff6897c042528b05d80
kinsk2839 <96980857+aken-you@users.noreply.github.com>
2024-09-09 00:15:10 +0900

    [Fix] SelectOpener가 비활성화되지 않는 문제 해결 (#125)
    * fix[#66] : SelectOpener가 비활성화되지 않는 문제 해결
    
    * test[#66] : SelectOpener 비활성화 테스트 코드 작성
    
    * test[#66] : storybook에서 SelectOpener의 click handler에 action 추가
    
    * test[#66] : 불필요한 test 관련 함수 삭제

--- src/entities/auth/ui/SelectOpener.stories.tsx
+++ src/entities/auth/ui/SelectOpener.stories.tsx
@@ -1,8 +1,10 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import { SelectOpener } from "./SelectOpener";
 import { useState } from "react";
 import { Select } from "@/shared/ui/select";
-import { expect, fireEvent, waitFor, within, fn } from "@storybook/test";
+import { expect, within, fn, userEvent } from "@storybook/test";
+import { Checkbox } from "@/shared/ui/checkbox";
 
 const meta: Meta = {
   title: "entities/auth/SelectOpener",
@@ -25,6 +27,12 @@ const meta: Meta = {
       description: "placeholder를 나타냅니다.",
       control: {
         type: "text",
+      },
+    },
+    disabled: {
+      description: "비활성 여부를 나타냅니다.",
+      control: {
+        type: "boolean",
       },
     },
   },
@@ -54,12 +62,10 @@ export const Default: Story = {
     $opener.onclick = handleOpenerClick;
 
     await step("label을 클릭하면, button이 포커스되고 클릭된다.", async () => {
-      fireEvent.click($label);
+      await userEvent.click($label);
 
-      await waitFor(() => {
-        expect($opener).toHaveFocus();
-        expect(handleOpenerClick).toHaveBeenCalled();
-      });
+      expect($opener).toHaveFocus();
+      expect(handleOpenerClick).toHaveBeenCalled();
     });
   },
 };
@@ -119,6 +125,91 @@ export const Example: Story = {
           </Select.BottomSheet>
         </Select>
       </div>
+    );
+  },
+};
+
+export const Disabled: Story = {
+  render: () => {
+    const optionList = ["가스코뉴 푸아트방", "가이아나쿠르세르"] as const;
+
+    /* eslint-disable */
+    const [isDisabled, setIsDisabled] = useState<boolean>(true);
+
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [selectedValue, setSelectedValue] = useState<string | null>(null);
+
+    const selectedName = optionList.find((option) => option === selectedValue);
+
+    return (
+      <div>
+        <div>
+          <SelectOpener
+            label="어떤 종의 아이인가요?"
+            essential={true}
+            placeholder="품종을 선택해 주세요"
+            value={selectedName ?? ""}
+            disabled={isDisabled}
+            onClick={() => {
+              action("onSelectOpenerClick")();
+              setIsOpen(!isOpen);
+            }}
+          />
+          <Checkbox
+            checked={isDisabled}
+            onChange={() => setIsDisabled((prev) => !prev)}
+          >
+            <span className="btn-3 text-grey-500">모르겠어요</span>
+          </Checkbox>
+        </div>
+
+        <Select isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          <Select.BottomSheet>
+            <Select.OptionList>
+              {optionList.map((value) => {
+                return (
+                  <Select.Option
+                    key={value}
+                    value={value}
+                    isSelected={value === selectedValue}
+                    onClick={() => {
+                      action("onOptionClick")(value);
+                      setSelectedValue(value);
+                    }}
+                  >
+                    {value}
+                  </Select.Option>
+                );
+              })}
+            </Select.OptionList>
+          </Select.BottomSheet>
+        </Select>
+      </div>
+    );
+  },
+
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    const $label = canvas.getByText("어떤 종의 아이인가요?");
+    const $opener = canvasElement.querySelector("button");
+
+    await step("disabled 상태일 때, button이 비활성화된다.", async () => {
+      expect($opener).toBeDisabled();
+    });
+
+    await step(
+      "disabled 상태일 때 label을 클릭해도, button이 클릭되지 않는다.",
+      async () => {
+        // 오프너 클릭 이벤트 핸들러 모킹
+        const handleOpenerClick = fn();
+        $opener!.onclick = handleOpenerClick;
+
+        await userEvent.click($label);
+
+        expect($opener).not.toHaveFocus();
+        expect(handleOpenerClick).not.toHaveBeenCalled();
+      },
     );
   },
 };
