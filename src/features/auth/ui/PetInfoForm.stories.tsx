875fdceedb33547779a9363726af942cf06fd1a9
yonghyeun <ttddcc119@naver.com>
2024-09-09 13:07:22 +0900

    fix[#129] : 병합 충돌 문제 해결

--- src/features/auth/ui/PetInfoForm.stories.tsx
+++ src/features/auth/ui/PetInfoForm.stories.tsx
@@ -82,13 +82,13 @@ export const Default: StoryObj<typeof _PetInfoForm> = {
     const $name = canvasElement.querySelector("#name")!;
     const $statusText = canvasElement.querySelectorAll("p")?.[1];
     const $breed = canvasElement.querySelector("#breed")!;
+    const $selectedBreedName = $breed.querySelector("span")!;
     const $unknownBreed = canvasElement.querySelector("#unknown-breed")!;
     const $characterButton1 = canvas.queryByText("호기심 많은");
     const $characterButton2 = canvas.queryByText("애착이 강한");
     const $textarea = canvasElement.querySelector("#introduce")!;
     const $submit = canvas.getByText("등록하기");
     const $bottomSheet = document.querySelector("#profile-bottom-sheet")!;
-
 
     const clearAll = async () => {
       await userEvent.clear($name);
