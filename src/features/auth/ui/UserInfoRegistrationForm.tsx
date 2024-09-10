cb4ec5405beddfb840794b42bb5224649758ae53
yonghyeun <ttddcc119@naver.com>
2024-09-11 00:03:28 +0900

    refactor[#140] : UserInfoRegistarionForm 에서 ActionChip 변경된 양식에 맞게 수정

--- src/features/auth/ui/UserInfoRegistrationForm.tsx
+++ src/features/auth/ui/UserInfoRegistrationForm.tsx
@@ -274,12 +274,14 @@ const MyRegionList = () => {
         <li className="flex flex-shrink-0" key={id}>
           <ActionChip
             variant="outlined"
-            label={address}
             trailingIcon={<CancelIcon width={20} height={20} />}
-            controlledIsSelected={true}
             key={id}
             onClick={() => handleRemoveRegion(address)}
-          />
+            isSelected={true}
+            isUncontrolled
+          >
+            {address}
+          </ActionChip>
         </li>
       ))}
     </ul>
