a3d5c65f304441395820836f2ac5fa0bde41edef
yonghyeun <ttddcc119@naver.com>
2024-09-10 11:32:26 +0900

    feat[#137] : 동네 검색 전체적인 뷰 완성

--- src/features/auth/ui/RegionModal.tsx
+++ src/features/auth/ui/RegionModal.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/shared/ui/button";
 import { ActionChip } from "@/shared/ui/chip";
-import { CloseIcon, MapLocationSearchingIcon } from "@/shared/ui/icon";
+import { CancelIcon, MapLocationSearchingIcon } from "@/shared/ui/icon";
 import { SearchIcon } from "@/shared/ui/icon";
 import { Input } from "@/shared/ui/input";
 import { List } from "@/shared/ui/list";
@@ -18,12 +18,19 @@ export const RegionModal = ({ onClose }: { onClose: () => Promise<void> }) => {
     "영등포 6동",
     "영등포 7동",
   ];
+  const selectedRegionList = ["영등포 1동", "영등포 2동", "영등포 3동"];
 
   return (
     <Modal modalType="fullPage">
       {/* Header */}
       <CloseNavigationBar onClick={onClose} />
-      <section className="px-4 flex flex-col gap-8">
+      {/* TODO flex grow 가 왜 안되지 ? */}
+      <section
+        className="px-4 flex flex-col gap-8"
+        style={{
+          height: "calc(100% - 6rem)",
+        }}
+      >
         <div className="flex flex-col gap-4">
           {/* 검색창 */}
           <Input
@@ -43,56 +50,62 @@ export const RegionModal = ({ onClose }: { onClose: () => Promise<void> }) => {
             <span>현재 위치로 찾기</span>
           </Button>
         </div>
-        {/* 검색 리스트 */}
-        <section className="flex flex-col gap-4 min-h-24">
-          {regionList.length > 0 && (
-            <>
-              <h1 className="title-2 text-grey-900">검색 결과</h1>
-              <List
-                style={{
-                  height: "18rem",
-                  overflowY: "auto",
-                  justifyContent: "start",
-                }}
-              >
-                {regionList.map((region, idx) => (
-                  <List.Item
-                    key={idx}
-                    style={{ justifyContent: "start" }}
-                    additionalClassName="title-3"
+        <div className="flex flex-col flex-grow justify-between ">
+          <div>
+            {/* 검색 리스트 */}
+            <section className="flex flex-col gap-4">
+              {regionList.length > 0 && (
+                <>
+                  <h1 className="title-2 text-grey-900">검색 결과</h1>
+                  <List
+                    style={{
+                      maxHeight: "18rem",
+                      overflowY: "auto",
+                      justifyContent: "start",
+                    }}
                   >
-                    {region}
-                  </List.Item>
-                ))}
-              </List>
-            </>
-          )}
-        </section>
-        {/* 검색 결과 리스트 */}
-        <section className="py-2 flex flex-col gap-4">
-          <p className="title-2">선택된 동네</p>
-          <ul className="flex items-start gap-2 self-stretch overflow-auto">
-            {[
-              "영등포동 5가",
-              "영등포동 6가",
-              "영등포동 7가",
-              "영등포동 8가",
-            ].map((selectedRegion, idx) => (
-              <li className="flex flex-shrink-0">
-                <ActionChip
-                  variant="outlined"
-                  label={selectedRegion}
-                  trailingIcon={<CloseIcon />}
-                  key={idx}
-                />
-              </li>
-            ))}
-          </ul>
-        </section>
-        {/* 확인 버튼 */}
-        <Button colorType="primary" variant="filled" size="medium">
-          확인
-        </Button>
+                    {regionList.map((region, idx) => (
+                      <List.Item
+                        key={idx}
+                        style={{ justifyContent: "start" }}
+                        additionalClassName="title-3"
+                      >
+                        {region}
+                      </List.Item>
+                    ))}
+                  </List>
+                </>
+              )}
+            </section>
+          </div>
+          <div className="flex flex-col gap-4 pb-8">
+            {/* 검색 결과 리스트 */}
+            <section className="py-2 flex flex-col gap-4">
+              {selectedRegionList.length > 0 && (
+                <>
+                  <p className="title-2">선택된 동네</p>
+                  <ul className="flex items-start gap-2 self-stretch overflow-auto pb-4">
+                    {selectedRegionList.map((selectedRegion, idx) => (
+                      <li className="flex flex-shrink-0">
+                        <ActionChip
+                          variant="outlined"
+                          label={selectedRegion}
+                          trailingIcon={<CancelIcon width={20} height={20} />}
+                          unControlledInitialIsSelect={true}
+                          key={idx}
+                        />
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              )}
+            </section>
+            {/* 확인 버튼 */}
+            <Button colorType="primary" variant="filled" size="medium">
+              확인
+            </Button>
+          </div>
+        </div>
       </section>
     </Modal>
   );
