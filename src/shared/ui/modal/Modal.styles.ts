821c56224904601c7a1af156f32ad08b660f6dc0
yonghyeun <ttddcc119@naver.com>
2024-09-10 10:24:22 +0900

    fix[#137] : fullPage modal overflow scroll 추가

--- src/shared/ui/modal/Modal.styles.ts
+++ src/shared/ui/modal/Modal.styles.ts
@@ -1,5 +1,6 @@
 export const modalStyles = {
-  fullPage: "fixed left-0 top-0 h-full w-full flex-col gap-8 bg-grey-0",
+  fullPage:
+    "fixed left-0 top-0 h-full w-full flex-col gap-8 bg-grey-0 overflow-y-auto",
   center:
     "shadow-custom-1 flex gap-8 fixed left-1/2 top-1/2 flex w-[20.5rem] -translate-x-1/2 -translate-y-1/2 transform flex-col rounded-[1.75rem] bg-grey-0 px-6 py-6 bg-grey-0",
 } as const;
