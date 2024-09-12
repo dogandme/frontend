cd56c91cdc604c2bbab1a5229a191208c933305e
yonghyeun <ttddcc119@naver.com>
2024-09-13 00:37:39 +0900

    fix[#148] : StartIcon -> StarIcon 오타 수정

--- src/widgets/map/ui/MapControlWidget.tsx
+++ src/widgets/map/ui/MapControlWidget.tsx
@@ -1,6 +1,6 @@
 import { MarkingButton } from "@/features/map/ui";
 import { FloatingButton } from "@/entities/map/ui";
-import { LocationIcon, MyLocationIcon, StartIcon } from "@/shared/ui/icon";
+import { LocationIcon, MyLocationIcon, StarIcon } from "@/shared/ui/icon";
 
 /**
  * MapControlWidget 컴포넌트는 사용자가 사용중인 모드에 따라 서로 다른 컨트롤 버튼들을 렌더링 해야 합니다.
@@ -22,7 +22,7 @@ export const MapControlWidget = () => {
           <LocationIcon />
         </FloatingButton>
         <FloatingButton>
-          <StartIcon />
+          <StarIcon />
         </FloatingButton>
       </div>
       <div className="absolute w-full px-4 bottom-[3.375rem]">
