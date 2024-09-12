7f96052703789fb57883de44cf6b7c9e54c01e81
yonghyeun <ttddcc119@naver.com>
2024-09-13 00:31:59 +0900

    refactor[#149] : MapMarkerWidget 생성

--- src/pages/map/page.tsx
+++ src/pages/map/page.tsx
@@ -1,9 +1,14 @@
-import { GoogleMapsCopyRight, MapControlWidget } from "@/widgets/map/ui";
+import {
+  GoogleMapsCopyRight,
+  MapControlWidget,
+  MapMarkerWidget,
+} from "@/widgets/map/ui";
 import { GoogleMaps } from "@/widgets/map/ui/GoogleMaps";
 
 export const MapPage = () => {
   return (
     <GoogleMaps>
+      <MapMarkerWidget />
       <MapControlWidget />
       <GoogleMapsCopyRight />
     </GoogleMaps>
