ab4181fe3a1ef48d968e88bfa1f3d59e97bb801a
yonghyeun <ttddcc119@naver.com>
2024-09-13 17:10:56 +0900

    refactor[#148] : MapControlWidget 리팩토링 된 요소 이용하여 재정의

--- src/widgets/map/ui/MapControlWidget.tsx
+++ src/widgets/map/ui/MapControlWidget.tsx
@@ -1,33 +1,58 @@
-import { MarkingButton } from "@/features/map/ui";
-import { FloatingButton } from "@/entities/map/ui";
-import { LocationIcon, MyLocationIcon, StarIcon } from "@/shared/ui/icon";
+import { useMapStore } from "@/features/map/store/map";
+import {
+  MarkingEditButton,
+  MarkingFormTriggerButton,
+  MyLocationButton,
+  ShowOthersMarkingButton,
+  CollectionButton,
+  ExitEditModeButton,
+} from "@/features/map/ui";
 
 /**
  * MapControlWidget 컴포넌트는 사용자가 사용중인 모드에 따라 서로 다른 컨트롤 버튼들을 렌더링 해야 합니다.
  */
 export const MapControlWidget = () => {
-  return (
-    <>
-      {/* Map 최하단에 렌더링 될 컨트롤 버튼들 */}
-      <div className="absolute bottom-[6.875rem] left-4">
-        <FloatingButton>
-          <LocationIcon />
-        </FloatingButton>
-      </div>
-      <div className="flex flex-col gap-2 absolute bottom-[6.875rem] right-4">
-        <FloatingButton>
-          <MyLocationIcon />
-        </FloatingButton>
-        <FloatingButton>
-          <LocationIcon />
-        </FloatingButton>
-        <FloatingButton>
-          <StarIcon />
-        </FloatingButton>
-      </div>
-      <div className="absolute w-full px-4 bottom-[3.375rem]">
-        <MarkingButton />
-      </div>
-    </>
+  const mode = useMapStore((state) => state.mode);
+  const setMode = useMapStore((state) => state.setMode);
+
+  const handleStartEditMode = () => setMode("edit");
+  const handleExitEditMode = () => setMode("view");
+
+  if (mode === "edit") {
+    return (
+      <>
+        <div className="absolute top-4 right-4">
+          <ExitEditModeButton onClick={handleExitEditMode} />
+        </div>
+        <div className="absolute w-full px-4 bottom-[3.375rem]">
+          <MarkingFormTriggerButton />
+        </div>
+      </>
+    );
+  }
+
+  if (mode === "view") {
+    return (
+      <>
+        {/* Map 최하단에 렌더링 될 컨트롤 버튼들 */}
+        <div className="absolute bottom-[6.875rem] left-4">
+          <MyLocationButton />
+        </div>
+        <div className="flex flex-col gap-2 absolute bottom-[6.875rem] right-4">
+          {/* TODO UserInfo API 나오면 유저 프로필 사진 붙이기 */}
+          <MyLocationButton />
+          <ShowOthersMarkingButton />
+          <CollectionButton />
+        </div>
+        {/* 마킹하기 버튼 */}
+        <div className="absolute w-full px-4 bottom-[3.375rem]">
+          <MarkingEditButton onClick={handleStartEditMode} />
+        </div>
+      </>
+    );
+  }
+
+  throw new Error(
+    'MapControlWidget 컴포넌트는 "view" 또는 "edit" 모드만 지원합니다.',
   );
 };
