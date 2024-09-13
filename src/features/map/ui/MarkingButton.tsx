e3330a95a54fa837c23660b561ea2d12de58594f
yonghyeun <ttddcc119@naver.com>
2024-09-13 17:12:05 +0900

    fix[#148] : 사용하지 않는 파일 제거

--- src/features/map/ui/MarkingButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/shared/ui/button";
-
-export const MarkingButton = () => {
-  return (
-    <Button colorType="primary" variant="filled" size="medium">
-      <span className="btn-2">마킹하기</span>
-    </Button>
-  );
-};
