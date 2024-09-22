779863953d2012091e7bed686409c61c79d7d0d7
yonghyeun <ttddcc119@naver.com>
2024-09-22 21:00:44 +0900

    feat[#184] : 임시 저장 중인 게시글 마킹 버튼 UI생성

---
+++ src/entities/profifle/ui/TemporaryMarkingBar.tsx
@@ -0,0 +1,14 @@
+import { ArrowRightIcon, InfoIcon } from "@/shared/ui/icon";
+
+export const TemporaryMarkingBar = () => {
+  return (
+    <div className="text-grey-500 flex px-4 py-4 items-center gap-[10px] self-stretch rounded-2xl border border-grey-50 bg-grey-50 justify-between">
+      <InfoIcon width={20} height={20} />
+      <p className="flex-1 title-3">
+        <span>임시 저장중인 내 마킹</span>
+        <span className="text-tangerine-500"> n</span>
+      </p>
+      <ArrowRightIcon width={20} height={20} />
+    </div>
+  );
+};
