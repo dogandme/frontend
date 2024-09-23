9a6ac05d3533d350daa78a5a6cfdd91c9f39efa4
yonghyeun <ttddcc119@naver.com>
2024-09-23 11:17:59 +0900

    feat[#184] : 임시 마킹 바 temporaryMarkingList 추가

--- src/entities/profile/ui/TemporaryMarkingBar.tsx
+++ src/entities/profile/ui/TemporaryMarkingBar.tsx
@@ -3,8 +3,10 @@ import { ArrowRightIcon, InfoIcon } from "@/shared/ui/icon";
 
 export const TemporaryMarkingBar = () => {
   const role = useAuthStore((state) => state.role);
+  // TODO API 로 불러오기
+  const temporaryMarkingList = ["path1", "path2", "path.3"];
 
-  if (role !== "ROLE_USER") {
+  if (role !== "ROLE_USER" || temporaryMarkingList.length === 0) {
     return;
   }
 
@@ -12,8 +14,10 @@ export const TemporaryMarkingBar = () => {
     <div className="text-grey-500 flex px-4 py-4 items-center gap-[10px] self-stretch rounded-2xl border border-grey-50 bg-grey-50 justify-between">
       <InfoIcon width={20} height={20} />
       <p className="flex-1 title-3">
-        <span>임시 저장중인 내 마킹</span>
-        <span className="text-tangerine-500"> n</span>
+        <span>임시 저장중인 내 마킹 </span>
+        <span className="text-tangerine-500">
+          {temporaryMarkingList.length}
+        </span>
       </p>
       <ArrowRightIcon width={20} height={20} />
     </div>
