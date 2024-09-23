f362db618147a18ddd7d6d97b3c1be93c5824e72
yonghyeun <ttddcc119@naver.com>
2024-09-24 01:14:19 +0900

    feat[#186] : /my-page/setting 페이지 생성

---
+++ src/pages/my-page/setting/page.tsx
@@ -0,0 +1,37 @@
+import { LogoutButton } from "@/features/profile";
+import {
+  AccountManagement,
+  EditMyInfo,
+  MyActivity,
+  Notification,
+  TermsOfService,
+  VersionInfo,
+  Report,
+} from "@/entities/profile/ui";
+import { DividerLine } from "@/shared/ui/divider";
+import { BackwardNavigationBar } from "@/shared/ui/navigationbar";
+
+export const SettingPage = () => {
+  return (
+    <>
+      <section>
+        <BackwardNavigationBar
+          label={<h1 className="title-1 text-grey-900">설정</h1>}
+        />
+        <section className="flex flex-col gap-4 px-4 py-4">
+          <AccountManagement />
+          <EditMyInfo />
+          <DividerLine axis="row" />
+          <MyActivity />
+          <Notification />
+          <DividerLine axis="row" />
+          <TermsOfService />
+          <VersionInfo />
+          <Report />
+          <DividerLine axis="row" />
+          <LogoutButton />
+        </section>
+      </section>
+    </>
+  );
+};
