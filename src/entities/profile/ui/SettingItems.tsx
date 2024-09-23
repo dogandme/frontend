19e33ee5bdb58726a939f5290e00f017683ec6f1
yonghyeun <ttddcc119@naver.com>
2024-09-24 01:42:09 +0900

    feat[#186] : Report 컴포넌트 클립보드 복사 로직 추가

--- src/entities/profile/ui/SettingItems.tsx
+++ src/entities/profile/ui/SettingItems.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
+import { useSnackBar } from "@/shared/lib";
 import { ArrowRightIcon } from "@/shared/ui/icon";
+import { Snackbar } from "@/shared/ui/snackbar";
 
 const SettingClassName =
   "py-2 flex justify-between w-full text-grey-700 title-2 hover:bg-grey-50 focus-visible:bg-grey-50 focus-visible:outline-none active:bg-grey-100";
@@ -62,9 +64,29 @@ export const VersionInfo = () => (
 );
 
 // 문의/제안
-export const Report = () => (
-  <Link to="." className={SettingClassName}>
-    <p>문의/제안</p>
-    <span className="text-grey-700 body-2">mungwithme@gmail.com</span>
-  </Link>
-);
+export const Report = () => {
+  const officialEmail = "mungwithme@gmail.com";
+
+  const { handleOpen, onClose } = useSnackBar(() => (
+    <Snackbar onClose={onClose}>
+      <p>문의 사항을 보낼 이메일 주소가 복사되었습니다</p>
+    </Snackbar>
+  ));
+
+  const handleClick = async () => {
+    try {
+      await window.navigator.clipboard.writeText(officialEmail);
+      handleOpen();
+    } catch (error) {
+      // TODO 에러 처리 로직 추가
+      console.error(error);
+      throw new Error("이메일 주소를 복사 하던 중 오류가 발생했습니다");
+    }
+  };
+  return (
+    <button onClick={handleClick} className={SettingClassName}>
+      <p>문의/제안</p>
+      <span className="text-grey-700 body-2">mungwithme@gmail.com</span>
+    </button>
+  );
+};
