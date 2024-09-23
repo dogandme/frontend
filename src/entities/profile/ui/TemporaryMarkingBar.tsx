3e7b7429a2e194e97952b257a6794516dd1651b8
yonghyeun <ttddcc119@naver.com>
2024-09-23 22:37:12 +0900

    refactor[#184] : px 단위 rem 단위로 변환

--- src/entities/profile/ui/TemporaryMarkingBar.tsx
+++ src/entities/profile/ui/TemporaryMarkingBar.tsx
@@ -14,7 +14,7 @@ export const TemporaryMarkingBar = ({
   }
 
   return (
-    <div className="text-grey-500 flex px-4 py-4 items-center gap-[10px] self-stretch rounded-2xl border border-grey-50 bg-grey-50 justify-between">
+    <div className="text-grey-500 flex px-4 py-4 items-center gap-[.625rem] self-stretch rounded-2xl border border-grey-50 bg-grey-50 justify-between">
       <InfoIcon width={20} height={20} />
       <p className="flex-1 title-3">
         <span>임시 저장중인 내 마킹 </span>
