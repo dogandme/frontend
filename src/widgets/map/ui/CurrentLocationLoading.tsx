0dcf6f34cc12e8acec20c905927eb3db2232ce4b
aken-you <kinsk2839@naver.com>
2024-09-23 11:51:32 +0900

    feat[#175] : 현재 위치를 가져오기 전까지 보여줄 loading 컴포넌트 생성

---
+++ src/widgets/map/ui/CurrentLocationLoading.tsx
@@ -0,0 +1,10 @@
+const CurrentLocationLoading = () => {
+  // todo: ui 수정
+  return (
+    <div className="fixed flex justify-center items-center top-0 left-0 right-0 bottom-0 bg-grey-0 bg-opacity-80 z-50">
+      위치를 확인하는 중입니다.
+    </div>
+  );
+};
+
+export default CurrentLocationLoading;
