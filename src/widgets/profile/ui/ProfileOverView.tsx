bf0dc081058f622dc3ea2e9d58f06b734727aad7
yonghyeun <ttddcc119@naver.com>
2024-09-23 15:38:23 +0900

    feat[#177] : ProfileOverView 권한에 따른 네비게이팅 경로 설정

--- src/widgets/profile/ui/ProfileOverView.tsx
+++ src/widgets/profile/ui/ProfileOverView.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import {
   PetIntroduce,
   ProfileEditButton,
   ProfileImage,
   ProfileInfo,
 } from "@/entities/profile/ui";
+import { ROUTER_PATH } from "@/shared/constants";
 import { useAuthStore } from "@/shared/store";
 import { PlusIcon } from "@/shared/ui/icon";
 
@@ -38,10 +40,20 @@ export const ProfileOverView = () => {
     );
   }
 
+  const navigatePath =
+    role === null
+      ? ROUTER_PATH.LOGIN
+      : role === "ROLE_NONE"
+        ? ROUTER_PATH.SIGN_UP_USER_INFO
+        : ROUTER_PATH.SIGN_UP_PET_INFO;
+
   return (
-    <section className="px-4 py-4 flex flex-col gap-4 rounded-2xl border border-grey-300 bg-grey-50 w-full items-center text-grey-500">
+    <Link
+      to={navigatePath}
+      className="px-4 py-4 flex flex-col gap-4 rounded-2xl border border-grey-300 bg-grey-50 w-full items-center text-grey-500"
+    >
       <PlusIcon />
       <p className="title-3">반려동물을 등록해 주세요</p>
-    </section>
+    </Link>
   );
 };
