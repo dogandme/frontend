bba479c76476aaba1c3fa66d41f01717f5702fef
yonghyeun <ttddcc119@naver.com>
2024-09-23 14:45:51 +0900

    refactor[#181] : ReactNode -> JSX.Element 로 타입 좁히기

--- src/pages/map/page.tsx
+++ src/pages/map/page.tsx
@@ -6,7 +6,7 @@ import {
 import { GoogleMaps } from "@/widgets/map/ui/GoogleMaps";
 
 interface MapPageProps {
-  GlobalNavigationBar: React.ReactNode;
+  GlobalNavigationBar: JSX.Element;
 }
 export const MapPage = ({ GlobalNavigationBar }: MapPageProps) => {
   return (
