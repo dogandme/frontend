d8fd35d4949118cf80bccf62042ccbeb61d53615
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-09-25 00:44:26 +0900

    [Feat] ConfirmModal 생성 (#192)
    * feat[#191] : ConfirmModal 컴포넌트 생성
    
    * feat[#191] : ConfirmModal docs 추가
    
    * fix[#191] : resolve 타입 조건부로 변경
    
    * refactor[#191] : ConfirmModal resolve 버튼 클릭 시 호출되는 함수 순서 변경
    
    * refactor[#191] : onCloseConfirmModal 타입 수정
    
    * fix[#191] : 스토리북 로그아웃 모달 예시 변경
    
    * refactor[#191] : ConfirmModal Props 명 수정
    
    * feat[#191] : 스토리북 docs ,argTypes 추가
    
    * refactor[#191] : nav 태그가 시멘틱 요소를 만족하지 않아 div 태그로 변경

---
+++ src/shared/ui/modal/ConfirmModal.stories.tsx
@@ -0,0 +1,128 @@
+import { action } from "@storybook/addon-actions";
+import { StoryObj } from "@storybook/react";
+import { useModal } from "@/shared/lib";
+import { OverlayPortal } from "@/app";
+import { ConfirmModal } from "./ConfirmModal";
+import { Modal } from "./Modal";
+
+export default {
+  title: "shared/modal/ConfirmModal",
+  tags: ["autodocs"],
+  component: ConfirmModal,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "취소 , 확인 기능이 존재하는 ConfirmModal 입니다. `onConfirm` 함수를 통해 확인 버튼을 눌렀을 때의 동작을, `onClose` 함수를 통해 취소 버튼을 눌렀을 때의 동작을 정의 할 수 있습니다. `ConfirmModal은` `Modal` 컴포넌트를 기반으로 하며 `useOverlay` 메소드와 함께 사용 되어야 합니다.<br/><br/> `ConfirmModal` 은 취소, 확인 등 `props`로 받은 메소드가 실행 되면 모달이 닫히는 것이 보장 됩니다. `onConfirm` 메소드 실행 후 내부적으로 `onClose` 메소드를 실행하기 때문입니다. ",
+      },
+    },
+  },
+  argTypes: {
+    onClose: {
+      description:
+        "ConfirmModal 을 닫을 때 실행되는 함수 입니다. useModal의 반환값으로 받은 onClose 함수를 사용하면 됩니다. 해당 메소드는 취소에 해당하는 버튼을 클릭 할 때도 실행 되고 확인에도 해당하는 버튼을 클릭 할 때에도 실행 됩니다.",
+    },
+    onConfirm: {
+      description:
+        "ConfirmModal 을 호출 한 컴포넌트에서 정의 된 비지니스 로직을 실행하는 함수 입니다. 해당 버튼은 `ConfirmModal` 내부에서 확인에 해당하는 버튼이 클릭 시 실행됩니다.",
+    },
+    children: {
+      description: "ConfirmModal 내부에 표시 될 컨텐츠 입니다.",
+    },
+    confirmText: {
+      description: "확인 버튼의 텍스트를 지정합니다.",
+    },
+    closeText: {
+      description: "취소 버튼의 텍스트를 지정합니다.",
+    },
+    closeIconAriaLabel: {
+      description: "취소 버튼의 아이콘의 `aria-label`을 지정합니다.",
+    },
+    title: {
+      description: "ConfirmModal 의 최상단에 렌더링 될 제목 입니다.",
+    },
+  },
+};
+
+type Story = StoryObj<typeof ConfirmModal>;
+
+// 어떤 모달
+const SomethingModal = ({
+  onCloseSomethingModal,
+}: {
+  onCloseSomethingModal: () => Promise<void>;
+}) => {
+  const { handleOpen, onClose: onCloseLogoutConfirmModal } = useModal(() => (
+    <_ConfirmLeaveModal
+      onConfirm={onCloseSomethingModal}
+      onCloseConfirmModal={onCloseLogoutConfirmModal}
+    />
+  ));
+
+  return (
+    <Modal modalType="center">
+      <p>방가 방가 방가워요</p>
+      <button
+        onClick={handleOpen}
+        className="border border-tangerine-500 px-2 py-2 rounded-2xl"
+      >
+        저장 안하고 나가기
+      </button>
+    </Modal>
+  );
+};
+
+// features 레이어에서 모달을 정의했다고 가정
+const _ConfirmLeaveModal = ({
+  onConfirm,
+  onCloseConfirmModal,
+}: {
+  onConfirm: () => void | Promise<void>;
+  onCloseConfirmModal: () => Promise<void>;
+}) => {
+  const resetStore = () => {
+    action("스토어를 초기화 합니다 얍!")(); // Logout 내부에서 정의 된 비즈니스 로직
+    onConfirm();
+  };
+
+  return (
+    <ConfirmModal
+      onConfirm={resetStore}
+      onClose={onCloseConfirmModal}
+      title="화면을 나가시겠습니까?"
+    >
+      <p>화면을 나갈 경우 입력한 정보들이 모두 삭제 됩니다</p>
+      <p>정말 화면을 나가시겠습니까?</p>
+    </ConfirmModal>
+  );
+};
+
+const App = () => {
+  const { handleOpen, onClose } = useModal(() => (
+    <SomethingModal onCloseSomethingModal={onClose} />
+  ));
+
+  return (
+    <button
+      onClick={handleOpen}
+      className="border border-tangerine-500 px-2 py-2 rounded-2xl"
+    >
+      어떤 모달 열기
+    </button>
+  );
+};
+
+export const Default: Story = {
+  decorators: [
+    (Story) => (
+      <div id="root">
+        <OverlayPortal />
+        <div className="w-96 h-44">
+          <Story />
+        </div>
+      </div>
+    ),
+  ],
+
+  render: App,
+};
---
+++ src/shared/ui/modal/ConfirmModal.tsx
@@ -0,0 +1,63 @@
+import type { useOverlay } from "@/shared/lib";
+import { Button } from "../button";
+import { CloseIcon } from "../icon";
+import { Modal } from "./Modal";
+
+interface ConfirmModalProps {
+  onConfirm?: () => void | Promise<void>;
+  onClose: ReturnType<typeof useOverlay>["onClose"];
+  children: React.ReactNode;
+  confirmText?: string;
+  closeText?: string;
+  closeIconAriaLabel?: string;
+  title?: string;
+}
+
+export const ConfirmModal = ({
+  onConfirm,
+  onClose,
+  closeIconAriaLabel = "확인창 닫기",
+  confirmText = "확인",
+  closeText = "취소",
+  title = "",
+  children,
+}: ConfirmModalProps) => {
+  const navClassName = title ? "flex justify-between" : "flex justify-end";
+
+  return (
+    <Modal modalType="center">
+      <div className={navClassName}>
+        {title && <h1 className="title-1 text-grey-900">{title}</h1>}
+        <button aria-label={closeIconAriaLabel} onClick={onClose}>
+          <CloseIcon />
+        </button>
+      </div>
+      <section className="text-grey-700 body-2">{children}</section>
+      <div className="flex gap-2">
+        <Button
+          variant="text"
+          colorType="tertiary"
+          size="medium"
+          fullWidth={false}
+          className="flex-1"
+          onClick={onClose}
+        >
+          {closeText}
+        </Button>
+        <Button
+          variant="text"
+          colorType="primary"
+          size="medium"
+          fullWidth={false}
+          className="flex-1"
+          onClick={async () => {
+            await onConfirm?.();
+            await onClose();
+          }}
+        >
+          {confirmText}
+        </Button>
+      </div>
+    </Modal>
+  );
+};
