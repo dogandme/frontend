6de7fb46bbe3ced45c891bd43b5e3765759ebd0a
yonghyeun <ttddcc119@naver.com>
2024-09-25 15:08:45 +0900

    feat[#199] : PasswordChangeModal Input 완성

---
+++ src/features/setting/ui/_PasswordChangeModal.stories.tsx
@@ -0,0 +1,112 @@
+import { StoryObj } from "@storybook/react";
+import { expect, userEvent } from "@storybook/test";
+import { PasswordChangeModal } from "./_PasswordChangeModal";
+
+export default {
+  title: "features/setting/PasswordChangeModal",
+  component: PasswordChangeModal,
+};
+
+type Story = StoryObj<typeof PasswordChangeModal>;
+
+export const Default: Story = {
+  render: () => <PasswordChangeModal onClose={async () => {}} />,
+
+  play: async ({ canvasElement, step }) => {
+    // PasswordInput 에 대한 기본적인 테스트는 실행 되었기에 변경된 store 가 잘 작동하는지에 대한 테스트 코드만 작성 합니다.
+    const $currentPasswordInput = canvasElement.querySelector(
+      "#current-password",
+    ) as HTMLInputElement;
+    const $newPasswordInput = canvasElement.querySelector(
+      "#new-password",
+    ) as HTMLInputElement;
+    const $confirmPasswordInput = canvasElement.querySelector(
+      "#confirm-new-password",
+    ) as HTMLInputElement;
+    const emptyStatusText = "비밀번호를 입력해 주세요";
+    const inValidStatusText = "비밀번호 형식에 맞게 입력해 주세요";
+    const inValidConfirmStatusText = "비밀번호가 서로 일치하지 않습니다";
+
+    await step(
+      `각 인풋 필드들은 focus시 statusText로 ${emptyStatusText}가 나타난다.`,
+      async () => {
+        const pTags = [...canvasElement.querySelectorAll("p")].slice(0, 3);
+        await userEvent.click($currentPasswordInput);
+        expect(pTags[0].textContent === emptyStatusText).toBeTruthy();
+
+        await userEvent.click($newPasswordInput);
+        expect(pTags[1].textContent === emptyStatusText).toBeTruthy();
+
+        await userEvent.click($confirmPasswordInput);
+        expect(pTags[2].textContent === emptyStatusText).toBeTruthy();
+      },
+    );
+
+    await step("현재 비밀번호는 정규성 검사를 적절하게 시행한다.", async () => {
+      const validPassword = "a1234567!";
+      const $statusText = canvasElement.querySelectorAll("p")[0];
+      // 일부만 입력
+      await userEvent.type($currentPasswordInput, validPassword.slice(0, 1));
+      expect($statusText.textContent).toBe(inValidStatusText);
+      // 나머지 모두 입력
+      await userEvent.type($currentPasswordInput, validPassword.slice(1));
+      expect($statusText.textContent).toBe("");
+      // 초기화
+      await userEvent.clear($currentPasswordInput);
+      expect($statusText.textContent).toBe(emptyStatusText);
+    });
+
+    await step("새 비밀번호는 정규성 검사를 적절하게 시행한다.", async () => {
+      const validPassword = "!1234567a";
+      const $statusText = canvasElement.querySelectorAll("p")[1];
+      // 일부만 입력
+      await userEvent.type($newPasswordInput, validPassword.slice(0, 1));
+      expect($statusText.textContent).toBe(inValidStatusText);
+      // 나머지 모두 입력
+      await userEvent.type($newPasswordInput, validPassword.slice(1));
+      expect($statusText.textContent).toBe("");
+      // 초기화
+      await userEvent.clear($newPasswordInput);
+      expect($statusText.textContent).toBe(emptyStatusText);
+    });
+
+    await step(
+      "새 비밀번호 확인은 정규성 검사를 적절하게 시행한다.",
+      async () => {
+        const $statusText = canvasElement.querySelectorAll("p")[2];
+        // 새 비밀번호 확인은 새 비밀번호 값에 종속되어 있기에 새 비밀번호를 먼저 입력합니다.
+        const validPassword = "!1234567aa";
+        await userEvent.type($newPasswordInput, validPassword);
+
+        await userEvent.click($confirmPasswordInput);
+        expect($statusText.textContent).toBe(emptyStatusText);
+
+        // 일부만 입력
+        await userEvent.type($confirmPasswordInput, validPassword.slice(0, 1));
+        expect($statusText.textContent).toBe(inValidConfirmStatusText);
+        // 나머지 모두 입력
+        await userEvent.type($confirmPasswordInput, validPassword.slice(1));
+        expect($statusText.textContent).toBe("");
+
+        // newPasswordInput 이 변경 되었을 때 statusText 가 변경 되는지 확인
+        await userEvent.type($newPasswordInput, "{backspace}");
+        expect($statusText.textContent).toBe(inValidConfirmStatusText);
+
+        await userEvent.type($confirmPasswordInput, "{backspace}");
+        expect($statusText.textContent).toBe("");
+
+        // 초기화
+        await userEvent.clear($confirmPasswordInput);
+        await userEvent.clear($newPasswordInput);
+
+        // confirmPasswordInput은 newPasswordInput이 정규성 검사를 만족하지 못하더라도 동일하기만 하면 statusText가 나타나지 않습니다.
+        await userEvent.type($newPasswordInput, validPassword.slice(0, 3));
+        await userEvent.type($confirmPasswordInput, validPassword.slice(0, 3));
+
+        const $newPasswordStatusText = canvasElement.querySelectorAll("p")[1];
+        expect($newPasswordStatusText.textContent).toBe(inValidStatusText);
+        expect($statusText.textContent).toBe("");
+      },
+    );
+  },
+};
---
+++ src/features/setting/ui/_PasswordChangeModal.tsx
@@ -0,0 +1,210 @@
+import { PasswordInput } from "@/entities/auth/ui";
+import { Button } from "@/shared/ui/button";
+import { CloseIcon } from "@/shared/ui/icon";
+import { Modal } from "@/shared/ui/modal";
+import { usePasswordChangeFormStore } from "../store";
+
+// TODO 사용 가능한 비밀 번호 시 statusText 변경
+
+const validatePassword = (password: string) => {
+  // 조건
+  // 1. 영문, 숫자, 특수문자 3가지 조합 포함
+  // 2. 8~15자 이내
+  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+
+  return passwordRegex.test(password);
+};
+
+const CurrentPasswordInput = () => {
+  const isValidPassword = usePasswordChangeFormStore(
+    (state) => state.isValidPassword,
+  );
+  const isEmptyCurrentPassword = usePasswordChangeFormStore(
+    (state) => state.isEmptyCurrentPassword,
+  );
+
+  const setCurrentPassword = usePasswordChangeFormStore(
+    (state) => state.setCurrentPassword,
+  );
+  const setIsValidPassword = usePasswordChangeFormStore(
+    (state) => state.setIsValidPassword,
+  );
+
+  const setIsEmptyCurrentPassword = usePasswordChangeFormStore(
+    (state) => state.setIsEmptyCurrentPassword,
+  );
+
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = target;
+    setCurrentPassword(value);
+
+    const isValidPassword = validatePassword(value);
+    const isPasswordEmpty = value.length === 0;
+
+    setIsValidPassword(isValidPassword);
+    setIsEmptyCurrentPassword(isPasswordEmpty);
+  };
+
+  const statusText = isEmptyCurrentPassword
+    ? "비밀번호를 입력해 주세요"
+    : isValidPassword
+      ? ""
+      : "비밀번호 형식에 맞게 입력해 주세요";
+
+  return (
+    <PasswordInput
+      id="current-password"
+      label="현재 비밀번호"
+      name="current-password"
+      placeholder="현재 비밀번호를 입력해주세요"
+      essential
+      onChange={handleChange}
+      statusText={statusText}
+      isError={!isEmptyCurrentPassword && !isValidPassword}
+    />
+  );
+};
+
+const NewPasswordInput = () => {
+  const isValidNewPassword = usePasswordChangeFormStore(
+    (state) => state.isValidNewPassword,
+  );
+  const isEmptyNewPassword = usePasswordChangeFormStore(
+    (state) => state.isEmptyNewPassword,
+  );
+
+  const setNewPassword = usePasswordChangeFormStore(
+    (state) => state.setNewPassword,
+  );
+  const setIsValidNewPassword = usePasswordChangeFormStore(
+    (state) => state.setIsValidNewPassword,
+  );
+  const setIsValidConfirmPassword = usePasswordChangeFormStore(
+    (state) => state.setIsValidConfirmPassword,
+  );
+
+  const setIsEmptyNewPassword = usePasswordChangeFormStore(
+    (state) => state.setIsEmptyNewPassword,
+  );
+
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = target;
+    setNewPassword(value);
+
+    const isValidNewPassword = validatePassword(value);
+    const isNewPasswordEmpty = value.length === 0;
+    setIsValidNewPassword(isValidNewPassword);
+    setIsEmptyNewPassword(isNewPasswordEmpty);
+
+    const { confirmPassword } = usePasswordChangeFormStore.getState();
+    setIsValidConfirmPassword(value === confirmPassword);
+  };
+
+  const statusText = isEmptyNewPassword
+    ? "비밀번호를 입력해 주세요"
+    : isValidNewPassword
+      ? ""
+      : "비밀번호 형식에 맞게 입력해 주세요";
+
+  return (
+    <PasswordInput
+      id="new-password"
+      label="새 비밀번호"
+      name="new-password"
+      placeholder="비밀번호를 입력해 주세요"
+      essential
+      onChange={handleChange}
+      statusText={statusText}
+      isError={!isEmptyNewPassword && !isValidNewPassword}
+    />
+  );
+};
+
+const ConfirmNewPasswordInput = () => {
+  const isValidConfirmPassword = usePasswordChangeFormStore(
+    (state) => state.isValidConfirmPassword,
+  );
+  const isEmptyConfirmPassword = usePasswordChangeFormStore(
+    (state) => state.isEmptyConfirmPassword,
+  );
+
+  const setConfirmPassword = usePasswordChangeFormStore(
+    (state) => state.setConfirmPassword,
+  );
+  const setIsValidConfirmPassword = usePasswordChangeFormStore(
+    (state) => state.setIsValidConfirmPassword,
+  );
+
+  const setIsEmptyConfirmPassword = usePasswordChangeFormStore(
+    (state) => state.setIsEmptyConfirmPassword,
+  );
+
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = target;
+    setConfirmPassword(value);
+
+    const isConfirmPasswordEmpty = value.length === 0;
+    setIsEmptyConfirmPassword(isConfirmPasswordEmpty);
+
+    const { newPassword } = usePasswordChangeFormStore.getState();
+    setIsValidConfirmPassword(newPassword === value);
+  };
+
+  const statusText = isEmptyConfirmPassword
+    ? "비밀번호를 입력해 주세요"
+    : isValidConfirmPassword
+      ? ""
+      : "비밀번호가 서로 일치하지 않습니다";
+
+  return (
+    <PasswordInput
+      id="confirm-new-password"
+      name="confirm-new-password"
+      placeholder="비밀번호를 다시 한 번 입력해주세요"
+      essential
+      onChange={handleChange}
+      statusText={statusText}
+      isError={!isEmptyConfirmPassword && !isValidConfirmPassword}
+    />
+  );
+};
+
+export const PasswordChangeModal = ({
+  onClose,
+}: {
+  onClose: () => Promise<void>;
+}) => {
+  return (
+    <Modal modalType="center">
+      <section className="flex justify-between self-stretch">
+        <h1 className="title-1 text-grey-900">비밀번호 변경</h1>
+        <button onClick={onClose}>
+          <CloseIcon />
+        </button>
+      </section>
+      <form
+        action=""
+        onSubmit={(e) => e.preventDefault()}
+        className="flex flex-col gap-4"
+      >
+        <CurrentPasswordInput />
+        <div className="flex flex-col gap-1">
+          <NewPasswordInput />
+          <ConfirmNewPasswordInput />
+        </div>
+        <p className="body-3 px-3 pt-1 text-grey-500">
+          영문, 숫자, 특수문자 3가지 조합을 포함하는 8자 이상 15자 이내로 입력해
+          주세요.
+        </p>
+      </form>
+      <div className="flex flex-col gap-2">
+        <Button colorType="primary" variant="filled" size="medium">
+          다음
+        </Button>
+        <Button colorType="tertiary" variant="text" size="medium">
+          취소
+        </Button>
+      </div>
+    </Modal>
+  );
+};
