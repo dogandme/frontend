52b1d1babdddf1cd9291c751e13aaf2bf6971cd7
yonghyeun <ttddcc119@naver.com>
2024-09-27 00:02:53 +0900

    refactor[#199] : emailFrom -> signUpMethod 로 이름 변경
    - 해당 변수가 나타내는 의미를 잘 표현 할 수 있도록 어떤 회원 가입시 어떤 방법으로 가입했는지를 표현

--- src/entities/setting/ui/Account.tsx
+++ src/entities/setting/ui/Account.tsx
@@ -2,15 +2,15 @@ import { settingClassName } from "./setting.styles";
 
 export const AccountEmail = () => {
   // TODO API 요청이나 userInfo 에서 가져오기
-  const emailFrom: "email" | "naver" | "google" = "email";
+  const signUpMethod: "email" | "naver" | "google" = "email";
   const email = "gaeun1234@gmail.com";
 
   // 회원가입 시 사용 한 방법에 따라 결정되는 문구
   // 이메일 회원 가입 시엔 이메일,  구글이나 네이버를 이용한 OAuth 의 경우엔 해당 서비스명이 표시됨
   const emailFromText =
-    emailFrom === "email"
+    signUpMethod === "email"
       ? "이메일"
-      : emailFrom === "naver"
+      : signUpMethod === "naver"
         ? "네이버"
         : "구글";
 
