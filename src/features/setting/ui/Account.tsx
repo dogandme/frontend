5498202bfd585998c88ed9b3b21ac1505788bccf
yonghyeun <ttddcc119@naver.com>
2024-09-27 02:07:25 +0900

    feat[#199] : 커스텀 훅 이용하여 모달 불러오도록 생성

--- src/features/setting/ui/Account.tsx
+++ src/features/setting/ui/Account.tsx
@@ -1,16 +1,15 @@
-import { useModal } from "@/shared/lib";
 import { ArrowRightIcon } from "@/shared/ui/icon";
-import { AccountCancellationModal } from "./_AccountCancellationModal";
-import { PasswordChangeModal } from "./_PasswordChangeModal";
+import { useCancellationCheckModal, useChangePasswordModal } from "../lib";
 import { settingClassName } from "./setting.styles";
 
 export const PasswordChangeButton = () => {
-  const { handleOpen, onClose } = useModal(() => (
-    <PasswordChangeModal onClose={onClose} />
-  ));
+  const handleOpenPasswordChangeModal = useChangePasswordModal();
 
   return (
-    <button className={settingClassName} onClick={handleOpen}>
+    <button
+      className={settingClassName}
+      onClick={handleOpenPasswordChangeModal}
+    >
       <p>비밀번호 변경</p>
       <div className="flex">
         <span className="text-grey-700 body-2">●●●●●●●●</span>
@@ -24,12 +23,13 @@ export const PasswordChangeButton = () => {
 
 // TODO 계정 탈퇴 기능은 기획 완성 후 추가하기
 export const AccountCancellation = () => {
-  const { handleOpen, onClose } = useModal(() => (
-    <AccountCancellationModal onClose={onClose} />
-  ));
+  const handleOpenCancellationCheckModal = useCancellationCheckModal();
 
   return (
-    <button className={settingClassName} onClick={handleOpen}>
+    <button
+      className={settingClassName}
+      onClick={handleOpenCancellationCheckModal}
+    >
       탈퇴하기
     </button>
   );
