4ca1ba1757c54c8fa8cac2752ab0116244a3e9d3
yonghyeun <ttddcc119@naver.com>
2024-09-27 01:46:03 +0900

    feat[#199] : useOverlay 기능 추가
    - beforeClose 로 removeOverlay 를 호출 시키는 것을 reject 시킬 수 있도록 try catch 문 추가
    - id 값을 new Date().getTime 이 아닌 랜덤한 값으로 변경

--- src/shared/lib/overlay.ts
+++ src/shared/lib/overlay.ts
@@ -16,16 +16,21 @@ export const useOverlay: UseOverlay = (
 ) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const { disableInteraction = true, beforeClose, afterClose } = options;
-  const id = new Date().getTime(); // 커스텀 훅이 호출 될 때 마다 유일한 id를 생성합니다.
+  const id = Math.random();
 
   const addOverlay = useOverlayStore((state) => state.addOverlay);
   const removeOverlay = useOverlayStore((state) => state.removeOverlay);
 
   const onClose = async () => {
-    await beforeClose?.();
-    removeOverlay(id);
-    setIsOpen(false);
-    await afterClose?.();
+    try {
+      await beforeClose?.();
+      removeOverlay(id);
+      setIsOpen(false);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      await afterClose?.();
+    }
   };
 
   const handleOpen = async () => {
