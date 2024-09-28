d48e3bde94183a27470d01295b5d837d9320ff7c
yonghyeun <ttddcc119@naver.com>
2024-09-28 21:14:35 +0900

    feat[#193] : 스토리북 props 변경에 따라 변경

--- src/features/setting/ui/_ChangeNickNameModal.stories.tsx
+++ src/features/setting/ui/_ChangeNickNameModal.stories.tsx
@@ -1,4 +1,5 @@
 import { StoryObj } from "@storybook/react";
+import { createModifyUserInfoStore } from "../store";
 import { ChangeNickNameModal } from "./_ChangeNickNameModal";
 
 export default {
@@ -19,11 +20,27 @@ type Story = StoryObj<typeof ChangeNickNameModal>;
 export const Default: Story = {
   decorators: [
     (Story) => (
-      <div className="w-96">
+      <div className="w-96 h-96">
         <Story />
       </div>
     ),
   ],
 
-  render: () => <ChangeNickNameModal onClose={async () => {}} />,
+  render: () => (
+    <ChangeNickNameModal
+      onClose={async () => {}}
+      store={createModifyUserInfoStore({
+        nickname: "뽀송이",
+        gender: "여자",
+        age: "20대",
+        regionList: [
+          "영등포동 1가",
+          "영등포동 2가",
+          "영등포동 3가",
+          "영등포동 4가",
+          "영등포동 5가",
+        ],
+      })}
+    />
+  ),
 };
