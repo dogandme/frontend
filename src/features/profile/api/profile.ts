996bfac6651c9a461c1f27d3eab929824fa75144
yonghyeun <ttddcc119@naver.com>
2024-10-01 00:24:31 +0900

    feat[#217] : Profile useQuery 문 생성

---
+++ src/features/profile/api/profile.ts
@@ -0,0 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import { END_POINT } from "../constants";
+
+interface PetInformation {
+  name: string;
+  breed: string;
+  description: string;
+  personalities: [];
+  profile: string;
+}
+
+interface ProfileResponse {
+  code: number;
+  message: string;
+  content: {
+    nickname: string;
+    pet: PetInformation;
+    followers: number[];
+    followings: number[];
+    likes: number[];
+    bookmarks: number[];
+    tempCnt: number;
+  };
+}
+
+const getProfile = async (nickname: string): Promise<ProfileResponse> => {
+  const response = await fetch(END_POINT.PROFILE(nickname));
+  const data: ProfileResponse = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+  return data;
+};
+
+export const useGetProfile = (nickname: string) => {
+  return useQuery({
+    queryKey: ["profile", nickname],
+    queryFn: () => getProfile(nickname),
+
+    enabled: !!nickname,
+  });
+};
