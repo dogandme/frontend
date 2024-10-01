27ad38b8a4dbcf9266654bb91748adb70c6000d5
yonghyeun <ttddcc119@naver.com>
2024-10-01 18:03:17 +0900

    refactor[#232] : usePostLoginForm mutationFn 분리

--- src/features/auth/api/login.ts
+++ src/features/auth/api/login.ts
@@ -18,29 +18,30 @@ interface EmailLoginFormData {
   persistLogin: boolean;
 }
 
+const postLogin = async (
+  formData: EmailLoginFormData,
+): Promise<LoginResponse> => {
+  const response = await fetch(LOGIN_END_POINT.EMAIL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  const data: LoginResponse = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
+};
+
 // TODO 리액트 쿼리를 활용하여 최적화 하기
 export const usePostLoginForm = () => {
   const mutate = useMutation<LoginResponse, Error, EmailLoginFormData>({
-    mutationFn: async (formData) => {
-      const response = await fetch(LOGIN_END_POINT.EMAIL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-
-      const data: LoginResponse = await response.json();
-
-      if (!response.ok) {
-        throw new Error("문제가 발생했습니다. 잠시 후 다시 이용해주세요");
-      }
-      if (data.code === 401) {
-        throw new Error("아이디 또는 비밀번호를 다시 확인해 주세요");
-      }
-
-      return data;
-    },
+    mutationFn: postLogin,
   });
 
   return mutate;
