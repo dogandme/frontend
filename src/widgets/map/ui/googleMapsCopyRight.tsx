62775e21400f2c9f89879f1f178ae5f039a72851
aken-you <kinsk2839@naver.com>
2024-10-01 17:34:34 +0900

    refactor[#231] : googleMapsCopyRight.tsx 생성

---
+++ src/widgets/map/ui/googleMapsCopyRight.tsx
@@ -0,0 +1,34 @@
+import { useMap } from "@vis.gl/react-google-maps";
+
+export const GoogleMapsCopyRight = () => {
+  const map = useMap();
+
+  /**
+   * 해당 핸들 클릭 이벤트는 기본 Google Logo 를 클릭하면 보고 있는 지도의 중심으로 구글 맵스가 열리던 것을 구현한 것입니다.
+   */
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const center = map.getCenter();
+    const latLon = center.toUrlValue();
+    const url = `https://www.google.com/maps?ll=${latLon}&z=16&t=m&hl=ko-KR&gl=US&mapclient=apiv3`;
+    window.open(url, "_blank", "noopener noreferrer");
+  };
+
+  return (
+    <section className="flex w-full absolute bottom-8 left-0  justify-between px-4">
+      <a href="https://www.google.co.kr/maps/?hl=ko" onClick={handleClick}>
+        <img src="/google_on_white.png" alt="google-logo" />
+      </a>
+      <p className="flex items-end gap-1 text-center text-[11px] text-grey-700">
+        <span>지도 데이터 &copy;2024 TMap Mobility</span>
+        <a
+          href="https://www.google.com/intl/ko-KR_US/help/terms_maps/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          약관
+        </a>
+      </p>
+    </section>
+  );
+};
--- src/widgets/map/ui/index.ts
+++ src/widgets/map/ui/index.ts
@@ -1,4 +1,4 @@
-export * from "./GoogleMapsCopyRight";
+export * from "./googleMapsCopyRight";
 export * from "./googleMaps";
 export * from "./MapControlWidget";
 export * from "./MapMarkerWidget";
