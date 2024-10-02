b703241656ee7a4541393008c47f17f7e727bfc7
yonghyeun <ttddcc119@naver.com>
2024-10-03 00:47:54 +0900

    fix[#217] : 이미지 경로 base url 추가

--- src/entities/marking/ui/dashboard.tsx
+++ src/entities/marking/ui/dashboard.tsx
@@ -15,7 +15,7 @@ export const MarkingThumbnailGrid = ({
     <div className="grid grid-cols-3 gap-2 w-full">
       {markings.map(({ id, image }) => (
         <img
-          src={image}
+          src={`${import.meta.env.VITE_API_BASE_URL}/${image}`}
           key={id}
           alt={`마킹 번호 ${id}의 썸네일 이미지`}
           className="w-full h-full object-cover rounded-lg aspect-square"
