d1b544ca39ed4bc6ad056beb17119edafda28b49
yonghyeun <ttddcc119@naver.com>
2024-10-04 00:35:36 +0900

    feat[#247] StackedButtonModal 생성

---
+++ src/shared/ui/modal/StackedButtonModal.stories.tsx
@@ -0,0 +1,69 @@
+import { StoryObj } from "@storybook/react";
+import { EmailInput } from "@/entities/auth/ui";
+import { StackedButtonModal } from "./StackedButtonModal";
+
+export default {
+  title: "shared/modal/StackedButtonModal",
+  component: StackedButtonModal,
+  tags: ["autodocs"],
+
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "ConfirmModal과 동일한 기능을 가지고 있으며 버튼들의 UI가 다릅니다. StackedButtonModal의 버튼은 Stack 형태로 존재하며 버튼의 순서와 디자인 시스템 상 버튼의 타입이 다릅니다.",
+      },
+    },
+  },
+  argTypes: {
+    onClose: {
+      description:
+        "모달을 닫는 함수로 useModal을 통해 생성된 onClose 메소드를 받습니다.",
+    },
+    children: {
+      description: "버튼과 타이틀을 제외 한 모달 내부 컨텐츠를 받습니다.",
+    },
+    closeIconAriaLabel: {
+      description: "닫기 아이콘의 aria-label을 설정합니다.",
+    },
+    title: {
+      description: "모달의 제목을 설정합니다.",
+    },
+    ConfirmButton: {
+      description:
+        "확인 버튼에 특별을 커스텀하여 사용 합니다. 만약 주입되지 않은 경우엔 DefaultConfirmButton을 사용합니다.",
+    },
+    CloseButton: {
+      description:
+        "취소 버튼에 특별을 커스텀하여 사용 합니다. 만약 주입되지 않은 경우엔 DefaultCloseButton을 사용합니다.",
+    },
+    onConfirm: {
+      description:
+        "DefaultConfirmButton의 버튼 클릭 시 실행할 함수를 받습니다. ConfirmButton 이 주입되지 않았을 경우에만 전달 가능 합니다.",
+    },
+    closeText: {
+      description: "DefaultCloseButton의 텍스트를 설정합니다.",
+    },
+    confirmText: {
+      description: "DefaultConfirmButton의 텍스트를 설정합니다.",
+    },
+  },
+};
+
+type Story = StoryObj<typeof StackedButtonModal>;
+
+export const Default: Story = {
+  decorators: [
+    (Story) => (
+      <div className="w-96 h-96">
+        <Story />
+      </div>
+    ),
+  ],
+
+  render: () => (
+    <StackedButtonModal onClose={async () => {}} title="이메일 변경">
+      <EmailInput id="." label="이메일 입력하기" essential />
+    </StackedButtonModal>
+  ),
+};
---
+++ src/shared/ui/modal/StackedButtonModal.tsx
@@ -0,0 +1,110 @@
+import type { useOverlay } from "@/shared/lib";
+import { Button } from "../button";
+import { CloseIcon } from "../icon";
+import { Modal } from "./Modal";
+
+type StackedButton = (onClose: () => Promise<void>) => JSX.Element;
+
+interface StackedButtonModalProps<
+  T extends StackedButton | undefined,
+  K extends StackedButton | undefined,
+> {
+  onClose: ReturnType<typeof useOverlay>["onClose"];
+  children: React.ReactNode;
+  closeIconAriaLabel?: string;
+  title?: string;
+  ConfirmButton?: T;
+  CloseButton?: K;
+  onConfirm?: T extends StackedButton ? never : () => void | Promise<void>;
+  confirmText?: T extends StackedButton ? never : string;
+  closeText?: K extends StackedButton ? never : string;
+}
+
+const DefaultConfirmButton = ({
+  onConfirm,
+  confirmText,
+}: {
+  onConfirm: () => Promise<void>;
+  confirmText: string;
+}) => {
+  return (
+    <Button
+      variant="filled"
+      colorType="primary"
+      size="medium"
+      onClick={onConfirm}
+    >
+      {confirmText}
+    </Button>
+  );
+};
+
+const DefaultCloseButton = ({
+  onClose,
+  closeText,
+}: {
+  onClose: () => void;
+  closeText: string;
+}) => {
+  return (
+    <Button
+      colorType="tertiary"
+      size="medium"
+      variant="text"
+      type="button"
+      onClick={onClose}
+    >
+      {closeText}
+    </Button>
+  );
+};
+
+export const StackedButtonModal = <
+  T extends StackedButton | undefined,
+  K extends StackedButton | undefined,
+>({
+  closeIconAriaLabel = "모달창 닫기",
+  title = "",
+  onClose,
+  children,
+  confirmText,
+  closeText,
+  onConfirm,
+  ConfirmButton,
+  CloseButton,
+}: StackedButtonModalProps<T, K>) => {
+  const navClassName = title ? "flex justify-between" : "flex justify-end";
+
+  return (
+    <Modal modalType="center">
+      <div className={navClassName}>
+        {title && <h1 className="title-1 text-grey-900">{title}</h1>}
+        <button aria-label={closeIconAriaLabel} onClick={onClose}>
+          <CloseIcon />
+        </button>
+      </div>
+      <section className="text-grey-700 body-2">{children}</section>
+      <div className="flex flex-col gap-2 self-stretch">
+        {ConfirmButton ? (
+          ConfirmButton(onClose)
+        ) : (
+          <DefaultConfirmButton
+            onConfirm={async () => {
+              await onConfirm?.();
+              onClose();
+            }}
+            confirmText={confirmText || "저장"}
+          />
+        )}
+        {CloseButton ? (
+          CloseButton(onClose)
+        ) : (
+          <DefaultCloseButton
+            onClose={onClose}
+            closeText={closeText || "취소"}
+          />
+        )}
+      </div>
+    </Modal>
+  );
+};
