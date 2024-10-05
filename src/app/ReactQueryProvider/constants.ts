433ff1eae6dbe8399c8fcdd5f51e23b032fc18fb
yonghyeun <ttddcc119@naver.com>
2024-10-05 15:44:15 +0900

    feat[#247] : 리액트 쿼리 에러 핸들리에 사용 할 상수 생성

---
+++ src/app/ReactQueryProvider/constants.ts
@@ -0,0 +1,9 @@
+// message
+export const ERROR_MESSAGE = {
+  ACCESS_TOKEN_INVALIDATED: "토큰 검증에 실패했습니다.",
+  REFRESH_TOKEN_INVALIDATED: "RefreshToken 검증에 실패했습니다.",
+};
+// endpoint
+export const END_POINT = {
+  REFRESH_ACCESS_TOKEN: `${import.meta.env.VITE_API_BASE_URL}/auth`,
+};
