2e85683b92a61abe7f7581bed1509f968bf990b2
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-10-09 19:50:40 +0900

    [Hotfix] AppProviderLayout 구조 올바르게 변경 (#276)
    * fix[#275] : AppProvider 컴포넌트 순서 변경
    
    * fix[#275] : FooterNavigationBar MainLayout 내부로 이동
    
    * refactor[#275] : Provider 들 MainLayout 외부로 변경

--- src/app/AppProviderLayout.tsx
+++ src/app/AppProviderLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { DevTools } from "./DevTools";
-import { FooterNavigationBar } from "./FooterNavigationBar";
 import { GoogleMapsProvider } from "./GoogleMapsProvider";
 import { HistoryTracker } from "./HistoryTracker";
 import { MainLayout } from "./MainLayout";
@@ -11,20 +10,19 @@ export const AppProviderLayout = () => {
   return (
     // TODO 레이아웃 범위 디자이너와 상의 후 픽스하기
     <ReactQueryProvider>
-      {/* 
+      <GoogleMapsProvider>
+        <MainLayout>
+          {/* 
       // ! TODO
       // ! 해당 컴포넌트는 개발 환경에서만 사용 되는 컴포넌트 입니다.
       // ! main.ts 파일에서 렌더링 되며 import.meta.DEV 를 통해 개발 환경인지 확인합니다.
       // ! 배포 시엔 해당 컴포넌트가 렌더링 되지 않지만 위험을 방지하기 위해 배포 시 해당 컴포넌트를 제거 해주세요
       */}
-      {import.meta.env.DEV && <DevTools />}
-      <HistoryTracker />
-      <OverlayPortal />
-      <GoogleMapsProvider>
-        <MainLayout>
+          {import.meta.env.DEV && <DevTools />}
+          <HistoryTracker />
+          <OverlayPortal />
           <Outlet />
         </MainLayout>
-        <FooterNavigationBar />
       </GoogleMapsProvider>
     </ReactQueryProvider>
   );
