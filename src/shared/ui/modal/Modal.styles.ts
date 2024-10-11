781caa6e81a102aa00c29241f992819c208baea8
yonghyeun <ttddcc119@naver.com>
2024-10-12 00:29:25 +0900

    fix[#283] : 풀페이지 모달 스타일 변경

--- src/shared/ui/modal/Modal.styles.ts
+++ src/shared/ui/modal/Modal.styles.ts
@@ -1,6 +1,6 @@
 export const modalStyles = {
   fullPage:
-    "left-0 top-0 h-full w-full max-w-[37.5rem] mx-auto flex-col gap-8 bg-grey-0 overflow-y-auto",
+    "left-0 top-0 h-full w-full max-w-[37.5rem] mx-auto flex flex-col gap-8 bg-grey-0 overflow-y-auto",
   center:
     "shadow-custom-1 flex gap-8 fixed left-1/2 top-1/2 w-[20.5rem] -translate-x-1/2 -translate-y-1/2 transform flex-col rounded-[1.75rem] bg-grey-0 px-6 py-6 overflow-y-auto max-h-[calc(100vh-1rem)]",
 } as const;
