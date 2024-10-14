30dca5297935d2d99b757217d2095839fb420452
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-10-14 23:15:45 +0900

    [Hotfix] 이미지 삭제 되지 않는 문제 해결 & 마킹 모달 폼에서 이미지 압축 되지 않는 문제 해결 (#326)
    * fix[#325] : 이미지 삭제 되지 않던 버그 해결
    
    - markingForm 에선 setter method에서 업데이트 된 배열 상태 수정
    - petInfoForm 에선 profile.file 이 null 일 경우 들어온 profile 값으로 업데이트
    
    * fix[#325] : 마킹 모달 폼에서 이미지 압축 되지 않던 문제 해결
    
    -  필터링 하는 배열이 잘못 되어 있던 문제 해결
    - 에러 로그 하는 요소 필터링 로직 밖으로 이동
    
    * refactor[#325] : filter 메소드 콜백 메소드 긍정문으로 변경

--- src/features/marking/store/markingForm.ts
+++ src/features/marking/store/markingForm.ts
@@ -53,7 +53,7 @@ export const useMarkingFormStore = create<
      * 사진이 추가되지 않고  사진이 삭제 된 경우엔 압축을 시작하지 않고 종료합니다.
      */
     if (images.length < get().images.length) {
-      set({ isCompressing: false, inputKey: get().inputKey + 1 });
+      set({ images, isCompressing: false, inputKey: get().inputKey + 1 });
       return;
     }
     /* 동기적으로 압축 예정인 이미지 파일을 업데이트 합니다. */
@@ -63,17 +63,22 @@ export const useMarkingFormStore = create<
       images.map(({ file }) => compressFileImage(file)),
     );
 
-    const resolvedImages = images.filter(({ name }, index) => {
-      const { status } = compressedFiles[index];
-      if (status === "rejected") {
+    /* 이미지 압축 실패 했던 내역에 대한 로그 남기기 */
+    compressedFiles
+      .filter((result) => result.status === "rejected")
+      .forEach((result, index) => {
         console.error(
-          `${name} 을 압축하는데 실패했습니다.
-          ${(compressedFiles[index] as PromiseRejectedResult).reason}`,
+          `${images[index].name}을 압축하는데 실패했습니다.`,
+          result.reason,
         );
-        return false;
-      }
-      return true;
-    });
+      });
+
+    const resolvedImages = compressedFiles
+      .map((result, index) => ({
+        ...images[index],
+        file: result.status === "fulfilled" ? result.value : images[index].file,
+      }))
+      .filter((_, index) => compressedFiles[index].status === "fulfilled");
 
     /**
      * 압축에 실패한 경우엔 input key를 증가 시켜 다시 마운트 합니다.
