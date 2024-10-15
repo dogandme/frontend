128d15b15b0fb8f3621f8f1b049456fb8857bcf8
yonghyeun <ttddcc119@naver.com>
2024-10-16 01:31:35 +0900

    refactor[#295] : 낙관적 업데이트 클린업 메소드 변경

--- src/features/setting/api/putChangeAge.tsx
+++ src/features/setting/api/putChangeAge.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { MyInfo, UserInfoResponse } from "@/entities/auth/api";
 import { AuthStore } from "@/shared/store";
@@ -31,24 +32,29 @@ const putChangeAge = async ({ token, age }: PutChangeAgeRequestData) => {
 
 export const usePutChangeAge = () => {
   const queryClient = useQueryClient();
-  const mutationKey = ["putChangeAge"];
 
   /**
-   * 해당 메소드는 낙관적 업데이트 이후 뮤테이션이 pending 상태일 때 언마운트 되게 된다면 데이터의 무결성을 위해 invalidateQueries를 호출해야 합니다.
+   * 해당 이펙트는 낙관적 업데이트 이후 뮤테이션이 pending 상태일 때 언마운트 되게 된다면 데이터의 무결성을 위해 invalidateQueries를 호출해야 합니다.
    * 이 작업은 myInfo 쿼리의 staleTime이 늘어날 것임을 기대하고 한 작업입니다.
    */
-  const optimisticCleanUpMethod = () => {
-    const cachedMutation = [...queryClient.getMutationCache().getAll()]
-      .reverse()
-      .find((mutation) => mutation?.options.mutationKey === mutationKey);
+  useEffect(() => {
+    return () => {
+      const cachedMutation = [
+        ...queryClient.getMutationCache().findAll({
+          mutationKey: ["putChangeAge"],
+        }),
+      ].reverse()[0];
 
-    if (cachedMutation?.state.status === "pending") {
-      queryClient.invalidateQueries({ queryKey: ["myInfo"] });
-    }
-  };
+      console.log(cachedMutation);
 
-  const mutation = useMutation({
-    mutationKey,
+      if (cachedMutation?.state.status === "pending") {
+        queryClient.invalidateQueries({ queryKey: ["myInfo"] });
+      }
+    };
+  }, [queryClient]);
+
+  return useMutation({
+    mutationKey: ["putChangeAge"],
     mutationFn: putChangeAge,
 
     /* 낙관적 업데이트 시행 */
@@ -81,6 +87,4 @@ export const usePutChangeAge = () => {
       queryClient.setQueryData(["myInfo"], context);
     },
   });
-
-  return { ...mutation, optimisticCleanUpMethod };
 };
