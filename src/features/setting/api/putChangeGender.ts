b7acb10800c7ac6b6f9142e617c782eed72bccb0
yonghyeun <ttddcc119@naver.com>
2024-10-16 01:34:54 +0900

    refactor[#294] : 낙관적 업데이트 클린업 메소드 리팩토링

--- src/features/setting/api/putChangeGender.ts
+++ src/features/setting/api/putChangeGender.ts
@@ -32,27 +32,27 @@ const putChangeGender = async ({ token, gender }: PutChangeAgeRequestData) => {
 
 export const usePutChangeGender = () => {
   const queryClient = useQueryClient();
-  const mutationKey = ["putChangeGender"];
 
   /**
-   * 해당 메소드는 낙관적 업데이트 이후 뮤테이션이 pending 상태일 때 언마운트 되게 된다면 데이터의 무결성을 위해 invalidateQueries를 호출해야 합니다.
+   * 해당 이펙트는 낙관적 업데이트 이후 뮤테이션이 pending 상태일 때 언마운트 되게 된다면 데이터의 무결성을 위해 invalidateQueries를 호출해야 합니다.
    * 이 작업은 myInfo 쿼리의 staleTime이 늘어날 것임을 기대하고 한 작업입니다.
    */
-  const optimisticCleanUpMethod = () => {
-    const cachedMutation = [...queryClient.getMutationCache().getAll()]
-      .reverse()
-      .find((mutation) => mutation?.options.mutationKey === mutationKey);
-
-    if (cachedMutation?.state.status === "pending") {
-      queryClient.invalidateQueries({ queryKey: ["myInfo"] });
-    }
-  };
   useEffect(() => {
-    return optimisticCleanUpMethod;
-  }, []);
+    return () => {
+      const cachedMutation = [
+        ...queryClient.getMutationCache().findAll({
+          mutationKey: ["putChangeGender"],
+        }),
+      ].reverse()[0];
+
+      if (cachedMutation?.state.status === "pending") {
+        queryClient.invalidateQueries({ queryKey: ["myInfo"] });
+      }
+    };
+  }, [queryClient]);
 
   return useMutation({
-    mutationKey,
+    mutationKey: ["putChangeGender"],
     mutationFn: putChangeGender,
 
     /* 낙관적 업데이트 시행 */
