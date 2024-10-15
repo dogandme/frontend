a99c89a9dc05861f84fa4885c221321677cbd924
yonghyeun <ttddcc119@naver.com>
2024-10-16 00:56:55 +0900

    feat[#294] : 성별 변경 퍼블리싱

---
+++ src/features/setting/ui/genderChangeButton.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { genderMap, GenderMapKey } from "@/features/auth/constants";
+import type { MyInfo } from "@/entities/auth/api";
+import { AuthStore, useAuthStore } from "@/shared/store";
+import { ArrowRightIcon } from "@/shared/ui/icon";
+import { Select } from "@/shared/ui/select";
+
+export const GenderChangeButton = ({ gender }: Pick<MyInfo, "gender">) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button className="setting-item" onClick={() => setIsOpen(!isOpen)}>
+        <span>성별 변경</span>
+        <div className="flex items-center text-grey-500">
+          <span className="body-2">{genderMap[gender]}</span>
+          <ArrowRightIcon />
+        </div>
+      </button>
+      <GenderChangeBottomSheet
+        isOpen={isOpen}
+        isSelected={(newGender: GenderMapKey) => newGender === gender}
+        onClose={() => setIsOpen(false)}
+        putChangeGender={() => {}}
+      />
+    </>
+  );
+};
+
+interface GenderChangeBottomSheetProps {
+  isOpen: boolean;
+  onClose: () => void;
+  putChangeGender: ({
+    gender,
+    token,
+  }: {
+    gender: GenderMapKey;
+    token: NonNullable<AuthStore["token"]>;
+  }) => void;
+  isSelected: (newGender: GenderMapKey) => boolean;
+}
+
+const GenderChangeBottomSheet = ({
+  isOpen,
+  putChangeGender,
+  onClose,
+  isSelected,
+}: GenderChangeBottomSheetProps) => {
+  return (
+    <Select id="gender-select" isOpen={isOpen} onClose={onClose}>
+      <Select.BottomSheet>
+        <Select.OptionList>
+          {Object.entries(genderMap).map(([key, value]) => {
+            return (
+              <Select.Option
+                key={key}
+                value={key}
+                isSelected={isSelected(key as GenderMapKey)}
+                onClick={() => {
+                  putChangeGender({
+                    gender: key as GenderMapKey,
+                    token: useAuthStore.getState().token!,
+                  });
+                }}
+              >
+                {value}
+              </Select.Option>
+            );
+          })}
+        </Select.OptionList>
+      </Select.BottomSheet>
+    </Select>
+  );
+};
