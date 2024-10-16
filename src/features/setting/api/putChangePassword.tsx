50c633ccac065e83ae83185ceaa37682b52c0348
yonghyeun <ttddcc119@naver.com>
2024-10-16 22:25:52 +0900

    refactor[#287] : token 선언 위치 mutationFn 으로 변경

--- src/features/setting/api/putChangePassword.tsx
+++ src/features/setting/api/putChangePassword.tsx
@@ -1,6 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import { useSnackBar } from "@/shared/lib";
-import { AuthStore } from "@/shared/store";
+import { useAuthStore } from "@/shared/store";
 import { Snackbar } from "@/shared/ui/snackbar";
 import { SETTING_END_POINT } from "../constants";
 import { usePasswordChangeFormStore } from "../store";
@@ -16,17 +16,14 @@ interface PutChangePasswordResponse {
   message: string;
 }
 
-const putChangePassword = async ({
-  token,
-  ...formOj
-}: PutChangePasswordRequest & { token: NonNullable<AuthStore["token"]> }) => {
+const putChangePassword = async (putRequestData: PutChangePasswordRequest) => {
   const response = await fetch(SETTING_END_POINT.CHANGE_PASSWORD, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
-      Authorization: token,
+      Authorization: useAuthStore.getState().token!,
     },
-    body: JSON.stringify(formOj),
+    body: JSON.stringify(putRequestData),
   });
 
   const data: PutChangePasswordResponse = await response.json();
@@ -56,10 +53,12 @@ export const usePutChangePassword = ({
     onSuccess: () => {
       resetPasswordChangeForm();
       handleOpenSnackbar();
-      // TODO beforeClose 에서 해당 뮤테이션의 key 가 확률적으로 pending 상태이거나 success 이거나 하는 문제가 있어
-      // 0초 뒤에 실행되도록 변경했습니다.
-      // 이러한 문제는 비동기 함수의 실행 순서에 따라 발생하는 문제로 보이는데
-      // 우선 테스트 이후 해당 문제를 해결하도록 하겠습니다.
+      /**
+       * onSuccessCallback 실행 후 시행 되는 beforeClose 는 mutationCache 를 통해 해당 뮤테이션의 상태를 확인하고
+       * pending 상태이면 pending 상태를 기다리고, success 상태이면 닫히도록 하는 로직이 있습니다.
+       * 하지만 mutationCache 는 onSuccess 시점에는 success 상태가 아니기 때문에 beforeClose 가 실행되지 않습니다.
+       * 따라서 setTimeout 을 통해 onSuccessCallback 이후에 실행되도록 합니다.
+       */
       setTimeout(onSuccessCallback, 0);
     },
     onError: (error) => {
