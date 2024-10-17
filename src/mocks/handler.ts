8c2b71f74b0e729e1bf8c7533e091c3f55a2f801
yonghyeun <ttddcc119@naver.com>
2024-10-17 12:29:18 +0900

    refactor[#295] : 핸들러 타입 선언 방식 변경

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,13 +1,10 @@
 import { http, HttpResponse, PathParams } from "msw";
 import { ERROR_MESSAGE } from "@/app/ReactQueryProvider/constants";
 import { APP_END_POINT } from "@/app/ReactQueryProvider/constants";
-import {
-  AGE_RANGE_MAP,
-  LOGIN_END_POINT,
-  SIGN_UP_END_POINT,
-} from "@/features/auth/constants";
+import { LOGIN_END_POINT, SIGN_UP_END_POINT } from "@/features/auth/constants";
 import { MarkingListRequest } from "@/features/marking/api";
 import { MARKING_REQUEST_URL } from "@/features/marking/constants";
+import { PutChangeAgeRequestData } from "@/features/setting/api";
 import { SETTING_END_POINT } from "@/features/setting/constants";
 import { MyInfo } from "@/entities/auth/api";
 import { MY_INFO_END_POINT } from "@/entities/auth/constants";
@@ -610,35 +607,36 @@ const getNewAccessTokenHandler = [
 ];
 
 const putChangeAgeHandler = [
-  http.put(SETTING_END_POINT.CHANGE_AGE, async ({ request }) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    const { age } = (await request.json()) as {
-      age: keyof typeof AGE_RANGE_MAP;
-    };
+  http.put<PathParams, PutChangeAgeRequestData>(
+    SETTING_END_POINT.CHANGE_AGE,
+    async ({ request }) => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      const { age } = await request.json();
 
-    const token = request.headers.get("Authorization")!;
-    if (token === "staleAccessToken") {
-      return HttpResponse.json(
-        {
-          code: 401,
-          message: ERROR_MESSAGE.ACCESS_TOKEN_INVALIDATED,
-        },
-        {
-          status: 401,
-        },
-      );
-    }
+      const token = request.headers.get("Authorization")!;
+      if (token === "staleAccessToken") {
+        return HttpResponse.json(
+          {
+            code: 401,
+            message: ERROR_MESSAGE.ACCESS_TOKEN_INVALIDATED,
+          },
+          {
+            status: 401,
+          },
+        );
+      }
 
-    if (token.split("-")[1] === "naver") {
-      userInfoDB["뽀송송_NAVER"].age = age;
-    }
-    userInfoDB["뽀송송_EMAIL"].age = age;
+      if (token.split("-")[1] === "naver") {
+        userInfoDB["뽀송송_NAVER"].age = age;
+      }
+      userInfoDB["뽀송송_EMAIL"].age = age;
 
-    return HttpResponse.json({
-      code: 200,
-      message: "success",
-    });
-  }),
+      return HttpResponse.json({
+        code: 200,
+        message: "success",
+      });
+    },
+  ),
 ];
 
 // * 나중에 msw 사용을 대비하여 만들었습니다.
