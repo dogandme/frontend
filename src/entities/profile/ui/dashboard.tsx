d1e69d8ad7ea5aedab6f2ff55955aa9e9f918c38
yonghyeun <ttddcc119@naver.com>
2024-10-21 01:23:55 +0900

    fix[#354] : 멀티라인의 경우 고려하여 생성

--- src/entities/profile/ui/dashboard.tsx
+++ src/entities/profile/ui/dashboard.tsx
@@ -52,6 +52,22 @@ export const PetDescription = ({ description }: PetIntroduceProps) => {
   const [isEllipsis, setIsEllipsis] = useState<boolean>(false);
   const descriptionRef = useRef<HTMLParagraphElement>(null);
 
+  // 줄바꿈이 적용된 배열
+  const multiLineDescription = description.split("\n");
+  const isMultiLine = multiLineDescription.length > 1;
+  const renderDescription = () => {
+    if (isMultiLine) {
+      return isSummary
+        ? multiLineDescription[0]
+        : multiLineDescription.map((line, index) => (
+            <p className="min-h-4" key={index}>
+              {line}
+            </p>
+          ));
+    }
+    return description;
+  };
+
   useEffect(() => {
     const $p = descriptionRef.current!;
     setIsEllipsis($p.scrollWidth > $p.clientWidth);
@@ -63,15 +79,15 @@ export const PetDescription = ({ description }: PetIntroduceProps) => {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-4 self-stretch text-grey-500 body-2 w-full justify-between">
-        <p
+        <div
           className={
             isSummary ? "text-ellipsis overflow-hidden text-nowrap" : ""
           }
           ref={descriptionRef}
         >
-          {description}
-        </p>
-        {description.length > 0 && isEllipsis && (
+          {renderDescription()}
+        </div>
+        {description.length > 0 && (isMultiLine || isEllipsis) && (
           <button
             className="w-4 h-4"
             onClick={handleClick}
