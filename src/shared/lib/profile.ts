cef598e855f370d16406afbcbf894820ffa928fb
yonghyeun <ttddcc119@naver.com>
2024-10-21 19:54:13 +0900

    feat[#363] : useNicknameParams 훅 생성

---
+++ src/shared/lib/profile.ts
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom";
+import { useAuthStore } from "../store";
+
+/**
+ * params에서 사용자의 nickname을 가져옵니다.
+ * 경로에서 사용자 닉네임 옆엔 @가 붙어있으므로 slice(1)을 사용하여 @를 제거합니다.
+ * @returns {nicknameParams: string, isMyPage: boolean}
+ */
+export const useNicknameParams = () => {
+  const { nickname } = useParams<{ nickname: string }>();
+  const role = useAuthStore((state) => state.role);
+
+  if (!nickname) {
+    throw new Error("parameter에 nickname이 없습니다.");
+  }
+
+  const nicknameParams = nickname.slice(1);
+
+  const isMyPage =
+    (nicknameParams === "null" && role === null) ||
+    (nicknameParams === "none" && role === "ROLE_NONE") ||
+    (nicknameParams === "guest" && role === "ROLE_GUEST") ||
+    nicknameParams === useAuthStore.getState().nickname;
+  return { nicknameParams, isMyPage };
+};
