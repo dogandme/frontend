88fe24b1be29c1ff34c024324780403038ec0bd0
yonghyeun <ttddcc119@naver.com>
2024-10-22 12:45:54 +0900

    feat[#365] 스낵바에서 직접 useOverlayStore 호출 하도록  props 변경

--- src/shared/ui/snackbar/Snackbar.tsx
+++ src/shared/ui/snackbar/Snackbar.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
+import { useOverlayStore } from "@/shared/store/overlay";
 import { CloseIcon } from "../icon";
 
+export const SNACKBAR_ID = 999999999;
 export interface SnackBarProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
-  onClose: () => Promise<void>;
   autoHideDuration?: number | null;
   positionClassName?: string;
 }
@@ -11,28 +12,28 @@ export interface SnackBarProps extends React.HTMLAttributes<HTMLDivElement> {
 /**
  * InfoSnackBar 컴포넌트는 사용자에게 정보를 제공하는 메시지를 표시합니다.
  * @param children - 스낵바에 표시할 메시지
- * @param onClose - 스낵바를 닫는 함수
  * @param autoHideDuration - 스낵바가 자동으로 사라지는 시간 (ms), 기본적으로 InfoSnackbar는 1초 후 사라집니다. 만약 사라지기를 원치 않는 경우엔 null 을 전달해 주세요
  * @param positionClassName - 스낵바의 위치를 지정하는 클래스명
  */
 export const Snackbar = ({
   children,
-  onClose,
   autoHideDuration = 1000,
   positionClassName = "absolute top-4 left-1/2 transform -translate-x-1/2",
   ...props
 }: SnackBarProps) => {
+  const removeOverlay = useOverlayStore((state) => state.removeOverlay);
+
   useEffect(() => {
     if (autoHideDuration === null) {
       return;
     }
 
     const timerId = setTimeout(() => {
-      onClose();
+      removeOverlay(SNACKBAR_ID);
     }, autoHideDuration);
 
     return () => clearTimeout(timerId);
-  }, [autoHideDuration, onClose]);
+  }, [autoHideDuration, removeOverlay]);
 
   const baseClassName =
     "shadow-custom-2 inline-flex min-w-[328px] max-w-96 items-center justify-between rounded-2xl bg-grey-0 py-1 pl-4 pr-3";
@@ -40,7 +41,11 @@ export const Snackbar = ({
   return (
     <div className={`${baseClassName} ${positionClassName}`} {...props}>
       <div className="body-2 text-grey-700 flex flex-col">{children}</div>
-      <button className="h-10" onClick={onClose} aria-label="스낵바 닫기">
+      <button
+        className="h-10"
+        onClick={() => removeOverlay(SNACKBAR_ID)}
+        aria-label="스낵바 닫기"
+      >
         <CloseIcon />
       </button>
     </div>
