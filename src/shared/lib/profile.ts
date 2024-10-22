3833b05e41ab55de13391ae37801edfc9ac976a5
yonghyeun <ttddcc119@naver.com>
2024-10-22 10:40:18 +0900

    refactor[#363] : 커스텀 훅 기능에 맞게 jsdoc 수정

--- src/shared/lib/profile.ts
+++ src/shared/lib/profile.ts
@@ -2,9 +2,14 @@ import { useParams } from "react-router-dom";
 import { useAuthStore } from "../store";
 
 /**
- * params에서 사용자의 nickname을 가져옵니다.
- * 경로에서 사용자 닉네임 옆엔 @가 붙어있으므로 slice(1)을 사용하여 @를 제거합니다.
- * @returns {nicknameParams: string, isMyPage: boolean}
+ * URL 경로에서 사용자의 닉네임을 가져옵니다.
+ * 경로에서 사용자 닉네임 앞에 '@'가 붙어있으므로 slice(1)을 사용하여 '@'를 제거합니다.
+ * 또한, 현재 사용자의 페이지인지 여부를 확인합니다.
+ *
+ * @returns {{ nicknameParams: string, isMyPage: boolean }}
+ * - nicknameParams: URL에서 '@'를 제거한 사용자 닉네임.
+ * - isMyPage: 현재 페이지가 사용자의 페이지인지 여부를 나타내는 불리언 값.
+ * @throws {Error} parameter에 nickname이 없을 경우 에러를 던집니다.
  */
 export const useNicknameParams = () => {
   const { nickname } = useParams<{ nickname: string }>();
