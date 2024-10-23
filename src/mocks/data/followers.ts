4c5f5a759966bce1c2cca23c80a3e2e93483fedd
yonghyeun <ttddcc119@naver.com>
2024-10-24 01:24:32 +0900

    feat[#362] : 팔로워 리스트는 1부터 400까지 중 200개의 랜덤한 유저 아이디로 변경

--- src/mocks/data/followers.ts
+++ src/mocks/data/followers.ts
@@ -1,21 +1,32 @@
-export const followerListData = Array.from({ length: 200 }).map((_, index) => {
+function getRandomNumbersArray(length: number, max: number): number[] {
+  const arr = Array.from({ length: max }, (_, i) => i + 1);
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr.slice(0, length);
+}
+
+const randomNumbers = getRandomNumbersArray(200, 400);
+
+export const followerListData = randomNumbers.map((num) => {
   return {
-    userId: index,
-    nickname: `user${index}`,
+    userId: num,
+    nickname: `user${num}`,
     pet: {
-      petId: index,
-      name: `name-${index}`,
-      description: `description-${index}`.repeat(Math.ceil(Math.random() * 10)),
+      petId: num,
+      name: `name-${num}`,
+      description: `description-${num}`.repeat(Math.ceil(Math.random() * 10)),
       profile: "/images/buddy.jpg",
-      breed: ["비숑", "비글", "시츄"].at(index % 3),
+      breed: ["비숑", "비글", "시츄"].at(num % 3),
       personalities: [
         "애교가 많은",
         "사람을 좋아하는",
         "까칠한",
         "부끄럼이 많은",
       ].slice(
-        index + (Math.ceil(Math.random() * 10) % 4),
-        index + (Math.ceil(Math.random() * 10) % 4),
+        num + (Math.ceil(Math.random() * 10) % 4),
+        num + (Math.ceil(Math.random() * 10) % 4),
       ),
     },
   };
