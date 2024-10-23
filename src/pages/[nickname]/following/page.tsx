c4a98df3ff29f4fe22e287c79bbf6a47bc13a7b0
yonghyeun <ttddcc119@naver.com>
2024-10-24 01:54:11 +0900

    feat[#362] : 팔로잉 페이지에서 mutualFollowMap 이용하여 isMutual 값 선언

--- src/pages/[nickname]/following/page.tsx
+++ src/pages/[nickname]/following/page.tsx
@@ -1,15 +1,21 @@
 import { FollowNavigationBar } from "@/widgets/follow";
 import { FollowingUserList } from "@/widgets/follow/followingUserList";
 import { useGetFollowingList } from "@/entities/follow/api";
+import { useGetMyMutualFollowMap } from "@/entities/profile/api";
 import { useNicknameParams } from "@/shared/lib/profile";
 import { BackwardNavigationBar } from "@/shared/ui/navigationbar";
 
 export const FollowingPage = () => {
   const { nicknameParams } = useNicknameParams();
 
+  const [mutualFollowMap] = useGetMyMutualFollowMap();
   const { data, fetchNextPage } = useGetFollowingList({
     nickname: nicknameParams,
   });
+
+  if (!mutualFollowMap) {
+    return <div>loading...</div>;
+  }
 
   return (
     <section>
@@ -20,13 +26,13 @@ export const FollowingPage = () => {
       {/* TODO 무한스크롤로 변경 하기 */}
       <ul className="px-4 flex flex-col gap-4 overflow-y-auto">
         <button onClick={() => fetchNextPage()}>next fetch</button>
-        {data?.map((content) => (
+        {data?.map(({ userId, nickname, pet }) => (
           <FollowingUserList
-            key={content.userId}
-            nickname={content.nickname}
-            petName={content.pet.name}
-            profile={content.pet.profile}
-            isMutualFollow={Math.random() > 0.5}
+            key={userId}
+            nickname={nickname}
+            petName={pet.name}
+            profile={pet.profile}
+            isMutualFollow={mutualFollowMap[userId]}
           />
         ))}
       </ul>
