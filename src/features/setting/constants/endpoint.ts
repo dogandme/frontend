bbbb5fbd9346f98b91001e93087978c70ffd0ee4
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-10-25 00:21:09 +0900

    [Hotfix] 동네 수정 엔드포인트 변경

--- src/features/setting/constants/endpoint.ts
+++ src/features/setting/constants/endpoint.ts
@@ -2,7 +2,7 @@ import { API_BASE_URL } from "@/shared/constants";
 
 export const SETTING_END_POINT = {
   LOGOUT: `${API_BASE_URL}/logout`,
-  CHANGE_REGION: `${API_BASE_URL}/users/profile/address`,
+  CHANGE_REGION: `${API_BASE_URL}/users/profile/addresses`,
   DELETE_ACCOUNT: `${API_BASE_URL}/users/me`,
   CHANGE_GENDER: `${API_BASE_URL}/users/profile/gender`,
   CHANGE_PASSWORD: `${API_BASE_URL}/users/profile/password`,
