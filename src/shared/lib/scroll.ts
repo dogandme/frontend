cdbf42fee01423de91244eb43036825cc25e07b7
yonghyeun <ttddcc119@naver.com>
2024-10-25 00:25:10 +0900

    feat[#378] : callbackFn 타입 수정

--- src/shared/lib/scroll.ts
+++ src/shared/lib/scroll.ts
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
  * 해당 훅은 setNode를 반환하며, setNode는 Element를 받아 IntersectionObserver를 생성합니다.
  * IntersectionObserver는 해당 setNode가 장착 된 Element가 화면에 보일 때 callback을 실행합니다.
  */
-export const useInfiniteScroll = (callback: (args?: unknown) => void) => {
+export const useInfiniteScroll = (callbackFn: () => void) => {
   const observer = useRef<IntersectionObserver | null>(null);
   const [node, setNode] = useState<Element | null>(null);
 
@@ -16,7 +16,7 @@ export const useInfiniteScroll = (callback: (args?: unknown) => void) => {
     observer.current = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          callback(entry.target);
+          callbackFn();
         }
       });
     });
@@ -28,7 +28,7 @@ export const useInfiniteScroll = (callback: (args?: unknown) => void) => {
     return () => {
       observer.current?.disconnect();
     };
-  }, [node, callback]);
+  }, [node, callbackFn]);
 
   return [setNode] as const;
 };
