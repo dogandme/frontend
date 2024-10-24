f912a3ddc5e8c0537734039f81a9f6ed4663521f
yonghyeun <ttddcc119@naver.com>
2024-10-24 22:56:27 +0900

    feat[#378] : FollowList 패딩 탑 추가

--- src/widgets/follow/followList.tsx
+++ src/widgets/follow/followList.tsx
@@ -1,3 +1,3 @@
 export const FollowList = ({ children }: { children: React.ReactNode }) => (
-  <ul className="px-4 flex flex-col gap-4 overflow-y-auto">{children}</ul>
+  <ul className="px-4 pt-4 flex flex-col gap-4 overflow-y-auto">{children}</ul>
 );
