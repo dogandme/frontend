92e2fff21d242f5c8e04d367b2e98d369060b426
aken-you <kinsk2839@naver.com>
2024-10-27 14:51:22 +0900

    refactor[#388] : 동네마킹 검색 api url 변경

--- src/entities/marking/constants/index.ts
+++ src/entities/marking/constants/index.ts
@@ -20,7 +20,7 @@ export const SEARCH_MARKING_END_POINT = ({
   sortType,
   offset,
 }: GetMarkingListRequest) =>
-  `${API_BASE_URL}/markings/search?southBottomLat=${southWestLat}&northTopLat=${northEastLat}&southLeftLng=${southWestLng}&northRightLng=${northEastLng}&lat=${lat}&lng=${lng}&sortType=${sortType}&offset=${offset}`;
+  `${API_BASE_URL}/markings/nearby?southBottomLat=${southWestLat}&northTopLat=${northEastLat}&southLeftLng=${southWestLng}&northRightLng=${northEastLng}&lat=${lat}&lng=${lng}&sortType=${sortType}&offset=${offset}`;
 
 export const MARKER_END_POINT = {
   BOUNDARY: ({
