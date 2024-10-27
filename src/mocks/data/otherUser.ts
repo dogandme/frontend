b21bfcaafc673d0355c119d6de0faa73cdd47272
yonghyeun <ttddcc119@naver.com>
2024-10-27 14:00:55 +0900

    fix[#398] : 다른 유저들의 유저아이디는 2부터 시작하도록 변경

--- src/mocks/data/otherUser.ts
+++ src/mocks/data/otherUser.ts
@@ -1,16 +1,16 @@
 // user.ts 에 존재하는 내 userId 는 1이라고 가정 합니다.
 export const otherUsers = Array.from({ length: 400 }).map((_, i) => ({
-  userId: i + 1,
-  nickname: `user${i + 1}`,
+  userId: i + 2,
+  nickname: `user${i + 2}`,
   socialType: null,
   tempCnt: 0,
   followersIds: Math.random() < 0.3 ? [1] : [],
   followingsIds: i < 200 ? [1] : [],
   markings: [],
   pet: {
-    petId: i + 1,
-    name: `name-${i + 1}`,
-    description: `description-${i + 1}`.repeat(Math.ceil(Math.random() * 10)),
+    petId: i + 2,
+    name: `name-${i + 2}`,
+    description: `description-${i + 2}`.repeat(Math.ceil(Math.random() * 10)),
     profile: "/images/buddy",
     breed: ["비숑", "비글", "시츄"][i % 3],
     personalities: [
