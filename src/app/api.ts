9fde56780c19e28bb88d7334fef35a0affe8e7f1
yonghyeun <ttddcc119@naver.com>
2024-10-27 17:32:57 +0900

    refactor[#390] : 타입명 대문자로 시작하도록 변경

--- src/app/api.ts
+++ src/app/api.ts
@@ -2,14 +2,14 @@ import { Nickname } from "@/entities/profile/api";
 import { apiClient } from "@/shared/lib";
 import { APP_END_POINT } from "./ReactQueryProvider/constants";
 
-interface getAccessTokenByRefreshTokenResponse {
+interface GetAccessTokenByRefreshTokenResponse {
   authorization: string;
   role: string;
   nickname: Nickname;
 }
 
 export const getAccessTokenByRefreshToken = () =>
-  apiClient.get<getAccessTokenByRefreshTokenResponse>(
+  apiClient.get<GetAccessTokenByRefreshTokenResponse>(
     APP_END_POINT.REFRESH_ACCESS_TOKEN,
     {
       credentials:
