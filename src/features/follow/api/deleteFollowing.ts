6908338943a96dcaa753b25fee7bbe6edd4fa67e
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-10-28 16:27:38 +0900

    feat[#401] : following , unFollowing 후 사용자 프로필 업데이트 로직 추가 (#402)

--- src/features/follow/api/deleteFollowing.ts
+++ src/features/follow/api/deleteFollowing.ts
@@ -1,12 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { apiClient } from "@/shared/lib";
 import { FOLLOW_END_POINT } from "../constants";
 
 export const useDeleteFollowing = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (nickname: string) => {
       return apiClient.delete(FOLLOW_END_POINT.DELETE_FOLLOWING(nickname), {
         withToken: true,
+      });
+    },
+    onSuccess: (_data, nickname) => {
+      queryClient.invalidateQueries({
+        queryKey: ["profile", nickname],
       });
     },
   });
