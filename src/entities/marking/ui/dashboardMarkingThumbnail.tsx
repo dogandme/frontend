f6bad9efc011f105372a630d34006f76d93f5a82
yonghyeun <ttddcc119@naver.com>
2024-10-28 13:46:30 +0900

    feat[#403] : 대시보드 마킹 썸네일 컴포넌트 생성

---
+++ src/entities/marking/ui/dashboardMarkingThumbnail.tsx
@@ -0,0 +1,20 @@
+import { Nickname } from "@/entities/profile/api";
+import { API_BASE_URL } from "@/shared/constants";
+import { Image, type ImageProps } from "@/shared/ui/image";
+import type { MarkingId, PreviewImage } from "../api";
+
+interface DashboardMarkingImageProps extends Omit<ImageProps, "src" | "alt"> {
+  nickname: Nickname;
+  markingId: MarkingId;
+  previewImage: PreviewImage;
+}
+export const DashboardMarkingThumbnail = ({
+  nickname,
+  markingId,
+  previewImage,
+}: DashboardMarkingImageProps) => (
+  <Image
+    src={`${API_BASE_URL}/markings/image/preview/${markingId}/${previewImage}`}
+    alt={`${nickname}의 ${markingId} 마킹의 썸네일 이미지`}
+  />
+);
