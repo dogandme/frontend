11678514569d5f2b0d8aadbedbca8013b67133d1
yonghyeun <ttddcc119@naver.com>
2024-10-29 17:52:14 +0900

    refactor[#416] : UI 를 통해 낙관적 업데이트를 하기에 북마크 mutation onSuccess 제거

--- src/features/marking/api/deleteSavedMarking.ts
+++ src/features/marking/api/deleteSavedMarking.ts
@@ -15,8 +15,5 @@ const deleteSavedMarking = async ({ markingId }: DeleteSavedMarkingRequest) => {
 export const useDeleteSavedMarking = () => {
   return useMutation<unknown, Error, DeleteSavedMarkingRequest>({
     mutationFn: deleteSavedMarking,
-    onSuccess: () => {
-      // todo 캐시된 데이터 수정
-    },
   });
 };
