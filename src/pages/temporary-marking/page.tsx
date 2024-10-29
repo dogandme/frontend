3cc6e517c58b130ca461207782a3b50aa83ff608
yonghyeun <ttddcc119@naver.com>
2024-10-29 12:32:24 +0900

    feat[#405] : temporary-marking 페이지 생성

--- src/pages/temporary-marking/page.tsx
+++ src/pages/temporary-marking/page.tsx
@@ -1,3 +1,59 @@
+import { useNavigate } from "react-router-dom";
+import { TemporaryMarkingItem } from "@/widgets/marking/ui";
+import { useGetTemporaryMarkingList } from "@/entities/marking/api";
+import { useInfiniteScroll } from "@/shared/lib";
+import { useAuthStore } from "@/shared/store";
+import { BackwardNavigationBar } from "@/shared/ui/navigationbar";
+
 export const TemporaryMarkingPage = () => {
-  return <div>hihi</div>;
+  const navigate = useNavigate();
+
+  const nickname = useAuthStore((state) => state.nickname);
+  const role = useAuthStore((state) => state.role);
+
+  const { data, fetchNextPage, isFetchingNextPage, hasNextPage } =
+    useGetTemporaryMarkingList();
+
+  const [setNode] = useInfiniteScroll(() => {
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  });
+
+  // TODO 권한 없는 회원에게 보여줄 페이지 구현 하기
+  if (role !== "ROLE_USER") {
+    return;
+  }
+
+  if (!data) {
+    return <div>loading ...</div>;
+  }
+
+  if (data.length === 0) {
+    return <div>임시저장된 마킹이 없습니다</div>;
+  }
+
+  return (
+    <>
+      <BackwardNavigationBar
+        label={<h1 className="text-grey-900 title-1">임시저장</h1>}
+        onClick={() => navigate(`/@${nickname}`)}
+      />
+      <section className="pt-4 px-4 pb-32 flex flex-col gap-8">
+        {data.map(([date, temporaryMarkingList]) => (
+          <div className="flex flex-col gap-8">
+            <p className="text-grey-700 title-2">{date}</p>
+            <ul className="flex flex-col gap-8">
+              {temporaryMarkingList.map((marking) => (
+                <li key={marking.markingId}>
+                  <TemporaryMarkingItem {...marking} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+        <div ref={setNode} />
+      </section>
+    </>
+  );
 };
