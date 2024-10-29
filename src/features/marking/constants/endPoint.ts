c652dbbedea6561319df7b48c3ce34d5bea652ab
yonghyeun <ttddcc119@naver.com>
2024-10-29 17:07:01 +0900

    fix[#412] : 마킹 삭제 엔드포인트 변경

--- src/features/marking/constants/endPoint.ts
+++ src/features/marking/constants/endPoint.ts
@@ -3,7 +3,7 @@ import { API_BASE_URL } from "@/shared/constants";
 export const MARKING_END_POINT = {
   ADD: `${API_BASE_URL}/markings`,
   SAVE_TEMP: `${API_BASE_URL}/markings/temp`,
-  DELETE: "/markings",
+  DELETE: `${API_BASE_URL}/markings`,
   LIKE: (markingId: number) => `${API_BASE_URL}/markings/like/${markingId}`,
   SAVE: (markingId: number) => `${API_BASE_URL}/markings/saves/${markingId}`,
 };
