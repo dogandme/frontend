a883614b8214d4103769dbe9bb8faa7de74be4a0
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-10-30 09:53:58 +0900

    fix[#411] : getNextPageParams 계산식 변경 (#415)

--- src/entities/marking/api/getMarkingList.ts
+++ src/entities/marking/api/getMarkingList.ts
@@ -172,7 +172,7 @@ export const useGetMarkingList = ({
         : skipToken,
 
     getNextPageParam: ({ pageAble: { pageNumber }, totalPages }) => {
-      return pageNumber < totalPages ? pageNumber + 1 : null;
+      return pageNumber < totalPages - 1 ? pageNumber + 1 : null;
     },
     initialPageParam: 0,
     select: (data) => data.pages.flatMap((page) => page.markings),
