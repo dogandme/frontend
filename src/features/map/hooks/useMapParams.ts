79cb6388d7721f3901ce5e9a221172b6f168d343
aken-you <kinsk2839@naver.com>
2024-10-31 16:05:47 +0900

    feat[#425] : 맵 페이지의 파라미터를 관리하는 훅 생성

---
+++ src/features/map/hooks/useMapParams.ts
@@ -0,0 +1,91 @@
+import { useSearchParams } from "react-router-dom";
+import type { SortType } from "@/entities/marking/api";
+import { sortTypeMap } from "../constants";
+import type { Bounds } from "./useGetMapCurrentBounds";
+
+export interface Filter {
+  sortType?: SortType;
+  bounds?: Bounds;
+}
+
+const getNumberParam = (
+  key: string,
+  searchParams: URLSearchParams,
+): number | null => {
+  const value = searchParams.get(key);
+
+  if (value === null) {
+    return null;
+  }
+
+  const paramToNumber = Number(value);
+  const isNumber = !Number.isNaN(paramToNumber);
+
+  if (isNumber) {
+    return paramToNumber;
+  }
+
+  return null;
+};
+
+export const useMapParams = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const bounds: Bounds = {
+    northEastLat: getNumberParam("boundsNELat", searchParams),
+    northEastLng: getNumberParam("boundsNELng", searchParams),
+    southWestLat: getNumberParam("boundsSWLat", searchParams),
+    southWestLng: getNumberParam("boundsSWLng", searchParams),
+  };
+
+  const sortTypeParam = searchParams.get("sortType");
+  const sortType =
+    sortTypeParam && sortTypeParam in sortTypeMap
+      ? (sortTypeParam as SortType)
+      : null;
+
+  const hasBoundsParams = Object.values(bounds).every(
+    (value) => value !== null,
+  );
+  const hasSortTypeParam = !!sortType;
+
+  const setMapParams = (mapParams: Filter) => {
+    const newSearchParams = new URLSearchParams(searchParams);
+    const hasNewBoundsParams =
+      mapParams.bounds &&
+      Object.values(mapParams.bounds).every((value) => value !== null);
+
+    if (mapParams.bounds && hasNewBoundsParams) {
+      newSearchParams.set(
+        "boundsNELat",
+        mapParams.bounds.northEastLat!.toString(),
+      );
+      newSearchParams.set(
+        "boundsNELng",
+        mapParams.bounds.northEastLng!.toString(),
+      );
+      newSearchParams.set(
+        "boundsSWLat",
+        mapParams.bounds.southWestLat!.toString(),
+      );
+      newSearchParams.set(
+        "boundsSWLng",
+        mapParams.bounds.southWestLng!.toString(),
+      );
+    }
+
+    if (mapParams.sortType) {
+      newSearchParams.set("sortType", mapParams.sortType);
+    }
+
+    setSearchParams(newSearchParams);
+  };
+
+  return {
+    bounds,
+    hasBoundsParams,
+    sortType,
+    hasSortTypeParam,
+    setMapParams,
+  };
+};
