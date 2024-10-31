2ecd26e1ca14f1c45c03d3f3e8316cb9c871a7a1
aken-you <kinsk2839@naver.com>
2024-10-31 16:24:17 +0900

    refactor[#425] : useGetMarkingList의 sortType 타입 수정

--- src/entities/marking/api/getMarkingList.ts
+++ src/entities/marking/api/getMarkingList.ts
@@ -131,7 +131,8 @@ export const useGetMarkingList = ({
   southWestLng: number | null;
   northEastLat: number | null;
   northEastLng: number | null;
-} & Pick<GetMarkingListRequest, "sortType">) => {
+  sortType: SortType | null;
+}) => {
   const map = useMap();
   const mapCenter = map?.getCenter();
 
@@ -157,7 +158,8 @@ export const useGetMarkingList = ({
       !!northEastLat &&
       !!northEastLng &&
       !!lat &&
-      !!lng
+      !!lng &&
+      !!sortType
         ? ({ pageParam }) =>
             getMarkingList({
               southWestLat,
