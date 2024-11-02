41ad0aee1f52aa0a228302b133571c7d9c8e2082
yonghyeun <ttddcc119@naver.com>
2024-11-02 13:27:34 +0900

    refactor[#426] : 포스트 비지빌리티 맵 엔티티에 하나만 정의

--- src/entities/marking/constants/index.ts
+++ src/entities/marking/constants/index.ts
@@ -36,3 +36,22 @@ export const MY_MARKING_END_POINT = {
   TEMPORARY: (offset: number) =>
     `${API_BASE_URL}/markings/temps?offset=${offset}`,
 };
+
+export const POST_VISIBILITY_MAP = {
+  "전체 공개": "PUBLIC",
+  "팔로우 공개": "FOLLOWERS_ONLY",
+  "나만 보기": "PRIVATE",
+} as const;
+
+export type PostVisibilityName = keyof typeof POST_VISIBILITY_MAP;
+export type PostVisibilityValue =
+  (typeof POST_VISIBILITY_MAP)[PostVisibilityName];
+
+export const REVERSE_POST_VISIBILITY_MAP: Record<
+  PostVisibilityValue,
+  PostVisibilityName
+> = {
+  PUBLIC: "전체 공개",
+  FOLLOWERS_ONLY: "팔로우 공개",
+  PRIVATE: "나만 보기",
+};
