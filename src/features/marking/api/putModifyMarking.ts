90844d78b9726ef8e5843fb1ba1c5c8cbad52cdf
yonghyeun <ttddcc119@naver.com>
2024-11-06 16:32:08 +0900

    feat[#445] : 마킹 수정 시 인수로 받은 쿼리키로 셋쿼리 데이터 , 인밸리데이트 시행

--- src/features/marking/api/putModifyMarking.ts
+++ src/features/marking/api/putModifyMarking.ts
@@ -1,13 +1,15 @@
+import { useLocation } from "react-router-dom";
 import {
   InfiniteData,
   useMutation,
   useQueryClient,
 } from "@tanstack/react-query";
 import type {
-  GetTemporaryMarkingListResponse,
+  Marking,
   TempMarkingFileInfo,
   TempMarkingInfo,
 } from "@/entities/marking/api";
+import { ROUTER_PATH } from "@/shared/constants";
 import { apiClient } from "@/shared/lib";
 import { MARKING_END_POINT } from "../constants";
 
@@ -24,18 +26,47 @@ type ModifyMarkingEndPoint = Extract<
   keyof typeof MARKING_END_POINT,
   "PUT_MODIFY_TEMP_MARKING" | "PUT_MODIFY_MARKING"
 >;
-type InvalidateQueryKey = "temporaryMarkingList"[] | "myMarkerList"[];
+
+type InvalidateQueryKey = "marker" | "markingList";
 
 export interface PutModifyMarkingArguments {
   endPoint: ModifyMarkingEndPoint;
-  queryKey: InvalidateQueryKey;
+  queryKeys: InvalidateQueryKey[];
+}
+
+// TODO : 타입 리팩토링 시 해당 타입을 기본적인 타입으로 한 후 다른 타입들을 확장 시켜 리팩토링
+interface MarkingListResponse {
+  markings: Marking[];
+  totalElements: number;
+  totalPages: number;
+  pageAble: {
+    pageNumber: number;
+    pageSize: number;
+    sort: {
+      sorted: boolean;
+      unsorted: boolean;
+      empty: boolean;
+    };
+  };
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+interface MarkerListResponse {
+  markingId: number;
+  lat: number;
+  lng: number;
+  previewImage: string;
 }
 
 export const usePutModifyMarking = ({
   endPoint,
-  queryKey,
+  queryKeys,
 }: PutModifyMarkingArguments) => {
   const queryClient = useQueryClient();
+  const { pathname } = useLocation();
+
   return useMutation<unknown, Error, PutModifyMarkingRequest>({
     mutationKey: ["markingFormModal"],
     mutationFn: ({ images, ...formObj }: PutModifyMarkingRequest) => {
@@ -54,30 +85,69 @@ export const usePutModifyMarking = ({
     },
 
     onSuccess: (_data, { isTempSaved, id }) => {
-      if (!isTempSaved) {
-        queryClient.setQueryData<InfiniteData<GetTemporaryMarkingListResponse>>(
-          queryKey,
-          (data) => {
-            if (!data) {
-              return data;
-            }
-            const { pages } = data;
-            const newPages = pages.map((page) => ({
-              ...page,
-              markings: page.markings.filter(
-                ({ markingId }) => markingId !== id,
-              ),
-            }));
+      queryKeys.forEach((queryKey) => {
+        // 임시저장 페이지에서 저장 할 경우엔 임시저장 리스트에서 해당 마킹 아이템 제거
+        if (!isTempSaved && pathname === ROUTER_PATH.TEMPORARY_MARKING) {
+          queryClient.setQueriesData<InfiniteData<MarkingListResponse>>(
+            { queryKey: [queryKey] },
+            (data) => {
+              if (!data) {
+                return data;
+              }
+              const { pages } = data;
+              const newPages = pages.map((page) => ({
+                ...page,
+                markings: page.markings.filter(
+                  ({ markingId }) => markingId !== id,
+                ),
+              }));
 
-            return {
-              ...data,
-              pages: newPages,
-            };
-          },
-        );
-      }
-      queryClient.invalidateQueries({
-        queryKey: ["temporaryMarkingList"],
+              return {
+                ...data,
+                pages: newPages,
+              };
+            },
+          );
+        }
+
+        if (isTempSaved && queryKey === "markingList") {
+          queryClient.setQueriesData<InfiniteData<MarkingListResponse>>(
+            { queryKey: [queryKey] },
+            (data) => {
+              if (!data) {
+                return data;
+              }
+              const { pages } = data;
+              const newPages = pages.map((page) => ({
+                ...page,
+                markings: page.markings.filter(
+                  ({ markingId }) => markingId !== id,
+                ),
+              }));
+
+              return {
+                ...data,
+                pages: newPages,
+              };
+            },
+          );
+        }
+
+        if (isTempSaved && queryKey === "marker") {
+          queryClient.setQueriesData<MarkerListResponse[]>(
+            {
+              queryKey: [queryKey],
+            },
+            (data) => {
+              if (!data) {
+                return data;
+              }
+              return data.filter(({ markingId }) => markingId !== id);
+            },
+          );
+        }
+
+        queryClient.invalidateQueries({ queryKey: [queryKey] });
       });
     },
   });
