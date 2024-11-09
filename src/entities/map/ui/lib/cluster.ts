4ea22954d417e7b550d88d52cf6774149fcc3ceb
yonghyeun <ttddcc119@naver.com>
2024-11-09 20:53:20 +0900

    refactor[#453]  : 클러스터 마커 바운더리 타입 리팩토링

--- src/entities/map/ui/lib/cluster.ts
+++ src/entities/map/ui/lib/cluster.ts
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { type Bounds } from "@/features/map/hooks";
-import { useMapStore } from "@/features/map/store";
+import { NonNullableBounds, useMapStore } from "@/features/map/store";
 
 interface LatLng {
   lat: number;
@@ -18,8 +18,8 @@ export class Cluster<T extends Marker> {
   center: LatLng = { lat: 0, lng: 0 };
   // markerBounds는 클러스터링을 시행한 전체 마커들이 존재하는 범위를 의미합니다. 클러스터링 생성 시 정규화에 사용 됩니다.
   // bounds는 해당 클러스터 내부 마커들의 바운더리를 의미합니다. 해당 클러스터에 포함되는 마커들의 영역을 의미 합니다.
-  markerBounds: Bounds;
-  bounds: Bounds;
+  markerBounds: NonNullableBounds;
+  bounds: NonNullableBounds;
   markerCount: number = 0;
   previewImage: string = "";
   markingId: number = 0;
@@ -38,7 +38,7 @@ export class Cluster<T extends Marker> {
     lng: 0,
   };
 
-  constructor({ lat, lng }: T, markerBounds: Bounds) {
+  constructor({ lat, lng }: T, markerBounds: NonNullableBounds) {
     this.center = { lat, lng };
     this.markers = [];
     this.markerBounds = markerBounds;
