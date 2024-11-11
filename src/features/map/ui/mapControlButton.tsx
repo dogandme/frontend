c6fbb523b23090a66a884928277b599e50c469dc
aken-you <kinsk2839@naver.com>
2024-11-11 14:16:09 +0900

    feat[#235] : [주변 마킹] 버튼을 클릭하면 주변 마킹 바텀 시트 노출

--- src/features/map/ui/mapControlButton.tsx
+++ src/features/map/ui/mapControlButton.tsx
@@ -19,6 +19,7 @@ import {
   useCurrentLocation,
   useGetMapCurrentBounds,
   useMapMode,
+  useMapQueryParams,
 } from "../hooks";
 import { useMapStore } from "../store";
 
@@ -124,8 +125,11 @@ export const ShowMyMarkingButton = () => {
 };
 
 export const ShowAroundMarkingButton = () => {
-  // todo 상태 전역으로 관리하기
-  const shouldShowAroundMarking = false;
+  const navigate = useNavigate();
+  const mapMode = useMapMode();
+  const { boundsParams } = useMapQueryParams();
+
+  const shouldShowAroundMarking = mapMode === "MAP";
 
   return (
     <Button
@@ -136,7 +140,9 @@ export const ShowAroundMarkingButton = () => {
       className={`${buttonBaseStyles} rounded-t-none`}
       aria-label="주변 마킹 보기"
       onClick={() => {
-        // todo 주변 마킹 보기로 상태 변경
+        navigate(
+          `${ROUTER_PATH.MAP}?boundsNELat=${boundsParams.northEastLat}&boundsNELng=${boundsParams.northEastLng}&boundsSWLat=${boundsParams.southWestLat}&boundsSWLng=${boundsParams.southWestLng}&sortType=POPULARITY`,
+        );
       }}
     >
       <DogFootIcon />
