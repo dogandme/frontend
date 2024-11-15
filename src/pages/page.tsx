ccf9a2929f5810ec67a91ae939a174b37ef7dfd6
yonghyeun <ttddcc119@naver.com>
2024-11-15 23:00:03 +0900

    feat[#476] : / page 경로 map 으로 라우팅 하도록 변경

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,12 +1,15 @@
-import { NotificationNavigationBar } from "@/widgets/notification/ui";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ROUTER_PATH } from "@/shared/constants";
 
 const MainPage = () => {
-  return (
-    <>
-      <NotificationNavigationBar />
-      <div>메인 페이지</div>
-    </>
-  );
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(ROUTER_PATH.MAP);
+  }, []);
+
+  return null;
 };
 
 export default MainPage;
