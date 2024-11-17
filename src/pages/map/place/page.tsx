af637ddd6898929ef03b5b1c8b02a108be1c05b4
yonghyeun <ttddcc119@naver.com>
2024-11-17 22:37:08 +0900

    feat[#483] : 이 장소 관련 마킹 초기 진입 시 maxZoom 으로 줌 변경

--- src/pages/map/place/page.tsx
+++ src/pages/map/place/page.tsx
@@ -1,5 +1,10 @@
+import { useEffect } from "react";
+import { useMap } from "@vis.gl/react-google-maps";
+import { mapOptions } from "@/widgets/map/constants";
 import { PlaceMarkingList } from "@/widgets/map/ui";
+import { MAP_INITIAL_ZOOM } from "@/features/map/constants";
 import { useMapQueryParams } from "@/features/map/hooks";
+import { useMapStore } from "@/features/map/store";
 import { MarkingPins } from "@/entities/map/ui";
 import { useGetBoundaryMarkerList } from "@/entities/marking/api";
 
@@ -8,6 +13,18 @@ export const PlaceMarkingPage = () => {
   const { data } = useGetBoundaryMarkerList({
     ...boundsParams,
   });
+  const map = useMap();
+  const isIdle = useMapStore((state) => state.isIdle);
+
+  // 이 장소 마킹으로 초기 진입한 경우에는 maxZoom 으로 변경합니다.
+  useEffect(() => {
+    if (!isIdle) return;
+
+    const { zoom } = useMapStore.getState().mapInfo;
+    if (zoom === MAP_INITIAL_ZOOM) {
+      map.setZoom(mapOptions.maxZoom);
+    }
+  }, [isIdle]);
 
   return (
     <>
