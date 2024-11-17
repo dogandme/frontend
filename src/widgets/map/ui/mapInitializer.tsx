cc1ee5b5107ce5f7fa7355dbd1b5e90e0f40bbfc
yonghyeun <ttddcc119@naver.com>
2024-11-17 17:15:09 +0900

    fix[#483] : 맵 스낵바 초기 설정 마친 후 나타나도록 기능 수정

--- src/widgets/map/ui/mapInitializer.tsx
+++ src/widgets/map/ui/mapInitializer.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useState } from "react";
 import { useMap } from "@vis.gl/react-google-maps";
 import { MAP_INITIAL_BOUNDS } from "@/features/map/constants";
 import {
@@ -12,6 +13,7 @@ import { CurrentLocationLoading } from "@/entities/map/ui";
 import { useSnackBar } from "@/shared/lib";
 
 export const MapInitializer = () => {
+  const [isInitialized, setIsInitialized] = useState<boolean>(false);
   const map = useMap();
   const mapMode = useMapMode();
 
@@ -52,6 +54,8 @@ export const MapInitializer = () => {
           bounds: getMapBounds(),
           sortType: sortTypeParam ?? defaultSortType,
         });
+
+        setIsInitialized(true);
       })();
       return;
     }
@@ -70,6 +74,7 @@ export const MapInitializer = () => {
           bounds: getMapBounds(),
           sortType: defaultSortType,
         });
+        setIsInitialized(true);
       },
       onError: async () => {
         await map.fitBounds(MAP_INITIAL_BOUNDS);
@@ -78,6 +83,7 @@ export const MapInitializer = () => {
           bounds: getMapBounds(),
           sortType: defaultSortType,
         });
+        setIsInitialized(true);
       },
     });
 
@@ -87,11 +93,11 @@ export const MapInitializer = () => {
   }, [map, isMapIdle]);
 
   useEffect(() => {
-    if (!hasBoundsParams) {
+    if (!isInitialized) {
       return;
     }
     handleOpen("스팟을 발견하고 마킹으로 추억을 남겨보세요", { type: "map" });
-  }, [hasBoundsParams]);
+  }, [isInitialized]);
 
   if (!map || loading || !isMapIdle) {
     return <CurrentLocationLoading />;
