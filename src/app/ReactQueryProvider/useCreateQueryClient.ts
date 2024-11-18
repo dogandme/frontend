197d6a0ae5c633291600134a330890d5e5d1dfbd
aken-you <kinsk2839@naver.com>
2024-11-18 16:37:37 +0900

    refactor[#470] : 유효하지 않은 토큰으로 요청을 보냈을 경우, 재발급하고 실패한 요청을 재시도하는 커스텀 훅 생성

--- src/app/ReactQueryProvider/useCreateQueryClient.ts
+++ src/app/ReactQueryProvider/useCreateQueryClient.ts
@@ -1,17 +1,12 @@
 import { useRef } from "react";
-import { useNavigate } from "react-router-dom";
 import { QueryClient, QueryCache, MutationCache } from "@tanstack/react-query";
-import { HttpError, useSnackBar } from "@/shared/lib";
-import { useAuthStore } from "@/shared/store";
+import { HttpError } from "@/shared/lib";
 import { useOverlayStore } from "@/shared/store/overlay";
-import { ERROR_MESSAGE } from "./constants";
-import { getValidAuthorization } from "./errorHandlers";
+import { useRefreshToken } from "./errorHandlers";
 
 export const useCreateQueryClient = () => {
-  const resetAuthStore = useAuthStore((state) => state.reset);
-  const navigate = useNavigate();
   const resetOverlays = useOverlayStore((state) => state.resetOverlays);
-  const handleOpenSnackbar = useSnackBar();
+  const { refreshTokenAndRetry } = useRefreshToken();
 
   const queryClient = useRef(
     new QueryClient({
@@ -36,24 +31,9 @@ export const useCreateQueryClient = () => {
 
       queryCache: new QueryCache({
         onError: async (error, query) => {
-          if (error instanceof HttpError && error.code === 500) {
-            handleOpenSnackbar(ERROR_MESSAGE.INTERNAL_SERVER_ERROR);
-          }
-
-          switch (error.message) {
-            case ERROR_MESSAGE.ACCESS_TOKEN_INVALIDATED: {
-              await getValidAuthorization({
-                queryClient,
-                callbackFunctions: { resetAuthStore, navigate },
-              });
-              queryClient.invalidateQueries({
-                queryKey: query.queryKey,
-              });
-              break;
-            }
-            default:
-              handleOpenSnackbar(error.message);
-              return;
+          if (error instanceof HttpError && error.code === 401) {
+            refreshTokenAndRetry(queryClient, query);
+            return;
           }
         },
       }),
@@ -70,48 +50,10 @@ export const useCreateQueryClient = () => {
             resetOverlays();
           }
         },
-        onError: async (error, variables, context, mutation) => {
-          if (error instanceof HttpError && error.code === 500) {
-            handleOpenSnackbar(ERROR_MESSAGE.INTERNAL_SERVER_ERROR);
-          }
-
-          switch (error.message) {
-            case ERROR_MESSAGE.ACCESS_TOKEN_INVALIDATED: {
-              await getValidAuthorization({
-                queryClient,
-                callbackFunctions: { resetAuthStore, navigate },
-              });
-              const { options, state } = mutation;
-              /**
-               * 이전에 시도 했던 mutationFn 을 업데이트 된 액세스 토큰을 이용해 다시 시도합니다.
-               * options.mutationFn 으로 재시도 하는 mutation의 경우엔 onSuccess, onError를 자동으로 호출하지 않습니다.
-               * 이에 try,catch 문을 이용해 onSuccess, onError 를 수동으로 호출합니다.
-               */
-              try {
-                const { token } = useAuthStore.getState();
-                const updatedVariables = { ...(variables as object), token };
-                await options.mutationFn?.(updatedVariables);
-                // mutation이 성공하면 mutationCached에 존재하는 로직과 동일하게 모든 오버레이 닫기
-                if (useOverlayStore.getState().overlays.length > 0) {
-                  resetOverlays();
-                }
-                // 기존 mutation 의 onSuccess 호출
-                options.onSuccess?.(state.data, updatedVariables, context);
-              } catch (error) {
-                options.onError?.(error, variables, context);
-              }
-              break;
-            }
-            default:
-              /**
-               * 만약 발생 한 에러가 전역으로 핸들링 할 에러가 아니라면
-               * 아무런 행위도 하지 않습니다.
-               * 2024/10/15 에서 throw error 문이 제거 되었습니다.
-               * throw error 가 있을 경우 에러가 mutation.onError 에게 에러가 전달 되지 않고
-               * 실행 스택이 중단 되어 mutation.onError 가 호출 되지 않았습니다.
-               */
-              handleOpenSnackbar(error.message);
-              return;
+        onError: async (error, variables, _context, mutation) => {
+          if (error instanceof HttpError && error.code === 401) {
+            refreshTokenAndRetry(queryClient, undefined, mutation, variables);
+            return;
           }
         },
       }),
