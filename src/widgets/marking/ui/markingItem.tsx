0484ea9f1219037d6082fdc1fe6af8ed74ad7b30
yonghyeun <ttddcc119@naver.com>
2024-11-19 01:41:49 +0900

    refactor[#485] : 사용되지 않는  useEffect 제거

--- src/widgets/marking/ui/markingItem.tsx
+++ src/widgets/marking/ui/markingItem.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { FollowingToggle } from "@/features/follow/ui";
 import { useDeleteMarking } from "@/features/marking/api";
 import {
