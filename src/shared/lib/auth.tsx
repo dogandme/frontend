d032d10127ac6f07033d1499d1f67bbf99c25da2
aken-you <kinsk2839@naver.com>
2024-11-19 22:37:32 +0900

    feat[#470] : withAuth에서 인증 에러 메시지 처리 로직 수정

--- src/shared/lib/auth.tsx
+++ src/shared/lib/auth.tsx
@@ -1,10 +1,23 @@
 import { type ComponentType } from "react";
 import { AUTH_ERROR_MESSAGE } from "../constants";
-import { useAuthStore } from "../store";
+import { type ROLE, useAuthStore } from "../store";
+
+const getAuthErrorMessage = (role: ROLE) => {
+  switch (role) {
+    case null:
+      return AUTH_ERROR_MESSAGE.NON_LOGIN;
+    case "ROLE_NONE":
+      return AUTH_ERROR_MESSAGE.NON_USER_INFO;
+    case "ROLE_GUEST":
+      return AUTH_ERROR_MESSAGE.NON_PET_INFO;
+    default:
+      return AUTH_ERROR_MESSAGE.NON_AUTHORIZED;
+  }
+};
 
 export const withAuth = <P extends Record<string, unknown>>(
   Component: ComponentType<P>,
-  role: (null | "ROLE_NONE" | "ROLE_GUEST" | "ROLE_USER")[],
+  role: ROLE[],
 ) => {
   return (props: P) => {
     const userRole = useAuthStore((state) => state.role);
@@ -25,16 +38,8 @@ export const withAuth = <P extends Record<string, unknown>>(
       return Math.max(max, roleLevels[cur === null ? "null" : cur]);
     }, 0);
 
-    if (userRole === null && currentLevel <= maxLevel) {
-      throw new Error(AUTH_ERROR_MESSAGE.NON_LOGIN);
-    }
-
-    if (userRole === "ROLE_NONE" && currentLevel <= maxLevel) {
-      throw new Error(AUTH_ERROR_MESSAGE.NON_USER_INFO);
-    }
-
-    if (userRole === "ROLE_GUEST" && currentLevel <= maxLevel) {
-      throw new Error(AUTH_ERROR_MESSAGE.NON_PET_INFO);
+    if (currentLevel <= maxLevel) {
+      throw new Error(getAuthErrorMessage(userRole));
     }
 
     throw new Error(AUTH_ERROR_MESSAGE.NON_AUTHORIZED);
