f945a4db99d98130f7aba84a96311d2477eb6e20
aken-you <kinsk2839@naver.com>
2024-11-19 21:57:05 +0900

    feat[#470] : 기본 정보 등록 페이지 접근 권한 설정

--- src/pages/sign-up/user-info/page.tsx
+++ src/pages/sign-up/user-info/page.tsx
@@ -14,6 +14,6 @@ const UserInfoRegistrationPage = withAuth(() => {
       </main>
     </div>
   );
-}, ["ROLE_NONE", "ROLE_GUEST", "ROLE_USER"]);
+}, ["ROLE_NONE"]);
 
 export default UserInfoRegistrationPage;
