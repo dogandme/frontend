147dd8fc67d5725a8b34d4cf7475113e4973ebbf
yonghyeun <ttddcc119@naver.com>
2024-11-20 15:06:12 +0900

    feat[#499] : 이 장소 마킹 , 내 마킹 state 에 따라 선택된 마킹이 나타나도록 기능 추가

--- src/widgets/map/ui/placeMarkingList.tsx
+++ src/widgets/map/ui/placeMarkingList.tsx
@@ -1,10 +1,11 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
 import { useMap } from "@vis.gl/react-google-maps";
 import { MarkingItem } from "@/widgets/marking/ui";
 import { useMapQueryParams, usePlaceQueryParams } from "@/features/map/hooks";
 import { SortTypeFilter } from "@/features/marking/ui";
-import { useGetMarkingList } from "@/entities/marking/api";
+import { LatLng } from "@/entities/auth/api";
+import { useGetMarkingDetail, useGetMarkingList } from "@/entities/marking/api";
 import {
   useGetMyFollowingIdsMap,
   useGetMyBookmarkIdsMap,
@@ -12,6 +13,7 @@ import {
 } from "@/entities/profile/api";
 import { ROUTER_PATH } from "@/shared/constants";
 import { useInfiniteScroll } from "@/shared/lib";
+import { DividerLine } from "@/shared/ui/divider";
 import { BackwardNavigationBar } from "@/shared/ui/navigationbar";
 import { mapOptions } from "../constants";
 import { MarkingList } from "./markingList";
@@ -19,15 +21,17 @@ import { MarkingList } from "./markingList";
 export const PlaceMarkingList = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const { state } = location;
+
   const { boundsParams, sortTypeParam, setMapQueryParams } =
     useMapQueryParams();
-  const { boundsAdjacentPlace } = usePlaceQueryParams();
+  const { boundsAdjacentPlace, setPlaceQueryParams } = usePlaceQueryParams();
 
-  const { data: myFollowingMap = {} } = useGetMyFollowingIdsMap();
-  const { data: myBookmarkedMap = {} } = useGetMyBookmarkIdsMap();
-  const { data: myLikedMap = {} } = useGetMyLikedIdsMap();
-
-  const { setPlaceQueryParams } = usePlaceQueryParams();
+  const { data: myFollowingIdsMap = {} } = useGetMyFollowingIdsMap();
+  const { data: myBookmarkIdsMap = {} } = useGetMyBookmarkIdsMap();
+  const { data: myLikedIdsMap = {} } = useGetMyLikedIdsMap();
 
   const {
     data: markingList,
@@ -39,6 +43,9 @@ export const PlaceMarkingList = () => {
     sortType: sortTypeParam,
     searchType: "LOCATION",
   });
+  const { data: clickedMarking } = useGetMarkingDetail({
+    markingId: state?.markingInfo?.markingId,
+  });
 
   const [setNode] = useInfiniteScroll(() => {
     if (hasNextPage && !isFetchingNextPage) {
@@ -47,6 +54,18 @@ export const PlaceMarkingList = () => {
   });
 
   const map = useMap();
+
+  const handleRegionClick = async ({ lat, lng }: LatLng) => {
+    await map.setCenter({
+      lat,
+      lng,
+    });
+    await map.setZoom(mapOptions.maxZoom);
+    setPlaceQueryParams({
+      lat,
+      lng,
+    });
+  };
 
   return (
     <>
@@ -61,45 +80,57 @@ export const PlaceMarkingList = () => {
         label={<h1 className="text-grey-900 title-1">이 장소 관련 마킹</h1>}
       />
 
-      <div className="flex justify-end w-full mb-4 px-4">
-        <SortTypeFilter
-          options={["POPULARITY", "RECENT"]}
-          selectedOption={sortTypeParam || "POPULARITY"}
-          onSelect={(sortType) => {
-            setMapQueryParams({ sortType });
-          }}
-        />
-      </div>
+      <section className="px-4 pb-4 flex flex-col gap-4">
+        {/* 내 마킹 페이지에서 특정 마커를 클릭한 경우 나타나는 마킹 아이템 */}
+        {clickedMarking && (
+          <>
+            <MarkingItem
+              {...clickedMarking}
+              onRegionClick={() =>
+                handleRegionClick({
+                  lat: clickedMarking.lat,
+                  lng: clickedMarking.lng,
+                })
+              }
+              isLiked={myLikedIdsMap[clickedMarking.markingId]}
+              isBookmarked={myBookmarkIdsMap[clickedMarking.markingId]}
+            />
+            <DividerLine axis="row" />
+          </>
+        )}
 
-      <MarkingList display="list" className="px-4">
-        {markingList?.map((marking) => (
-          <MarkingItem
-            key={marking.markingId}
-            onRegionClick={async () => {
-              await map.setCenter({
-                lat: marking.lat,
-                lng: marking.lng,
-              });
-              await map.setZoom(mapOptions.maxZoom);
-              setPlaceQueryParams({
-                lat: marking.lat,
-                lng: marking.lng,
-              });
+        <div className="flex justify-end w-full">
+          <SortTypeFilter
+            options={["POPULARITY", "RECENT"]}
+            selectedOption={sortTypeParam || "POPULARITY"}
+            onSelect={(sortType) => {
+              setMapQueryParams({ sortType });
             }}
-            onDelete={() => {
-              queryClient.invalidateQueries({
-                queryKey: ["markingList"],
-              });
-            }}
-            queryKeys={["marker", "markingList"]}
-            isLiked={myLikedMap[marking.markingId]}
-            isBookmarked={myBookmarkedMap[marking.markingId]}
-            isFollowing={myFollowingMap[marking.userId]}
-            {...marking}
           />
-        ))}
-      </MarkingList>
-      <div className="h-[.125rem]" ref={setNode} />
+        </div>
+
+        <MarkingList display="list">
+          {markingList?.map((marking) => (
+            <MarkingItem
+              key={marking.markingId}
+              onRegionClick={() =>
+                handleRegionClick({ lat: marking.lat, lng: marking.lng })
+              }
+              onDelete={() => {
+                queryClient.invalidateQueries({
+                  queryKey: ["markingList"],
+                });
+              }}
+              queryKeys={["marker", "markingList"]}
+              isFollowing={myFollowingIdsMap[marking.userId]}
+              isLiked={myLikedIdsMap[marking.markingId]}
+              isBookmarked={myBookmarkIdsMap[marking.markingId]}
+              {...marking}
+            />
+          ))}
+        </MarkingList>
+        <div className="h-[.125rem]" ref={setNode} />
+      </section>
     </>
   );
 };
