f29d88dfb8283ed3555e68a02f0dcfee781f10ea
yonghyeun <ttddcc119@naver.com>
2024-11-22 18:18:55 +0900

    refactor[#501] : 유저 마킹 페이지 컴포넌트 모듈화

--- src/pages/[nickname]/marking/page.tsx
+++ src/pages/[nickname]/marking/page.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { MarkingList } from "@/widgets/map/ui/markingList";
 import { MarkingItem } from "@/widgets/marking/ui";
+import { useMapQueryParams } from "@/features/map/hooks";
 import { SortTypeFilter } from "@/features/marking/ui";
 import {
   type Marking,
@@ -19,24 +19,68 @@ import { DividerLine } from "@/shared/ui/divider";
 import { BackwardNavigationBar } from "@/shared/ui/navigationbar";
 
 export const UserMarkingPage = withAuth(() => {
-  const { state } = useLocation() as { state: null | { markingId: number } };
-  const navigate = useNavigate();
-
-  const sortTypeOptions: Exclude<SortType, "DISTANCE">[] = [
-    "RECENT",
-    "POPULARITY",
-  ];
-  const [selectedSortType, setSelectedSortType] = useState<
-    Exclude<SortType, "DISTANCE">
-  >(sortTypeOptions[0]);
-
-  const markingId = state?.markingId;
   const { nicknameParams, isMyPage } = useNicknameParams();
-
   const { nickname: myNickname, token } = useAuthStore.getState();
 
   const { data: myProfile } = useGetMyProfile();
   const { data: profile } = useGetProfile({ nickname: myNickname });
+
+  // todo ui 표시
+  if (!token) return null;
+
+  // TODO 로딩 처리
+  if (!myProfile) {
+    return <div>내 정보를 가져오는 중...</div>;
+  }
+
+  return (
+    <div className="px-4">
+      <BackwardNavigationBar
+        label={
+          <h1 className="title-1 text-grey-900 py-4">
+            {nicknameParams === myNickname ? "내" : nicknameParams} 마킹
+          </h1>
+        }
+      />
+      <section className="pb-4 flex flex-col gap-4">
+        {isMyPage &&
+          typeof profile?.tempCnt === "number" &&
+          profile.tempCnt > 0 && (
+            <div className="pt-4">
+              <TemporaryMarkingBar tempCnt={profile.tempCnt} />
+            </div>
+          )}
+        <MyMarkingSortTypeFilter />
+        <MyMarkingList />
+      </section>
+    </div>
+  );
+}, ["ROLE_GUEST", "ROLE_USER"]);
+
+const MyMarkingSortTypeFilter = () => {
+  const { sortTypeParam, setMapQueryParams } = useMapQueryParams();
+
+  return (
+    <div className="flex w-full justify-end">
+      <SortTypeFilter
+        options={["RECENT", "POPULARITY"]}
+        selectedOption={sortTypeParam || "RECENT"}
+        onSelect={(sortType) => {
+          setMapQueryParams({ sortType });
+        }}
+      />
+    </div>
+  );
+};
+
+const MyMarkingList = () => {
+  const { state } = useLocation() as { state: null | { markingId: number } };
+  const navigate = useNavigate();
+  const { sortTypeParam } = useMapQueryParams();
+
+  const markingId = state?.markingId;
+  const { nicknameParams, isMyPage } = useNicknameParams();
+
   const { data: clickedMarking } = useGetMarkingDetail({ markingId });
   const {
     data: markingList,
@@ -45,7 +89,7 @@ export const UserMarkingPage = withAuth(() => {
     isFetchingNextPage,
   } = useGetAllMarkingsOfUser({
     nickname: nicknameParams,
-    sortType: selectedSortType,
+    sortType: (sortTypeParam || "RECENT") as Exclude<SortType, "DISTANCE">,
   });
 
   const [setNode] = useInfiniteScroll(() => {
@@ -72,79 +116,43 @@ export const UserMarkingPage = withAuth(() => {
     });
   };
 
-  // todo ui 표시
-  if (!token) return null;
-
-  // TODO 로딩 처리
-  if (!myProfile) {
-    return <div>내 정보를 가져오는 중...</div>;
-  }
-
   return (
-    <div className="px-4">
-      <BackwardNavigationBar
-        label={
-          <h1 className="title-1 text-grey-900 py-4">
-            {nicknameParams === myNickname ? "내" : nicknameParams} 마킹
-          </h1>
-        }
-      />
+    <>
+      <MarkingList display="list">
+        {clickedMarking && (
+          <>
+            <MarkingItem
+              {...clickedMarking}
+              onRegionClick={() =>
+                handleRegionClick({
+                  markingId: clickedMarking.markingId,
+                  lat: clickedMarking.lat,
+                  lng: clickedMarking.lng,
+                })
+              }
+            />
+            <DividerLine axis="row" />
+          </>
+        )}
+        {markingList?.map((marking) => {
+          if (marking.markingId === markingId) return;
 
-      <section className="pb-4 flex flex-col gap-4">
-        {nicknameParams === myNickname &&
-          typeof profile?.tempCnt === "number" &&
-          profile.tempCnt > 0 && (
-            <div className="pt-4">
-              <TemporaryMarkingBar tempCnt={profile.tempCnt} />
-            </div>
-          )}
-
-        <div className="flex w-full justify-end">
-          <SortTypeFilter
-            options={sortTypeOptions}
-            selectedOption={selectedSortType}
-            onSelect={(sortType) => {
-              setSelectedSortType(sortType as Exclude<SortType, "DISTANCE">);
-            }}
-          />
-        </div>
-
-        <MarkingList display="list">
-          {clickedMarking && (
-            <>
-              <MarkingItem
-                {...clickedMarking}
-                onRegionClick={() =>
-                  handleRegionClick({
-                    markingId: clickedMarking.markingId,
-                    lat: clickedMarking.lat,
-                    lng: clickedMarking.lng,
-                  })
-                }
-              />
-              <DividerLine axis="row" />
-            </>
-          )}
-          {markingList?.map((marking) => {
-            if (marking.markingId === markingId) return;
-
-            return (
-              <MarkingItem
-                key={marking.markingId}
-                onRegionClick={() =>
-                  handleRegionClick({
-                    markingId: marking.markingId,
-                    lat: marking.lat,
-                    lng: marking.lng,
-                  })
-                }
-                {...marking}
-              />
-            );
-          })}
-        </MarkingList>
-        <div className="h-[.125rem]" ref={setNode} />
-      </section>
-    </div>
+          return (
+            <MarkingItem
+              key={marking.markingId}
+              onRegionClick={() =>
+                handleRegionClick({
+                  markingId: marking.markingId,
+                  lat: marking.lat,
+                  lng: marking.lng,
+                })
+              }
+              {...marking}
+            />
+          );
+        })}
+      </MarkingList>
+      <div className="h-[.125rem]" ref={setNode} />
+    </>
   );
-}, ["ROLE_GUEST", "ROLE_USER"]);
+};
