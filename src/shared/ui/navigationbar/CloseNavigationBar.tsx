9b1b8b233b749c0d7404b2b5d1cfd13013abc491
yonghyeun <ttddcc119@naver.com>
2024-11-27 13:38:12 +0900

    refactor[#504] : NavigationBar 를 확장해 생성되는 네비게이션 바 props 수정

--- src/shared/ui/navigationbar/CloseNavigationBar.tsx
+++ src/shared/ui/navigationbar/CloseNavigationBar.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/shared/ui/button";
 import { CloseIcon } from "@/shared/ui/icon";
-import { NavigationBar, NavigationBarProps } from "./NavigationBar";
+import { NavigationBar } from "./NavigationBar";
 
 const CloseButton = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
@@ -17,22 +17,21 @@ const CloseButton = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   );
 };
 
-type CloseNavigationBarProps = Omit<
-  NavigationBarProps,
-  "componentType" | "button"
->;
+interface CloseNavigationBarProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: string;
+}
 
-export const CloseNavigationBar = (props: CloseNavigationBarProps) => {
+export const CloseNavigationBar = ({
+  children,
+  ...props
+}: CloseNavigationBarProps) => {
   const navigate = useNavigate();
-  const { onClick, label, ...rest } = props;
 
   return (
-    <NavigationBar
-      componentType="buttonRight"
-      button={
-        <CloseButton onClick={onClick || (() => navigate(-1))} {...rest} />
-      }
-      label={label}
-    />
+    <NavigationBar>
+      {children}
+      <CloseButton onClick={() => navigate(-1)} {...props} />
+    </NavigationBar>
   );
 };
