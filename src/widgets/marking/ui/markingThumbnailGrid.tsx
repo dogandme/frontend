0eeceffae53d0b66937c7f19eea1a2f84141cffd
yonghyeun <ttddcc119@naver.com>
2024-11-29 21:39:07 +0900

    feat[#513] : 프로필 페이지에서 사용되는 마킹 그리드 썸네일 로딩 처리

--- src/widgets/marking/ui/markingThumbnailGrid.tsx
+++ src/widgets/marking/ui/markingThumbnailGrid.tsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
-import { useGetDashboardMarkingThumbnail } from "@/entities/marking/api";
+import {
+  Marking,
+  useGetDashboardMarkingThumbnail,
+} from "@/entities/marking/api";
 import {
   EmptyMarkingThumbnailGrid,
   EmptyMyMarkingThumbnailGrid,
 } from "@/entities/marking/ui";
 import { Nickname } from "@/entities/profile/api";
 import { API_BASE_URL } from "@/shared/constants";
-import { useInfiniteScroll, useNicknameParams } from "@/shared/lib";
+import {
+  useImageState,
+  useInfiniteScroll,
+  useNicknameParams,
+} from "@/shared/lib";
 
 interface MarkingThumbnailGridProps {
   nickname: Nickname;
@@ -37,31 +44,64 @@ export const MarkingThumbnailGrid = ({
     );
   }
 
+  const thumbnailList = data.map(({ markingId, previewImage }) => ({
+    src: `${API_BASE_URL}/markings/image/preview/${markingId}/${previewImage}`,
+    markingId,
+  }));
+
   return (
     <section className="w-full grid grid-cols-3 gap-2">
-      {data.map(({ markingId, previewImage }) => (
-        <Link
-          to={`/@${nicknameParams}/markings`}
-          key={markingId}
-          className="aspect-square"
-          state={{ markingId }}
-        >
-          <img
-            src={`${API_BASE_URL}/markings/image/preview/${markingId}/${previewImage}`}
-            alt={`${nickname}의 ${markingId} 마킹의 썸네일 이미지`}
-            className="w-full h-full object-cover rounded-[1rem]"
-          />
-        </Link>
-      ))}
+      <MarkingThumbnailGridImages
+        thumbnailList={thumbnailList}
+        nickname={nicknameParams}
+      />
       <div ref={setNode} />
     </section>
   );
 };
 
+interface MarkingThumbnailGridImagesProps {
+  thumbnailList: (Pick<Marking, "markingId"> & {
+    src: string;
+  })[];
+  nickname: string;
+}
+
+const MarkingThumbnailGridImages = ({
+  thumbnailList,
+  nickname,
+}: MarkingThumbnailGridImagesProps) => {
+  const { isLoading, getImageCache } = useImageState(thumbnailList);
+
+  if (isLoading) {
+    return thumbnailList.map((_, idx) => (
+      <div key={idx} className="aspect-square skeleton  rounded-[1rem]" />
+    ));
+  }
+
+  return thumbnailList.map(({ markingId, src }) => {
+    const { isSuccess } = getImageCache(src);
+    return (
+      <Link
+        to={`/@${nickname}/markings`}
+        key={markingId}
+        className="aspect-square"
+        state={{ markingId }}
+      >
+        <img
+          src={isSuccess ? src : "/default-image.png"}
+          alt={`${nickname}의 ${markingId} 마킹의 썸네일 이미지`}
+          className="w-full h-full object-cover rounded-[1rem]"
+        />
+      </Link>
+    );
+  });
+};
+
 export const MarkingThumbnailGridSkeleton = () => (
   <section className="w-full grid grid-cols-3 gap-2">
     {Array.from({ length: 20 }, (_, idx) => idx).map((key) => (
-      <div key={key} className="aspect-square skeleton"></div>
+      <div key={key} className="aspect-square skeleton  rounded-[1rem]" />
     ))}
   </section>
 );
