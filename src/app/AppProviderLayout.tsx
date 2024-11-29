afafd03f1ce422df62c3c06f19e307ec3e6ef5c7
aken-you <kinsk2839@naver.com>
2024-11-29 16:47:54 +0900

    refactor[#470] : DevTools 위치 변경
    - 에러 페이지에서도 DevTools 보이기 위해 위치 변경

--- src/app/AppProviderLayout.tsx
+++ src/app/AppProviderLayout.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import { DevTools } from "./DevTools";
 import { GoogleMapsProvider } from "./GoogleMapsProvider";
 import { HistoryTracker } from "./HistoryTracker";
 import { MainLayout } from "./MainLayout";
@@ -12,13 +11,6 @@ export const AppProviderLayout = () => {
     <ReactQueryProvider>
       <GoogleMapsProvider>
         <MainLayout>
-          {/* 
-      // ! TODO
-      // ! 해당 컴포넌트는 개발 환경에서만 사용 되는 컴포넌트 입니다.
-      // ! main.ts 파일에서 렌더링 되며 import.meta.DEV 를 통해 개발 환경인지 확인합니다.
-      // ! 배포 시엔 해당 컴포넌트가 렌더링 되지 않지만 위험을 방지하기 위해 배포 시 해당 컴포넌트를 제거 해주세요
-      */}
-          {import.meta.env.DEV && <DevTools />}
           <HistoryTracker />
           <OverlayPortal />
           <Outlet />
