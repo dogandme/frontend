c82163ed0d090af291f5259f5ad556de30a7ebb5
aken-you <kinsk2839@naver.com>
2024-12-10 17:52:38 +0900

    feat[#523] : entities/map에 Bounds, LatLng 타입 선언

---
+++ src/entities/map/types/client.ts
@@ -0,0 +1,11 @@
+export interface Bounds {
+  southWestLat: number | null;
+  southWestLng: number | null;
+  northEastLat: number | null;
+  northEastLng: number | null;
+}
+
+export interface LatLng {
+  lat: number | null;
+  lng: number | null;
+}
