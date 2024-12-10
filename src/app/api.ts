6ca6c1d5a1bd26d99633760faf29697777d1cdf0
yonghyeun <ttddcc119@naver.com>
2024-12-10 17:17:57 +0900

    refactor[#523] : app/api 에서 불필요한 제네릭 타입 제거

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,13 +1,9 @@
 import type { SignUpResponse } from "@/features/auth/type/server";
 import { apiClient } from "@/shared/lib";
-import type { Role } from "@/shared/store";
 import { APP_END_POINT } from "./ReactQueryProvider/constants";
 
 export const getAccessTokenByRefreshToken = () =>
-  apiClient.get<SignUpResponse<NonNullable<Role>>>(
-    APP_END_POINT.REFRESH_ACCESS_TOKEN,
-    {
-      credentials:
-        process.env.NODE_ENV === "development" ? "include" : "same-origin",
-    },
-  );
+  apiClient.get<SignUpResponse>(APP_END_POINT.REFRESH_ACCESS_TOKEN, {
+    credentials:
+      process.env.NODE_ENV === "development" ? "include" : "same-origin",
+  });
