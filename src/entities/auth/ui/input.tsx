77172bed7f7eba8f4f7fb803223db3205c7ad8d8
aken-you <kinsk2839@naver.com>
2024-12-13 17:56:19 +0900

    refactor[#526] : entities/auth/ui 파일명 변경

---
+++ src/entities/auth/ui/input.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { VisibilityOffIcon, VisibilityOnIcon } from "@/shared/ui/icon";
+import { Input, InputProps } from "@/shared/ui/input/Input";
+
+type FixedInputProps = "type" | "componentType";
+
+export const EmailInput = (props: Omit<InputProps, FixedInputProps>) => (
+  <Input
+    type="email"
+    inputMode="email"
+    placeholder="이메일을 입력해주세요"
+    componentType="outlinedText"
+    {...props}
+  />
+);
+
+export const PasswordInput = (props: Omit<InputProps, FixedInputProps>) => {
+  const [isVisibilityOn, setIsVisibilityOn] = useState<boolean>(false);
+
+  const handleVisibility = () => {
+    setIsVisibilityOn((prev) => !prev);
+  };
+
+  return (
+    <Input
+      componentType="outlinedText"
+      type={isVisibilityOn ? "text" : "password"}
+      trailingNode={
+        <button
+          onClick={handleVisibility}
+          aria-label="비밀번호 보기 버튼"
+          aria-pressed={isVisibilityOn}
+          title={isVisibilityOn ? "비밀번호 숨기기" : "비밀번호 보기"}
+          type="button"
+        >
+          {isVisibilityOn ? <VisibilityOnIcon /> : <VisibilityOffIcon />}
+        </button>
+      }
+      placeholder="비밀번호를 입력해 주세요"
+      {...props}
+    />
+  );
+};
---
+++ src/entities/auth/ui/signUpLandingModal.tsx
@@ -0,0 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import { ROUTER_PATH } from "@/shared/constants";
+import { useAuthStore } from "@/shared/store/auth";
+import { useRouteHistoryStore } from "@/shared/store/history";
+import { Modal } from "@/shared/ui/modal";
+
+interface SignUpLandingModalProps {
+  onClose: () => Promise<void>;
+}
+
+export const SignUpLandingModal = ({ onClose }: SignUpLandingModalProps) => {
+  const { nickname } = useAuthStore.getState();
+
+  const lastNoneAuthRoute = useRouteHistoryStore(
+    (state) => state.lastNoneAuthRoute,
+  );
+  const navigate = useNavigate();
+
+  const handleNavigate = async (path: string) => {
+    await onClose();
+    navigate(path);
+  };
+
+  return (
+    <Modal modalType="fullPage">
+      <Modal.Header onClick={() => handleNavigate(lastNoneAuthRoute)} />
+      <Modal.Content>
+        <section className="flex flex-col justify-center items-center gap-2 self-stretch headline-3">
+          <h1>
+            <span className="text-tangerine-500">{nickname}</span>님
+          </h1>
+          <p>회원가입을 축하해요</p>
+        </section>
+        <div className="flex justify-center items-center">
+          <img src="/default-image.png" alt="landing-image" />
+        </div>
+        <section className="body-1 text-grey-700 text-center">
+          <p>반려동물을 등록하고</p>
+          <p>더 많은 혜택을 받아보세요</p>
+        </section>
+      </Modal.Content>
+      <Modal.Footer axis="col">
+        <Modal.FilledButton
+          onClick={() => handleNavigate(ROUTER_PATH.SIGN_UP_PET_INFO)}
+        >
+          등록하기
+        </Modal.FilledButton>
+        <Modal.TextButton
+          colorType="tertiary"
+          onClick={() => handleNavigate(lastNoneAuthRoute)}
+        >
+          나중에 할게요
+        </Modal.TextButton>
+      </Modal.Footer>
+    </Modal>
+  );
+};
