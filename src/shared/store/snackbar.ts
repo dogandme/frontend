f4ed820e8d08363a2162f74a2f352277d9cb6d91
yonghyeun <ttddcc119@naver.com>
2024-12-17 17:05:40 +0900

    refactor[#531] : snackbarStore 에 맞게 handleOpen .. 제거하고 setSnackbarProps 로 변경

--- src/shared/store/snackbar.ts
+++ src/shared/store/snackbar.ts
@@ -1,33 +1,28 @@
 import { create } from "zustand";
-import { SNACKBAR_ID } from "../constants";
 import { OverlayStore, useOverlayStore } from "../store/overlay";
 import type { SnackBarProps } from "../ui/snackbar";
 
 interface SnackbarStore extends OverlayStore {
   snackbarProps: SnackBarProps | null;
-  setSnackbarProps: (snackbarProps: SnackBarProps | null) => void;
-  handleOpenSnackbar: (
+  setSnackbarProps: (
     children: SnackBarProps["children"],
     snackbarOptions?: Omit<SnackBarProps, "children">,
   ) => void;
 }
 
-export const useSnackBarStore = create<SnackbarStore>((set, get) => ({
+export const useSnackBarStore = create<SnackbarStore>((set) => ({
   ...useOverlayStore.getState(),
 
   snackbarProps: null,
-
-  setSnackbarProps: (snackbarProps) => set({ snackbarProps }),
-  handleOpenSnackbar: (
+  setSnackbarProps: (
     children: SnackBarProps["children"],
     snackbarOptions?: Omit<SnackBarProps, "children">,
   ) => {
-    const removeOverlay = get().removeOverlay;
-    const setSnackbarProps = get().setSnackbarProps;
-    removeOverlay(SNACKBAR_ID);
-    setSnackbarProps({
-      ...snackbarOptions,
-      children,
+    set({
+      snackbarProps: {
+        ...snackbarOptions,
+        children,
+      },
     });
   },
 }));
