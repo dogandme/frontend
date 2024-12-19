dfe0510811a87e6ef9e37bedd328835618cf2a6a
yonghyeun <ttddcc119@naver.com>
2024-12-19 13:45:36 +0900

    chore[#534] : assert 점수 하향하여 error, warn 추가

--- lighthouserc.ts
+++ lighthouserc.ts
@@ -6,6 +6,40 @@ module.exports = {
       staticDistDir: "./dist",
       isSinglePageApplication: true,
     },
+    assert: {
+      assertions: {
+        "categories:performance": [
+          "warn",
+          { minScore: 0.8 },
+          "error",
+          { minScore: 0.6 },
+        ],
+        "categories:accessibility": [
+          "warn",
+          { minScore: 0.8 },
+          "error",
+          { minScore: 0.6 },
+        ],
+        "categories:best-practices": [
+          "warn",
+          { minScore: 0.8 },
+          "error",
+          { minScore: 0.6 },
+        ],
+        "categories:seo": [
+          "warn",
+          { minScore: 0.8 },
+          "error",
+          { minScore: 0.6 },
+        ],
+        "categories:pwa": [
+          "warn",
+          { minScore: 0.8 },
+          "error",
+          { minScore: 0.6 },
+        ],
+      },
+    },
     upload: {
       // 실행 결과를 로컬 파일이 아닌 구글에서 제공하는 temporary-public-storage에 업로드
       target: "temporary-public-storage",
