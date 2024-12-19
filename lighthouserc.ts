1c242697ef2a0bdb83deb299f0df91db56c805a0
yonghyeun <ttddcc119@naver.com>
2024-12-19 12:46:53 +0900

    chore[#534] : lighthouse 워크플로우에서 build 추가 및 staticDir경로 추가

--- lighthouserc.ts
+++ lighthouserc.ts
@@ -3,6 +3,8 @@ module.exports = {
     collect: {
       url: ["http://localhost:3000/"],
       startServerCommand: "npm run dev",
+      staticDistDir: "./dist",
+      isSinglePageApplication: true,
     },
     upload: {
       // 실행 결과를 로컬 파일이 아닌 구글에서 제공하는 temporary-public-storage에 업로드
