88a57080b9105a96c3e148da9b713a38ed037650
yonghyeun <ttddcc119@naver.com>
2024-12-23 15:49:18 +0900

    refactor[#402] : 펫 정보 변경 후 myProfile invalidate 쿼리키를 이용하여 변경

--- src/features/setting/api/putChangePetInfo.ts
+++ src/features/setting/api/putChangePetInfo.ts
@@ -2,7 +2,6 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { profileQueryKey } from "@/entities/profile/api";
 import type { PetInfo } from "@/entities/profile/types/server";
 import { apiClient } from "@/shared/lib";
-import { useAuthStore } from "@/shared/store";
 import { SETTING_END_POINT } from "../constants";
 
 export interface PutChangePetInfoRequest
@@ -30,18 +29,16 @@ const putChangePetInfo = async ({
 
 export const usePutChangePetInfo = () => {
   const queryClient = useQueryClient();
-  const nickname = useAuthStore((state) => state.nickname);
 
   return useMutation<unknown, Error, PutChangePetInfoRequest>({
     mutationFn: putChangePetInfo,
     mutationKey: ["putChangePetInfo"],
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: profileQueryKey.profile(nickname!),
+        queryKey: profileQueryKey.myProfile(),
       });
     },
     onError: (error) => {
-      // TODO 에러 바운더리 나오면 수정 하기
       console.error(error);
     },
   });
