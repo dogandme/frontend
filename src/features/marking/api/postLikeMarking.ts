144e45989b5abc7e3fb8ef8bb1c86689466df343
yonghyeun <ttddcc119@naver.com>
2024-12-23 15:47:15 +0900

    feat[#402] : 마킹 좋아요 추가 및 제거 후 myProfileInvalidate

--- src/features/marking/api/postLikeMarking.ts
+++ src/features/marking/api/postLikeMarking.ts
@@ -1,5 +1,6 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { Marking } from "@/entities/marking/types/server";
+import { profileQueryKey } from "@/entities/profile/api";
 import { apiClient } from "@/shared/lib";
 import { MARKING_END_POINT } from "../constants";
 
@@ -8,10 +9,16 @@ interface PostLikeMarkingRequest {
 }
 
 export const usePostLikeMarking = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: ({ markingId }: PostLikeMarkingRequest) =>
       apiClient.post(MARKING_END_POINT.LIKE(markingId), {
         withToken: true,
       }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: profileQueryKey.myProfile(),
+      });
+    },
   });
 };
