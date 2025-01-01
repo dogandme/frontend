dbf4615090baee2725861e576054e02852dd11ff
yonghyeun <ttddcc119@naver.com>
2025-01-01 13:02:04 +0900

    fix[#558] : onMutate 에서 authStore 초기화 하던 거 onSuccess로 변경

--- src/features/setting/ui/logoutModal.tsx
+++ src/features/setting/ui/logoutModal.tsx
@@ -1,6 +1,3 @@
-import { useNavigate } from "react-router-dom";
-import { ROUTER_PATH } from "@/shared/constants";
-import { useSnackbar } from "@/shared/store";
 import { CloseIcon } from "@/shared/ui/icon";
 import { Modal } from "@/shared/ui/modal";
 import { usePostLogout } from "../api";
@@ -10,20 +7,12 @@ interface LogoutModalProps {
 }
 
 export const LogoutModal = ({ onCloseLogoutModal }: LogoutModalProps) => {
-  const navigate = useNavigate();
-  const handleOpenSnackbar = useSnackbar("default");
-
-  const { mutate: postLogout } = usePostLogout({
-    onMutate: () => {
-      onCloseLogoutModal();
-      navigate(ROUTER_PATH.MAIN);
-      // 네비게이팅이 완료된 후에 스낵바를 띄우기 위해 setTimeout 사용
-      setTimeout(() => handleOpenSnackbar("로그아웃 되었습니다"), 100);
-    },
-  });
+  const { mutate: postLogout } = usePostLogout();
 
   const handleLogout = () => {
-    postLogout();
+    postLogout(undefined, {
+      onSuccess: () => onCloseLogoutModal(),
+    });
   };
 
   return (
