9955521e3031f6efebe5a291e292a0fe2cbeb1e8
yonghyeun <ttddcc119@naver.com>
2025-01-18 12:02:21 +0900

    refactor[#590] : postCheckDuplicateNickname 에선 onSuccess 시 onClose 호출하지 않도록 수정

--- src/features/setting/ui/changeNicknameModal.tsx
+++ src/features/setting/ui/changeNicknameModal.tsx
@@ -127,7 +127,6 @@ export const ChangeNicknameModal = ({
                         });
                       }
                     },
-                    onSuccess: onClose,
                   },
                 );
               },
